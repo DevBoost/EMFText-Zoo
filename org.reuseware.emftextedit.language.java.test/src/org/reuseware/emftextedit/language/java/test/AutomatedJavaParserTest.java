7f854b5e4700e7a349bdcb1e74ff6742c797d433
Mirko Seifert <mirko.seifert@devboost.de>
2008-10-29 12:19:46 +0000

    fixed warnings and refactored test class code

--- org.reuseware.emftextedit.language.java.test/src/org/reuseware/emftextedit/language/java/test/AutomatedJavaParserTest.java
+++ org.reuseware.emftextedit.language.java.test/src/org/reuseware/emftextedit/language/java/test/AutomatedJavaParserTest.java
@@ -23,35 +23,43 @@ public class AutomatedJavaParserTest extends AbstractJavaParserTest {
 		File inputFolder = new File("./" + TEST_INPUT_FOLDER_NAME);
 		List<File> allTestFiles = collectAllFilesRecursive(inputFolder);
 		for (final File file : allTestFiles) {
-			suite.addTest(new TestCase("Parse " + file.getName()) {
-				public void runTest() {
-					try {
-						test.parseResource(file, TEST_INPUT_FOLDER_NAME);
-					}
-					catch (Exception e) {
-						this.fail(e.getClass() +  ": " + e.getMessage());
-						e.printStackTrace();
-					}
-				}
-			});
-			suite.addTest(new TestCase("Parse and Reprint " + file.getName()) {
-				public void runTest() {
-					try {
-						test.parseAndReprint(file.getName(), TEST_INPUT_FOLDER_NAME, TEST_OUTPUT_FOLDER_NAME);
-					}
-					catch (Exception e) {
-						this.fail(e.getClass() +  ": " + e.getMessage());
-						e.printStackTrace();
-					}
-					
-				}
-			});
+			addParseTest(test, suite, file);
+			addParseAndReprintTest(test, suite, file);
 		}
 		
 		return suite;
-		
 	}
 
+	private static void addParseTest(final AutomatedJavaParserTest test,
+			TestSuite suite, final File file) {
+		suite.addTest(new TestCase("Parse " + file.getName()) {
+			public void runTest() {
+				try {
+					test.parseResource(file, TEST_INPUT_FOLDER_NAME);
+				}
+				catch (Exception e) {
+					fail(e.getClass() +  ": " + e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private static void addParseAndReprintTest(
+			final AutomatedJavaParserTest test, TestSuite suite, final File file) {
+		suite.addTest(new TestCase("Parse and Reprint " + file.getName()) {
+			public void runTest() {
+				try {
+					test.parseAndReprint(file.getName(), TEST_INPUT_FOLDER_NAME, TEST_OUTPUT_FOLDER_NAME);
+				}
+				catch (Exception e) {
+					fail(e.getClass() +  ": " + e.getMessage());
+					e.printStackTrace();
+				}
+				
+			}
+		});
+	}
 
 	@Override
 	protected boolean isExcludedFromReprintTest(String filename) {
