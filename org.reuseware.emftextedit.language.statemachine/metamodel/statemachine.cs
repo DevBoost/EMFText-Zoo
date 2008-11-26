b9593fcc7cdfd2a5bfc0ed31dd03e4434f8c8547
Christian Wende <christian.wende@devboost.de>
2008-11-26 13:27:49 +0000

    renamed statechart to statemachine to conform to uml

---
+++ org.reuseware.emftextedit.language.statemachine/metamodel/statemachine.cs
@@ -0,0 +1,41 @@
+SYNTAXDEF statemachine
+FOR <http://www.eclipse.org/uml2/2.1.0/UML>
+START StateMachine
+
+TOKENS{
+		DEFINE COMMENT$'//'(~('\n'|'\r'))*$;
+		DEFINE INTEGER$('-')?('1'..'9')('0'..'9')*|'0'$;
+		DEFINE FLOAT$('-')?(('1'..'9') ('0'..'9')* | '0') '.' ('0'..'9')+ $;
+}
+
+RULES{
+		
+		StateMachine	::= "StateMachine" name['"','"'] "{" region "}" ;
+		
+		Region 			::= ( subvertex ";" | transition ";")*;
+		
+		State 			::= "state" name['"','"'] "{"
+							("entry" ":" entry)?
+							("exit" ":" exit)?
+							"do" ":" doActivity							
+							"}";
+		
+		Pseudostate 	::= kind[] "state" name['"', '"'];
+							
+		FinalState		::= "final" "state"	name['"','"'] "{"
+							("entry" ":" entry)?
+							("exit" ":" exit)?
+							"do" ":" doActivity							
+							"}"; 					
+							
+		Transition		::= "transition" 
+							"from" source['"','"']
+							"to" target['"','"']
+							"when" trigger
+							( "do" ":" effect )?;
+							
+		Trigger			::= name['"','"']; 
+		
+		Activity		::= name['"','"'];
+										
+}
