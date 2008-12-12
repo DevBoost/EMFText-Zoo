6feed1f10358833a93122d578fe752daf02e87e8
Mirko Seifert <mirko.seifert@devboost.de>
2008-12-12 12:28:20 +0000

    removed unused imports

--- org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/VariableTypeArgumentsReferenceResolver.java
+++ org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/VariableTypeArgumentsReferenceResolver.java
@@ -1,6 +1,5 @@
 package org.emftext.language.java.resource.java.analysis;
 
-import org.emftext.runtime.resource.impl.ReferenceResolverImpl;
 
 public class VariableTypeArgumentsReferenceResolver extends JavaReferenceResolver {
 
