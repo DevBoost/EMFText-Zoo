79bb89e71f8e9cd4c90fd4d72d7a3493242c3e3b
Jendrik Johannes <jendrik.johannes@devboost.de>
2008-12-12 22:30:01 +0000

    multi file save support

--- org.emftext.language.java.resource.classfile/src/org/emftext/language/java/resource/classfile/JavaSourcFileResourceImpl.java
+++ org.emftext.language.java.resource.classfile/src/org/emftext/language/java/resource/classfile/JavaSourcFileResourceImpl.java
@@ -1,6 +1,14 @@
 package org.emftext.language.java.resource.classfile;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.emftext.language.java.core.CompilationUnit;
 import org.emftext.language.java.resource.java.JavaResourceImpl;
 
 public class JavaSourcFileResourceImpl extends JavaResourceImpl {
@@ -9,4 +17,39 @@ public class JavaSourcFileResourceImpl extends JavaResourceImpl {
 		super(uri);
 	}
 
+	@Override
+	protected void doSave(OutputStream outputStream, Map<?, ?> options)
+			throws IOException {
+		if(getContents().size() > 1 && getResourceSet() != null) {
+			for(EObject eObject : new BasicEList<EObject>(getContents())) {
+				if (eObject instanceof CompilationUnit) {
+					CompilationUnit cu = (CompilationUnit) eObject;
+					if (cu.getClassifiers().isEmpty()) {
+						continue;
+					}
+					
+					String[] folder = cu.getPackage().toArray(
+							new String[cu.getPackage().size()]);
+					String   file   = cu.getClassifiers().get(0).getName();
+					
+					URI subResourcURI = getURI().trimSegments(1);
+					subResourcURI = subResourcURI.appendSegments(folder);
+					subResourcURI = subResourcURI.appendSegment(file);
+					subResourcURI =  subResourcURI.appendFileExtension("java");
+					
+					Resource subResource = 
+						getResourceSet().createResource(subResourcURI);
+					
+					subResource.getContents().add(cu);
+					subResource.save(options);					
+				}
+				else {
+					//nothing
+				}
+			}
+		}
+		else {
+			super.doSave(outputStream, options);
+		}
+	}
 }
