55f6f0bfd3affa9a97c46c7da4db174fa40befbd
Mirko Seifert <mirko.seifert@devboost.de>
2008-12-30 15:33:58 +0000

    fixed integer parsing bug refactored integer token resolve code

--- org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaDECIMAL_LITERALTokenResolver.java
+++ org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaDECIMAL_LITERALTokenResolver.java
@@ -1,5 +1,7 @@
 package org.emftext.language.java.resource.java.analysis;
 
+import java.math.BigInteger;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.emftext.runtime.resource.ITextResource;
@@ -15,12 +17,22 @@ public class JavaDECIMAL_LITERALTokenResolver extends JavaBasedTokenResolver imp
 
 	@Override
 	public Object resolve(String lexem, EStructuralFeature feature, EObject container, ITextResource resource) {
-		//return super.resolve(lexem, feature, container, resource);
+		//System.out.println("JavaDECIMAL_LITERALTokenResolver.resolve("+lexem+") : " + result.getClass());
+		try {
+			return parseInteger(lexem, resource);
+		} catch (NumberFormatException nfe) {
+			System.out.println(nfe.getClass().getSimpleName() + ": " + nfe.getMessage() + " in " + resource.getURI());
+			return null;
+		}
+	}
+
+
+	public static Long parseInteger(String lexem, ITextResource resource) throws NumberFormatException {
 		if (lexem.toLowerCase().endsWith("l")) {
 			lexem = lexem.substring(0, lexem.length() - 1);
 		}
-		Long result = Long.decode(lexem);
-		//System.out.println("JavaDECIMAL_LITERALTokenResolver.resolve("+lexem+") : " + result.getClass());
+		BigInteger tempInteger = new BigInteger(lexem, 16);
+		Long result = tempInteger.longValue();
 		return result;
 	}
 }
--- org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaHEX_LITERALTokenResolver.java
+++ org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaHEX_LITERALTokenResolver.java
@@ -1,36 +1,29 @@
 package org.emftext.language.java.resource.java.analysis;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.emftext.runtime.resource.ITextResource;
 import org.emftext.runtime.resource.ITokenResolver;
 import org.emftext.runtime.resource.impl.JavaBasedTokenResolver;
 
-public class JavaHEX_LITERALTokenResolver extends JavaBasedTokenResolver implements ITokenResolver{ 
+public class JavaHEX_LITERALTokenResolver extends JavaBasedTokenResolver implements ITokenResolver {
+	
 	@Override
 	public String deResolve(Object value, EStructuralFeature feature, EObject container) {
-		String result = super.deResolve(value,feature,container);
+		String result = super.deResolve(value, feature, container);
 		return result;
 	}
 
 	@Override
 	public Object resolve(String lexem, EStructuralFeature feature, EObject container, ITextResource resource) {
 		//System.out.println("JavaHEX_LITERALTokenResolver.resolve("+lexem+") : " + (result != null ? result.getClass() : "null"));
+		if (lexem.startsWith("0x")) {
+			lexem = lexem.substring(2);
+		}
 		try {
-			if (lexem.endsWith("L")) {
-				lexem = lexem.substring(0, lexem.length() - 1);
-			}
-			if (lexem.startsWith("0x")) {
-				lexem = lexem.substring(2);
-			}
-			BigInteger tempInteger = new BigInteger(lexem, 16);
-			Long result = tempInteger.longValue();
-			return result;
+			return JavaDECIMAL_LITERALTokenResolver.parseInteger(lexem, resource);
 		} catch (NumberFormatException nfe) {
-			System.out.println(nfe.getMessage() + " in " + resource.getURI());
+			System.out.println(nfe.getClass().getSimpleName() + ": " + nfe.getMessage() + " in " + resource.getURI());
 			return null;
 		}
 	}
