a409a04a67472d01b1ea7f78e6537d580406d5cd
Mirko Seifert <mirko.seifert@devboost.de>
2009-01-08 22:04:40 +0000

    fixed compile errors

--- org.emftext.language.java/src/org/emftext/language/java/UnicodeConverterProvider.java
+++ org.emftext.language.java/src/org/emftext/language/java/UnicodeConverterProvider.java
@@ -3,9 +3,9 @@ package org.emftext.language.java;
 import java.io.InputStream;
 
 import org.emftext.runtime.InputStreamProcessor;
-import org.emftext.runtime.InputStreamProcessorProvider;
+import org.emftext.runtime.IInputStreamProcessorProvider;
 
-public class UnicodeConverterProvider implements InputStreamProcessorProvider {
+public class UnicodeConverterProvider implements IInputStreamProcessorProvider {
 	public InputStreamProcessor getInputStreamProcessor(InputStream inputStream) {
 		return new UnicodeConverter(inputStream);
 	}
