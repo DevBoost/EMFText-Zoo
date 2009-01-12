e424a9de42996a154a92ad33e51a22d12aa8ec29
Florian Heidenreich <florian.heidenreich@devboost.de>
2009-01-12 15:42:36 +0000

    added some folders to ignore

--- org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/JavaPrinter.java
+++ org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/JavaPrinter.java
@@ -1,7 +1,5 @@
 package org.emftext.language.java.resource.java;
 
-import org.emftext.language.java.expressions.UnaryExpressionNotPlusMinus;
-
 
 /**
 * This is the printer class used by EMFText.
@@ -14,20 +12,4 @@ public class JavaPrinter extends JavaPrinterBase {
 	public JavaPrinter(java.io.OutputStream o, org.emftext.runtime.resource.ITextResource resource) {
 		super(o, resource);
 	}
-	
-	@Override
-	public void print_org_emftext_language_java_expressions_UnaryExpressionNotPlusMinus(UnaryExpressionNotPlusMinus element, java.lang.String outertab, java.io.PrintWriter out){
-		if (element.getNegate() != null) {
-			super.print_org_emftext_language_java_operators_Negate(element.getNegate(), outertab, out);
-			super.print_org_emftext_language_java_references_Primary(element.getPrimary(), outertab, out);
-		}
-		else if (element.getComplement() != null) {
-			super.print_org_emftext_language_java_operators_Complement(element.getComplement(), outertab, out);
-			super.print_org_emftext_language_java_references_Primary(element.getPrimary(), outertab, out);
-		}
-		else {
-			super.print_org_emftext_language_java_expressions_UnaryExpressionNotPlusMinus(element, outertab, out);
-		}
-	}
-
 }
