e335af2750d03e14874c3fbb7f7c83f7c87ba411
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-01-13 15:03:38 +0000

    (un)escaping also for single characters

--- org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaCHARACTER_LITERALTokenResolver.java
+++ org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaCHARACTER_LITERALTokenResolver.java
@@ -2,6 +2,7 @@ package org.emftext.language.java.resource.java.analysis;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.emftext.language.java.resource.java.analysis.helper.CharacterEscaper;
 import org.emftext.runtime.resource.ITextResource;
 import org.emftext.runtime.resource.ITokenResolver;
 import org.emftext.runtime.resource.impl.JavaBasedTokenResolver;
@@ -10,18 +11,19 @@ public class JavaCHARACTER_LITERALTokenResolver extends JavaBasedTokenResolver i
 	@Override
 	public String deResolve(Object value, EStructuralFeature feature, EObject container) {
 		String result = super.deResolve(value,feature,container);
+		result = CharacterEscaper.escapeEscapedCharacters(result);
 		result = '\'' + result + '\'';
 		return result;
 	}
 
 	@Override
 	public Object resolve(String lexem, EStructuralFeature feature, EObject container, ITextResource resource) {
-		// TODO we must handle escaped character, for example '\u0000'
 		assert lexem.length() == 3;
 		// remove single quotes
 		assert lexem.charAt(0) == '\'';
 		assert lexem.charAt(lexem.length() - 1) == '\'';
 		lexem = lexem.substring(1, lexem.length() - 1);
+		lexem = CharacterEscaper.unescapeEscapedCharacters(lexem);
 		Character result = Character.valueOf(lexem.charAt(0));
 		//System.out.println("JavaCHARACTER_LITERALTokenResolver.resolve("+lexem+") : " + (result != null ? result.getClass() : "null"));
 		return result;
