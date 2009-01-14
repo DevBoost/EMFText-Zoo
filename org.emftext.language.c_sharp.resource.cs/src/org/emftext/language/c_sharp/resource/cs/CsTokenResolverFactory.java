7f99a03575ffe0af0c9559a97703f103795110eb
Matthias Range <s9586336@mail.inf.tu-dresden.de>
2009-01-14 10:43:51 +0000

    removed the generated code

--- org.emftext.language.c_sharp.resource.cs/src/org/emftext/language/c_sharp/resource/cs/CsTokenResolverFactory.java
+++
@@ -1,8 +0,0 @@
-package org.emftext.language.c_sharp.resource.cs;
-
-public class CsTokenResolverFactory extends org.emftext.runtime.resource.impl.BasicTokenResolverFactory implements org.emftext.runtime.resource.ITokenResolverFactory {
-
-	public CsTokenResolverFactory(){
-		super.registerTokenResolver("TEXT", new org.emftext.language.c_sharp.resource.cs.analysis.CsTEXTTokenResolver());
-	}
-}
