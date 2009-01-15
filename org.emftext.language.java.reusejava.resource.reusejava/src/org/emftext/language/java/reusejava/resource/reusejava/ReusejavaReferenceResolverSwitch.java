becb6f0ee0b8343e015c66a7c0c36bd85d537b91
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-01-15 13:24:30 +0000

    reuseware for java example

---
+++ org.emftext.language.java.reusejava.resource.reusejava/src/org/emftext/language/java/reusejava/resource/reusejava/ReusejavaReferenceResolverSwitch.java
@@ -0,0 +1,7 @@
+package org.emftext.language.java.reusejava.resource.reusejava; 
+
+import org.emftext.language.java.resource.java.JavaReferenceResolverSwitch;
+public class ReusejavaReferenceResolverSwitch extends JavaReferenceResolverSwitch {
+
+
+}
