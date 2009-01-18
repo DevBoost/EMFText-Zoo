7d5bb6c4806171ca3484fc45deed85937bba73b8
Mirko Seifert <mirko.seifert@devboost.de>
2009-01-18 00:14:42 +0000

    changed ignore list (must not ignore analysis package, because it is not needed anymore and should be deleted) removed ResolverSwitch since it will be regenerated

--- org.emftext.language.java.reusejava.resource.reusejava/src/org/emftext/language/java/reusejava/resource/reusejava/ReusejavaReferenceResolverSwitch.java
+++
@@ -1,7 +0,0 @@
-package org.emftext.language.java.reusejava.resource.reusejava; 
-
-import org.emftext.language.java.resource.java.JavaReferenceResolverSwitch;
-public class ReusejavaReferenceResolverSwitch extends JavaReferenceResolverSwitch {
-
-
-}
