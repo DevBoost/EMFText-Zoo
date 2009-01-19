361b9b7f85c68ecd99d6a7e295947a640462a96f
Mirko Seifert <mirko.seifert@devboost.de>
2009-01-19 13:46:05 +0000

    replaced comment resolver by new collect-in resolver

--- org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaCOLLECT_commentsTokenResolver.java
+++ org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/JavaCOLLECT_commentsTokenResolver.java
@@ -1,14 +1,16 @@
 package org.emftext.language.java.resource.java.analysis;
 
-public class JavaML_COMMENTTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
+public class JavaCOLLECT_commentsTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
 	@Override
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		System.out.println("JavaCOLLECT_commentsTokenResolver.deResolve(" + value + ")");
 		java.lang.String result = super.deResolve(value, feature, container);
 		return result;
 	}
 
 	@Override
 	public java.lang.Object resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container, org.emftext.runtime.resource.ITextResource resource) {
+		System.out.println("JavaCOLLECT_commentsTokenResolver.resolve(" + lexem + ")");
 		return super.resolve(lexem,feature,container,resource);
 	}
 }
