819f1ff070bdfed29cc61717de579e290151f869
Mirko Seifert <mirko.seifert@devboost.de>
2009-01-20 09:09:45 +0000

    added bulk test for the Sun JDK

---
+++ org.emftext.language.java.test.bulk/src/org/emftext/language/java/test/bulk/SunJDKTest.java
@@ -0,0 +1,30 @@
+package org.emftext.language.java.test.bulk;
+
+import java.io.IOException;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.core.runtime.CoreException;
+import org.emftext.language.java.test.util.ThreadedTestSuite;
+
+public class SunJDKTest extends AbstractZipFileInputTest {
+	
+	private static final String INPUT_FILE = BULK_INPUT_DIR + "sun_jdk_1.6.0_07.zip";
+	
+	public static Test suite() throws CoreException, IOException {
+		TestSuite suite = new ThreadedTestSuite("Suite testing all files in sun jdk 1.6.0_07 zip file automatically", 5 * 60 * 1000, 8);
+		addToTestSuite(suite, getTestsForZipFileEntries("./" + INPUT_FILE));
+		return suite;
+	}
+
+	@Override
+	protected boolean isExcludedFromReprintTest(String filename) {
+		return false;
+	}
+	
+	@Override
+	protected String getTestInputFolder() {
+		return null;
+	}
+}
