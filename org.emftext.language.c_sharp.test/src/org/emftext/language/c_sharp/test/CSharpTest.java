7577aa4c91bf5a344d989c8e61268d542a6fbb27
Meike Zehlike <s2305824@inf.tu-dresden.de>
2009-01-22 16:10:00 +0000

    fixing testbug, now it's also available for Linux :-)

--- org.emftext.language.c_sharp.test/src/org/emftext/language/c_sharp/test/CSharpTest.java
+++ org.emftext.language.c_sharp.test/src/org/emftext/language/c_sharp/test/CSharpTest.java
@@ -1,5 +1,6 @@
 package org.emftext.language.c_sharp.test;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
@@ -12,7 +13,7 @@ public class CSharpTest extends AbstractCSharpTest {
 	
 	public void testVariables() {
 		try {
-			CompilationUnit unit = loadResource(new FileInputStream("input\\variables.c"), "variables.c");
+			CompilationUnit unit = loadResource(new FileInputStream("input"+File.separator+"variables.c"), "variables.c");
 			List<Definition> defs = unit.getDefinitions();
 			assertEquals(1, defs.size());
 			Definition firstDef = defs.get(0);
