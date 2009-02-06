69168d9d25d438729c636409873b1e49324f54b1
Christian Wende <christian.wende@devboost.de>
2009-02-06 14:06:22 +0000

    fixed fuzzy resolving

--- org.eclipse.uml2.uml.resource.statemachine/src/org/eclipse/uml2/uml/resource/statemachine/analysis/TransitionSourceReferenceResolver.java
+++ org.eclipse.uml2.uml.resource.statemachine/src/org/eclipse/uml2/uml/resource/statemachine/analysis/TransitionSourceReferenceResolver.java
@@ -1,23 +1,21 @@
 package org.eclipse.uml2.uml.resource.statemachine.analysis;
 
-import org.eclipse.uml2.uml.Transition;
 import org.eclipse.uml2.uml.Vertex;
 
-public class TransitionSourceReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver {
+public class TransitionSourceReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.eclipse.uml2.uml.Transition> {
 
 	@Override
-	protected java.lang.String doDeResolve(org.eclipse.emf.ecore.EObject element, org.eclipse.emf.ecore.EObject container, org.eclipse.emf.ecore.EReference reference) {
+	protected java.lang.String doDeResolve(org.eclipse.emf.ecore.EObject element, org.eclipse.uml2.uml.Transition container, org.eclipse.emf.ecore.EReference reference) {
 		return ((Vertex) element).getName();
 	}
 
 	@Override
-	protected void doResolve(java.lang.String identifier, org.eclipse.emf.ecore.EObject container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IResolveResult result) {
-		Transition transition = (Transition) container;	
+	protected void doResolve(java.lang.String identifier, org.eclipse.uml2.uml.Transition container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IResolveResult result) {	
 		
-		for (Vertex sourceCand: transition.getContainer().getSubvertices()) {
+		for (Vertex sourceCand: container.getContainer().getSubvertices()) {
 			if (resolveFuzzy) {
-				if (identifier.startsWith(sourceCand.getName())) {
-					result.addMapping(identifier, sourceCand);
+				if (sourceCand.getName().startsWith(identifier)) {
+					result.addMapping(sourceCand.getName(), sourceCand);
 				}
 			} else {
 				if (identifier.equals(sourceCand.getName())) {
--- org.eclipse.uml2.uml.resource.statemachine/src/org/eclipse/uml2/uml/resource/statemachine/analysis/TransitionTargetReferenceResolver.java
+++ org.eclipse.uml2.uml.resource.statemachine/src/org/eclipse/uml2/uml/resource/statemachine/analysis/TransitionTargetReferenceResolver.java
@@ -1,23 +1,20 @@
 package org.eclipse.uml2.uml.resource.statemachine.analysis;
 
-import org.eclipse.uml2.uml.Transition;
 import org.eclipse.uml2.uml.Vertex;
 
-public class TransitionTargetReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver {
+public class TransitionTargetReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.eclipse.uml2.uml.Transition> {
 
 	@Override
-	protected java.lang.String doDeResolve(org.eclipse.emf.ecore.EObject element, org.eclipse.emf.ecore.EObject container, org.eclipse.emf.ecore.EReference reference) {
-		return ((Vertex)element).getName();
+	protected java.lang.String doDeResolve(org.eclipse.emf.ecore.EObject element, org.eclipse.uml2.uml.Transition container, org.eclipse.emf.ecore.EReference reference) {
+		return ((Vertex) element).getName();
 	}
 
 	@Override
-	protected void doResolve(java.lang.String identifier, org.eclipse.emf.ecore.EObject container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IResolveResult result) {
-		Transition transition = (Transition) container;	
-		
-		for (Vertex targetCand: transition.getContainer().getSubvertices()) {
+	protected void doResolve(java.lang.String identifier, org.eclipse.uml2.uml.Transition container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IResolveResult result) {
+		for (Vertex targetCand: container.getContainer().getSubvertices()) {
 			if (resolveFuzzy) {
-				if (identifier.startsWith(targetCand.getName())) {
-					result.addMapping(identifier, targetCand);
+				if (targetCand.getName().startsWith(identifier)) {
+					result.addMapping(targetCand.getName(), targetCand);
 				}
 			} else {
 				if (identifier.equals(targetCand.getName())) {
