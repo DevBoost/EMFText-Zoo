bce5ae16ca88a4f7136938d68aa7f817bf51bb84
Mirko Seifert <mirko.seifert@devboost.de>
2009-02-19 20:27:41 +0000

    added path to java.cs

--- org.emftext.language.java.reusejava/metamodel/reusejava.cs
+++ org.emftext.language.java.reusejava/metamodel/reusejava.cs
@@ -3,7 +3,7 @@ FOR <http://www.emftext.org/reusejava>
 START java.Containers.CompilationUnit, java.Containers.Package, StatementUnit
 
 IMPORTS {
-	java : <http://www.emftext.org/java> WITH SYNTAX java
+	java : <http://www.emftext.org/java> WITH SYNTAX java <platform:/resource/org.emftext.language.java/metamodel/java.cs>
 }
 
 OPTIONS {
