3d37e311dff7b112964d4eba71e6c55c039eb195
Christian Wende <christian.wende@devboost.de>
2009-03-11 17:08:23 +0000

    additional proxy resolvers

---
+++ org.emftext.language.owl.resource.owl/src/org/emftext/language/owl/resource/owl/analysis/ObjectPropertyDisjointPropertiesReferenceResolver.java
@@ -0,0 +1,14 @@
+package org.emftext.language.owl.resource.owl.analysis;
+
+public class ObjectPropertyDisjointPropertiesReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.emftext.language.owl.ObjectProperty, org.emftext.language.owl.ObjectProperty> {
+	
+	@Override	
+	protected java.lang.String doDeResolve(org.emftext.language.owl.ObjectProperty element, org.emftext.language.owl.ObjectProperty container, org.eclipse.emf.ecore.EReference reference) {
+		return super.doDeResolve(element, container, reference);
+	}
+	
+	@Override	
+	protected void doResolve(java.lang.String identifier, org.emftext.language.owl.ObjectProperty container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IReferenceResolveResult<org.emftext.language.owl.ObjectProperty> result) {
+		super.doResolve(identifier, container, reference, position, resolveFuzzy, result);
+	}
+}
