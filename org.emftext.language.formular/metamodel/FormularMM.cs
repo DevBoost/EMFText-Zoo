39782f1168f7a56615328a7e445634e433fc31e5
Mirko Seifert <mirko.seifert@devboost.de>
2009-03-27 11:44:05 +0000

    added token styles to make forms look really german added example form

--- org.emftext.language.formular/metamodel/FormularMM.cs
+++ org.emftext.language.formular/metamodel/FormularMM.cs
@@ -2,9 +2,21 @@ SYNTAXDEF formular
 FOR <http://www.emftext.org/language/formular>
 START Formular
 
+TOKENSTYLES {
+	"TEXT" COLOR #da0000;
+	"FORMULAR" COLOR #000000, BOLD;
+	"FRAGE" COLOR #000000, BOLD;
+	"AUSWAHL" COLOR #000000, BOLD;
+	"NUR" COLOR #da0000, BOLD;
+	"BEI" COLOR #da0000, BOLD;
+	"DATUM" COLOR #000000, BOLD;
+	"FREITEXT" COLOR #000000, BOLD;
+	"ZAHL" COLOR #000000, BOLD;
+}
+
 RULES {
 	Formular ::= "FORMULAR" titel['"','"'] fragen*;
-	Frage ::= "FRAGE" text['"','"'] ( erklaerung['"','"'] )? ("NUR" "BEI" abhaengigVon[])? antwortTyp;
+	Frage ::= "FRAGE" text['"','"'] ( erklaerung['"','"'] )? ("NUR" "BEI" abhaengigVon[])? ":" antwortTyp;
 	Auswahl ::= "AUSWAHL" (mehrfach[])? "(" optionen ("," optionen)* ")";
 	Option ::= ( id[] ":")? text['"','"'];
 	Datum ::= "DATUM";
