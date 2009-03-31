71e6ab193f624a056e114767a6ae281b4d315833
Sven Karol <sven.karol@tu-dresden.de>
2009-03-31 18:20:57 +0000

    abstract generator class with some little helpers

---
+++ org.emftext.language.formular.resource.formular/src/org/emftext/language/formular/resource/formular/custom/AbstractGenerator.java
@@ -0,0 +1,20 @@
+package org.emftext.language.formular.resource.formular.custom;
+
+import org.emftext.language.formular.*;
+public abstract class AbstractGenerator implements IGenerator {
+
+	public abstract String generate(Object argument);
+	
+	protected String erzeugeFragenAdresse(Option option){
+		Frage frage = (Frage)option.eContainer().eContainer();
+		return erzeugeFragenAdresse(frage);
+	}
+	
+	protected String erzeugeFragenAdresse(Frage frage){
+		Gruppe gruppe = (Gruppe)frage.eContainer();
+		String suffix = "."+(gruppe.getFragen().indexOf(frage)+1);
+		Formular formular = (Formular)gruppe.eContainer();
+		return (formular.getGruppen().indexOf(gruppe)+1)+suffix;
+	}
+	
+}
