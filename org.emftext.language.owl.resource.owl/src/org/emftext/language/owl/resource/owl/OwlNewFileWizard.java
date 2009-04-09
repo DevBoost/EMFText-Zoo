89ed64e579bc0b95e715324a6733c4a0eb95a7dd
Christian Wende <christian.wende@devboost.de>
2009-04-09 11:31:42 +0000

    cleanup

--- org.emftext.language.owl.resource.owl/src/org/emftext/language/owl/resource/owl/OwlNewFileWizard.java
+++
@@ -1,9 +0,0 @@
-package org.emftext.language.owl.resource.owl;
-public class OwlNewFileWizard extends org.emftext.runtime.ui.new_wizard.AbstractNewFileWizard {
-	public String getFileExtension() {
-		return "owl";
-	}
-	public String getExampleContent() {
-		return "Ontology: ";
-	}
-}
