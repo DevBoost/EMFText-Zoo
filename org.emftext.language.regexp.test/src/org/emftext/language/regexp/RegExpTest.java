eea161591adce1c1cd14fe23faf0a01a360fbd26
Mirko Seifert <mirko.seifert@devboost.de>
2009-04-09 21:46:25 +0000

    added empty test project for regular expressions

---
+++ org.emftext.language.regexp.test/src/org/emftext/language/regexp/RegExpTest.java
@@ -0,0 +1,8 @@
+package org.emftext.language.regexp;
+
+import junit.framework.TestCase;
+
+// TODO mseifert: add some basic tests for parsing regular expressions
+public class RegExpTest extends TestCase {
+
+}
