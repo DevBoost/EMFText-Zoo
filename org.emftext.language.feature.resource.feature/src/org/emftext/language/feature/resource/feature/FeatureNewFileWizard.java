9704e456882d237a64d65b58ca4cba18cd4ab5c2
Mirko Seifert <mirko.seifert@devboost.de>
2009-04-13 18:34:22 +0000

    removed generated files from version control

--- org.emftext.language.feature.resource.feature/src/org/emftext/language/feature/resource/feature/FeatureNewFileWizard.java
+++
@@ -1,9 +0,0 @@
-package org.emftext.language.feature.resource.feature;
-public class FeatureNewFileWizard extends org.emftext.runtime.ui.new_wizard.AbstractNewFileWizard {
-	public String getFileExtension() {
-		return "feature";
-	}
-	public String getExampleContent() {
-		return "FeatureModel \" someFeatureModelName \" Feature \" someFeatureName \" ";
-	}
-}
