6f2dcc6eeb36c65bd91b7da18767d64b98bb573c
Mirko Seifert <mirko.seifert@devboost.de>
2009-04-14 08:37:27 +0000

    added TreeJava - a simple example for embedding a DSL into Java

---
+++ org.emftext.language.java.treejava/metamodel/treejava.cs
@@ -0,0 +1,17 @@
+SYNTAXDEF treejava
+FOR <http://www.emftext.org/language/treejava>
+START java.Containers.CompilationUnit
+ 
+IMPORTS {
+	java : <http://www.emftext.org/java> WITH SYNTAX java <../../org.emftext.language.java/metamodel/java.cs>
+}
+
+OPTIONS {
+	defaultTokenName = "IDENTIFIER";
+	usePredefinedTokens = "false";
+	srcFolder = "src-gen";
+}
+
+RULES {
+	Node ::= "@" name[] ("->" "(" children* ")" )?;
+}
