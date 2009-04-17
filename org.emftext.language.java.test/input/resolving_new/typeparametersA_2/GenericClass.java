c6d7d94a2418e73b1566af681a252412e0732173
Mirko Seifert <mirko.seifert@devboost.de>
2009-04-17 14:43:48 +0000

    added simple test for resolving type parameters

---
+++ org.emftext.language.java.test/input/resolving_new/typeparametersA_2/GenericClass.java
@@ -0,0 +1,16 @@
+package resolving_new.typeparametersA_2;
+
+public class GenericClass<
+	// target:1
+	T,
+	// target:2
+	S> {
+
+	public void m(
+		// source:1:target
+		T t,
+		// source:2:target
+		S s) {
+		
+	}
+}
