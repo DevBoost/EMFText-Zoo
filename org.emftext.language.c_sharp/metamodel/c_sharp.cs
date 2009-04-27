1b8dc598e37e19eb58d2c42f93da63fe65e2aa30
Matthias Range <s9586336@mail.inf.tu-dresden.de>
2009-04-27 14:22:40 +0000

    c#: solve problem with character-literal

--- org.emftext.language.c_sharp/metamodel/c_sharp.cs
+++ org.emftext.language.c_sharp/metamodel/c_sharp.cs
@@ -35,6 +35,8 @@ TOKENS {
 	DEFINE DECIMAL_INTEGER_LITERAL $('0'..'9')* ('U' | 'u' | 'L' | 'l' | 'UL' | 'Ul' | 'uL' | 'ul' | 'LU' | 'Lu' | 'lU' | 'lu')?$;
 	DEFINE HEXA_DECIMAL_INTEGER_LITERAL $'0' ('x'|'X') ('0'..'9'|'a'..'f'|'A'..'F')* ('U' | 'u' | 'L' | 'l' | 'UL' | 'Ul' | 'uL' | 'ul' | 'LU' | 'Lu' | 'lU' | 'lu')?$;
 	DEFINE REAL_LITERAL $('0'..'9')* ( ('.' ('0'..'9')+ (('e'|'E') ('+' | '-')? ('0'..'9')+)? ('F'|'f'|'D'|'d'|'M'|'m')? ) | ((('e'|'E') ('+' | '-')? ('0'..'9')+) ('F'|'f'|'D'|'d'|'M'|'m')? ) | ('F'|'f'|'D'|'d'|'M'|'m') )$;
+	DEFINE CHARACTER_LITERAL $'\''('\\'('0'|'a'|'b'|'t'|'n'|'f'|'r'|'v'|'\"'|'\''|'\\')|('\\''u'('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F'))|('\\''U'('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F'))|('\\''x'('0'..'9'|'a'..'f'|'A'..'F')('0'..'9'|'a'..'f'|'A'..'F')?('0'..'9'|'a'..'f'|'A'..'F')?('0'..'9'|'a'..'f'|'A'..'F')?)|('\\'('0'..'3')('0'..'7')('0'..'7')|'\\'('0'..'7')('0'..'7')|'\\'('0'..'7'))|~('\''|'\\'))'\''$;
+	
 	
 	DEFINE WHITESPACE $(' '|'\t'|'\f')$;
 	DEFINE LINEBREAKS $('\r\n'|'\r'|'\n')$;
@@ -477,5 +479,11 @@ literals.HexadecimalIntegerLiteral
 
 literals.RealLiteral
 	::= value[REAL_LITERAL];
+	
+literals.CharacterLiteral
+	::= value[CHARACTER_LITERAL];
+	
+//literals.StringLiteral
+//	::= value[STRING_LITERAL];
 
 }
