13b2d46894b889faebfdcaf85eba521aef3d6173
Mirko Seifert <mirko.seifert@devboost.de>
2009-05-12 20:25:24 +0000

    added OCL expression checker that statically verifies conditions and expressions in templates registered expression checker for the custom_sandwich language

---
+++ org.emftext.language.template_concepts/src/org/emftext/language/template_concepts/ExpressionChecker.java
@@ -0,0 +1,114 @@
+package org.emftext.language.template_concepts;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.ocl.OCL;
+import org.eclipse.ocl.ParserException;
+import org.eclipse.ocl.Query;
+import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.ocl.expressions.OCLExpression;
+import org.eclipse.ocl.helper.OCLHelper;
+import org.emftext.language.template_concepts.Placeholder;
+import org.emftext.language.template_concepts.Template;
+import org.emftext.runtime.IOptionProvider;
+import org.emftext.runtime.IOptions;
+import org.emftext.runtime.IResourcePostProcessor;
+import org.emftext.runtime.IResourcePostProcessorProvider;
+import org.emftext.runtime.resource.ITextResource;
+
+public class ExpressionChecker implements IOptionProvider, IResourcePostProcessor, IResourcePostProcessorProvider {
+
+	public void process(ITextResource resource) {
+		List<EObject> templates = getObjectsByType(resource, Template_conceptsPackage.eINSTANCE.getTemplate());
+		if (templates.size() < 1) {
+			return;
+		}
+		Template template = (Template) templates.get(0);
+		EObject inputMetaClass = template.getInputMetaClass();
+		EClass metaClass = (EClass) inputMetaClass;
+		if (metaClass == null) {
+			return;
+		}
+		
+		List<EObject> placeholders = getObjectsByType(resource, Template_conceptsPackage.eINSTANCE.getPlaceholder());
+		for (EObject next : placeholders) {
+			Placeholder placeholder = (Placeholder) next;
+			String expression = placeholder.getExpression();
+			boolean ok = parseExpression(metaClass, expression);
+			if (!ok) {
+				resource.addError("The expression \"" + expression + "\" is invalid.", placeholder);
+			}
+		}
+
+		List<EObject> loops = getObjectsByType(resource, Template_conceptsPackage.eINSTANCE.getForLoop());
+		for (EObject next : loops) {
+			ForLoop loop = (ForLoop) next;
+			String expression = loop.getCollection();
+			boolean ok = parseExpression(metaClass, expression);
+			if (!ok) {
+				resource.addError("The expression \"" + expression + "\" is invalid.", loop);
+			}
+		}
+
+		List<EObject> conditions = getObjectsByType(resource, Template_conceptsPackage.eINSTANCE.getIfCondition());
+		for (EObject next : conditions) {
+			IfCondition condition = (IfCondition) next;
+			String expression = condition.getCondition();
+			boolean ok = parseExpression(metaClass, expression);
+			if (!ok) {
+				resource.addError("The expression \"" + expression + "\" is invalid.", condition);
+			}
+		}
+	}
+
+	private List<EObject> getObjectsByType(ITextResource resource, EClass metaClass) {
+		List<EObject> foundObjects = new ArrayList<EObject>();
+		
+		TreeIterator<EObject> contents = resource.getAllContents();
+		while (contents.hasNext()) {
+			EObject next = contents.next();
+			if (metaClass.isInstance(next)) {
+				foundObjects.add(next);
+			}
+		}
+		return foundObjects;
+	}
+
+	private boolean parseExpression(EClass inputMetaClass, String expressionString) {
+
+		OCL ocl = org.eclipse.ocl.ecore.OCL.newInstance();
+		OCLHelper<EClassifier, EOperation, EStructuralFeature, Constraint> helper = ocl.createOCLHelper();
+		
+		helper.setContext(inputMetaClass);
+
+		OCLExpression<EClassifier> expression;
+		try {
+			expression = helper.createQuery(expressionString);
+			Query query = ocl.createQuery(expression);
+			//Object result = query.evaluate(next);
+			return true;
+		} catch (ParserException e) {
+			//e.printStackTrace();
+			return false;
+		}
+	}
+
+	public IResourcePostProcessor getResourcePostProcessor() {
+		return this;
+	}
+
+	public Map<?, ?> getOptions() {
+		Map<String, Object> options = new HashMap<String, Object>();
+		options.put(IOptions.RESOURCE_POSTPROCESSOR_PROVIDER, this);
+		return options;
+	}
+}
