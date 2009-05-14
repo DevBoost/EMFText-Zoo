e64a1ed41d78c13df55ede4700ea59e4a4e81acd
Mirko Seifert <mirko.seifert@devboost.de>
2009-05-14 22:25:46 +0000

    fixed references

--- org.emftext.language.java_templates/metamodel/java_templates.cs
+++ org.emftext.language.java_templates/metamodel/java_templates.cs
@@ -13,48 +13,50 @@ OPTIONS {
 }
 
 TOKENSTYLES {
-	"<%TEMPLATE INPUT=" COLOR #C00000, BOLD;
+	"<%TEMPLATE" COLOR #C00000, BOLD; 
+	"INPUT=" COLOR #C00000, BOLD;
 	"<%IF" COLOR #C00000, BOLD;
 	"<%FOR" COLOR #C00000, BOLD;
 	"<%=" COLOR #C00000, BOLD;
 	"%>" COLOR #C00000, BOLD;
 	"<%ENDFOR%>" COLOR #C00000, BOLD;
 	"<%ENDIF%>" COLOR #C00000, BOLD;
+	"SL_COMMENT" COLOR #00A000, ITALIC;
 }
 
 RULES {
-	JavaTemplate ::= "<%TEMPLATE INPUT=" inputMetaClass[STRING_LITERAL] "%>" body;
+	JavaTemplate ::= "<%TEMPLATE" "INPUT=" inputMetaClass[STRING_LITERAL] "%>" body;
 	
 	PhNamedElementName ::= "<%=" expression[STRING_LITERAL] "%>";
 	
-	IfAnnotationParameter ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfExpression ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfArrayInitializationValue ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForArrayInitializationValue ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	ForExpression ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfTypeReference ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForTypeReference ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfConcreteClassifier ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForConcreteClassifier ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfConditionalExpressionChild ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfTypeArgument ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForTypeArgument ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfImport ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForImport ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfSelf ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfMember ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForMember ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfAnnotationInstanceOrModifier ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForAnnotationInstanceOrModifier ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfModifier ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForModifier ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfParameter ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForParameter ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfReference ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfReferenceableElement ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfStatement ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForStatement ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfForLoopInitializer ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfSwitchCase ::= "<%IF" condition[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForSwitchCase ::= "<%FOR" collection[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfAnnotationParameter ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfArrayInitializationValue ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForArrayInitializationValue ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	ForExpression ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfTypeReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForTypeReference ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfConcreteClassifier ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForConcreteClassifier ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfConditionalExpressionChild ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfTypeArgument ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForTypeArgument ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfImport ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForImport ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfSelf ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfMember ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForMember ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfAnnotationInstanceOrModifier ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForAnnotationInstanceOrModifier ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfModifier ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForModifier ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfParameter ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForParameter ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfReferenceableElement ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfStatement ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForStatement ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfForLoopInitializer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfSwitchCase ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForSwitchCase ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
 }
