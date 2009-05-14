bbc3b1e9cc8d3caddd993fa06cbbc795a0922259
Mirko Seifert <mirko.seifert@devboost.de>
2009-05-14 07:57:12 +0000

    fixed compile error cause by renamings

--- org.emftext.language.sandwich.resource.sandwich/src/org/emftext/language/sandwich/resource/sandwich/analysis/InstructionUsingReferenceResolver.java
+++ org.emftext.language.sandwich.resource.sandwich/src/org/emftext/language/sandwich/resource/sandwich/analysis/InstructionUsingReferenceResolver.java
@@ -3,29 +3,29 @@ package org.emftext.language.sandwich.resource.sandwich.analysis;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
-import org.emftext.language.sandwich.AttIngredientName;
+import org.emftext.language.sandwich.FeatureIngredientName;
 import org.emftext.language.sandwich.Ingredient;
 import org.emftext.language.sandwich.IngredientName;
 import org.emftext.language.sandwich.Instruction;
-import org.emftext.language.sandwich.RefInstructionUsing;
+import org.emftext.language.sandwich.FeatureInstructionUsing;
 import org.emftext.runtime.resource.IReferenceResolveResult;
 
-public class InstructionUsingReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.emftext.language.sandwich.Instruction, org.emftext.language.sandwich.RefInstructionUsing> {
+public class InstructionUsingReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.emftext.language.sandwich.Instruction, org.emftext.language.sandwich.FeatureInstructionUsing> {
 	
 	@Override	
-	protected java.lang.String doDeResolve(org.emftext.language.sandwich.RefInstructionUsing element, org.emftext.language.sandwich.Instruction container, org.eclipse.emf.ecore.EReference reference) {
+	protected java.lang.String doDeResolve(org.emftext.language.sandwich.FeatureInstructionUsing element, org.emftext.language.sandwich.Instruction container, org.eclipse.emf.ecore.EReference reference) {
 		return super.doDeResolve(element, container, reference);
 	}
 	
 	@Override	
-	protected void doResolve(String identifier, Instruction container, EReference reference, int position, boolean resolveFuzzy, IReferenceResolveResult<RefInstructionUsing> result) {
+	protected void doResolve(String identifier, Instruction container, EReference reference, int position, boolean resolveFuzzy, IReferenceResolveResult<FeatureInstructionUsing> result) {
 		result.setErrorMessage("Ingredient " + identifier + " can not be found.");
 		TreeIterator<EObject> it = container.eResource().getAllContents();
 		while (it.hasNext()) {
 			EObject next = it.next();
 			if (next instanceof Ingredient) {
 				Ingredient ingredient = (Ingredient) next;
-				AttIngredientName attName = ingredient.getName();
+				FeatureIngredientName attName = ingredient.getName();
 				if (attName == null) {
 					continue;
 				}
