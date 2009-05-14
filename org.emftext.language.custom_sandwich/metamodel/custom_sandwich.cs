c19bb5130050b58a4356d55cc29a6b54c61db947
Mirko Seifert <mirko.seifert@devboost.de>
2009-05-14 14:45:06 +0000

    adjusted metamodel and syntax to the one in the paper

--- org.emftext.language.custom_sandwich/metamodel/custom_sandwich.cs
+++ org.emftext.language.custom_sandwich/metamodel/custom_sandwich.cs
@@ -19,6 +19,7 @@ TOKENSTYLES {
 	"<%FOR" COLOR #C00000, BOLD;
 	"<%=" COLOR #C00000, BOLD;
 	"%>" COLOR #C00000, BOLD;
+	"<%ELSE%>" COLOR #C00000, BOLD;
 	"<%ENDFOR%>" COLOR #C00000, BOLD;
 	"<%ENDIF%>" COLOR #C00000, BOLD;
 	"QUOTED_34_34" COLOR #C08000, BOLD;
@@ -26,7 +27,10 @@ TOKENSTYLES {
 
 RULES {
 	RecipeTemplate ::= "<%TEMPLATE INPUT=" inputMetaClass['"','"'] "%>" body;
-	IfRefRecipeIngredients ::= "<%IF" expression['"','"'] "%>" body "<%ENDIF%>";
-	ForRefRecipeIngredients ::= "<%FOR" expression['"','"'] "%>" body "<%ENDFOR%>";
-	PhAttIngredientName ::= "<%=" expression['"','"'] "%>";
+	IfFeatureRecipeIngredients ::= "<%IF" expression['"','"'] "%>" body "<%ENDIF%>";
+	ForFeatureRecipeIngredients ::= "<%FOR" expression['"','"'] "%>" body "<%ENDFOR%>";
+	IfFeatureRecipeInstructions ::= "<%IF" expression['"','"'] "%>" body "<%ENDIF%>";
+	ForFeatureRecipeInstructions ::= "<%FOR" expression['"','"'] "%>" body "<%ENDFOR%>";
+	IfElseFeatureInstructionUsing ::= "<%IF" expression['"','"'] "%>" thenBody "<%ELSE%>" elseBody "<%ENDIF%>";
+	PhFeatureIngredientName ::= "<%=" expression['"','"'] "%>";
 }
