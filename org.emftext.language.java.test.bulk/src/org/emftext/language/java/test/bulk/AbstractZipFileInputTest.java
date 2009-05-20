ceb25304e1b3dfa590752966225ddb7aa9afc890
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-05-20 13:53:29 +0000

    individual rt.jar for each bulk test

--- org.emftext.language.java.test.bulk/src/org/emftext/language/java/test/bulk/AbstractZipFileInputTest.java
+++ org.emftext.language.java.test.bulk/src/org/emftext/language/java/test/bulk/AbstractZipFileInputTest.java
@@ -99,7 +99,7 @@ public abstract class AbstractZipFileInputTest extends AbstractJavaParserTestCas
 		if (!zipFilePath.endsWith("jdt_test_files" + File.separator + "src.zip")) {
 			ResourceSet dummyRS = new ResourceSetImpl();
 			dummyRS.getLoadOptions().put(JavaClasspath.OPTION_USE_LOCAL_CLASSPATH, Boolean.TRUE);
-			dummyRS.getLoadOptions().put(JavaClasspath.OPTION_REGISTER_STD_LIB, Boolean.TRUE);
+			dummyRS.getLoadOptions().put(JavaClasspath.OPTION_REGISTER_STD_LIB, Boolean.FALSE);
 			String plainZipFileName = zipFile.getName().substring(AbstractZipFileInputTest.BULK_INPUT_DIR.length());
 			plainZipFileName = plainZipFileName.substring(0, plainZipFileName.length() - File.separator.length() - "src.zip".length());
 			registerLibs("input/" + plainZipFileName, dummyRS, "");
@@ -107,8 +107,12 @@ public abstract class AbstractZipFileInputTest extends AbstractJavaParserTestCas
 			packageClassifierMap = JavaClasspath.get(dummyRS).getPackageClassifierMap();
 		}
 		else {
-			uriMap = Collections.emptyMap();
-			packageClassifierMap = Collections.emptyMap();
+			//for the JDT test file register only the std. lib
+			ResourceSet dummyRS = new ResourceSetImpl();
+			dummyRS.getLoadOptions().put(JavaClasspath.OPTION_USE_LOCAL_CLASSPATH, Boolean.TRUE);
+			dummyRS.getLoadOptions().put(JavaClasspath.OPTION_REGISTER_STD_LIB, Boolean.TRUE);
+			uriMap = dummyRS.getURIConverter().getURIMap();
+			packageClassifierMap = JavaClasspath.get(dummyRS).getPackageClassifierMap();
 		}
 		
 		
