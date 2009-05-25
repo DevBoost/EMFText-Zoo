3e1cbb931867a8303b45c1a01cb6b0b01a7b26eb
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-05-25 13:37:57 +0000

    fixed bug that occurred when Package and not CU is root element of a file (package-info.java files)

--- org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/decider/PackageDecider.java
+++ org.emftext.language.java.resource.java/src/org/emftext/language/java/resource/java/analysis/decider/PackageDecider.java
@@ -5,8 +5,8 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.emftext.language.java.commons.NamedElement;
-import org.emftext.language.java.containers.CompilationUnit;
 import org.emftext.language.java.containers.ContainersFactory;
+import org.emftext.language.java.containers.JavaRoot;
 import org.emftext.language.java.containers.Package;
 import org.emftext.language.java.references.IdentifierReference;
 import org.emftext.language.java.references.MethodCall;
@@ -40,7 +40,7 @@ public class PackageDecider extends AbstractDecider {
 	}
 	
 	public EList<? extends EObject> getAdditionalCandidates(String identifier, EObject container)  {
-		if (container instanceof CompilationUnit) {
+		if (container instanceof JavaRoot && container.eResource() != null) {
 			EList<EObject> resultList = new BasicEList<EObject>();
 			
 			Package p = ContainersFactory.eINSTANCE.createPackage();
