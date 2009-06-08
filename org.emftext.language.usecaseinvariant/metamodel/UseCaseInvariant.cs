307b9a73c6dda047049d82d131e955db46621426
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-06-08 13:49:08 +0000

    language for defining use cases with value invariants

---
+++ org.emftext.language.usecaseinvariant/metamodel/UseCaseInvariant.cs
@@ -0,0 +1,41 @@
+SYNTAXDEF ucinv
+FOR <http://www.emftext.org/language/UseCaseInvariant>
+START UseCaseModel
+
+OPTIONS {
+	reloadGeneratorModel = "true";
+	overridePluginXML = "false";
+	tokenspace = "1";
+}
+
+TOKENSTYLES {
+	"TEXT" COLOR #000000;
+}
+
+RULES {
+
+UseCaseModel 
+   ::=
+        "invariants" "for" name[] ":" (!1 invariants)* !0 "."
+	;
+
+NormalActor
+   ::=
+        "actor" name[] ":" "[" (valuesBefore (#0 "," valuesBefore)*)? "]" "-->" "[" (valuesAfter (#0 "," valuesAfter)*)? "]" "."
+	;
+
+CounterActor
+   ::=
+        "counter" name[] ":" "[" (valuesBefore (#0 "," valuesBefore)*)? "]" "-->" "[" (valuesAfter (#0 "," valuesAfter)*)? "]" "."
+	;
+
+SingleValue 
+   ::=
+        type[] ("(" #0 ID[] #0 ")")?
+	;
+
+SetOfValues
+   ::=
+        "set" "of" type[] ("excluding" excluding)? ("including" including)?
+	;
+}
