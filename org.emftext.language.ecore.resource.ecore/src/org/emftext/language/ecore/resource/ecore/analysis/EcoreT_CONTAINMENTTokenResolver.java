928c1e0319968a7359c782cf7e2e1c4cb26c387e
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-06-12 17:08:23 +0000

    removed old ecore syntax plugins

--- org.emftext.language.ecore.resource.ecore/src/org/emftext/language/ecore/resource/ecore/analysis/EcoreT_CONTAINMENTTokenResolver.java
+++
@@ -1,7 +0,0 @@
-package org.emftext.language.ecore.resource.ecore.analysis;
-
-import org.emftext.language.ecore.resource.ecore.analysis.helper.BooleanAttributeResolver;
-
-public class EcoreT_CONTAINMENTTokenResolver extends BooleanAttributeResolver {
-
-}
