f6dfb189033eee51d637cf262328340380a76838
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-06-12 16:04:25 +0000

    text.ecore: generated artifacts adjusted to new runitme

--- org.emftext.language.ecore.resource.text/src/org/emftext/language/ecore/resource/text/analysis/EGenericTypeEClassifierReferenceResolver.java
+++ org.emftext.language.ecore.resource.text/src/org/emftext/language/ecore/resource/text/analysis/EGenericTypeEClassifierReferenceResolver.java
@@ -1,16 +1,39 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2009 
+ * Software Technology Group, Dresden University of Technology
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * See the GNU Lesser General Public License for more details. You should have
+ * received a copy of the GNU Lesser General Public License along with this
+ * program; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA  02111-1307 USA
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany 
+ *   - initial API and implementation
+ ******************************************************************************/
 package org.emftext.language.ecore.resource.text.analysis;
+
+import java.util.Map;
 
 import org.emftext.language.ecore.resource.text.analysis.helper.EMFTypesResolver;
 
 public class EGenericTypeEClassifierReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.eclipse.emf.ecore.EGenericType, org.eclipse.emf.ecore.EClassifier> {
 	
-	@Override	
-	protected java.lang.String doDeResolve(org.eclipse.emf.ecore.EClassifier element, org.eclipse.emf.ecore.EGenericType container, org.eclipse.emf.ecore.EReference reference) {
-		return super.doDeResolve(element, container, reference);
+	public java.lang.String deResolve(org.eclipse.emf.ecore.EClassifier element, org.eclipse.emf.ecore.EGenericType container, org.eclipse.emf.ecore.EReference reference) {
+		return element.getName();
 	}
 	
-	@Override	
-	protected void doResolve(java.lang.String identifier, org.eclipse.emf.ecore.EGenericType container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IReferenceResolveResult<org.eclipse.emf.ecore.EClassifier> result) {
+	public void resolve(java.lang.String identifier, org.eclipse.emf.ecore.EGenericType container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IReferenceResolveResult<org.eclipse.emf.ecore.EClassifier> result) {
 		new EMFTypesResolver().doResolve(identifier, container.eResource(), reference, position, resolveFuzzy, result);
+	}
+
+	public void setOptions(Map<?, ?> options) {
 	}
 }
