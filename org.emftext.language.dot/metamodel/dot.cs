1327bd274fd185d39c7e7de91ae23a117b222d56
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-16 19:36:48 +0000

    added DOT language (created by Sebastian Patschorke)

---
+++ org.emftext.language.dot/metamodel/dot.cs
@@ -0,0 +1,90 @@
+SYNTAXDEF dot
+FOR <http://www.emftext.org/language/dot>
+START Graph
+
+// see: http://www.graphviz.org/doc/info/lang.html
+
+OPTIONS {
+        reloadGeneratorModel = "true";
+        generateCodeFromGeneratorModel = "true";
+        overrideManifest = "false";
+        usePredefinedTokens = "false";
+        tokenspace = "1";
+}
+
+TOKENS {
+		DEFINE GRAPHTYPE $ (('g'|'G') ('r'|'R') ('a'|'A') ('p'|'P') ('h'|'H')) |
+		                   (('d'|'D') ('i'|'I') ('g'|'G') ('r'|'R') ('a'|'A') ('p'|'P') ('h'|'H')) $;
+
+		DEFINE STRICT $ ('s'|'S') ('t'|'T') ('r'|'R') ('i'|'I') ('c'|'C') ('t'|'T') $;
+
+		DEFINE SUBGRAPHTYPE $ ('s'|'S') ('u'|'U') ('b'|'B') ('g'|'G') ('r'|'R') ('a'|'A') ('p'|'P') ('h'|'H') $;
+		
+		DEFINE EDGEOP $ '->' | '--' $;
+		DEFINE COMPASS $ 'n' | 'ne' | 'e' | 'se' | 's' | 'sw' | 'w' | 'nw' | 'c' | '_' $;
+		
+		DEFINE CONTEXT $ (('g'|'G') ('r'|'R') ('a'|'A') ('p'|'P') ('h'|'H')) |
+		                 (('n'|'N') ('o'|'O') ('d'|'D') ('e'|'E')) |
+		                 (('e'|'E') ('d'|'D') ('g'|'G') ('e'|'E')) $;
+	
+	    DEFINE ID $ (('a'..'z'|'A'..'Z'|'_'|'\u0080'..'\u00ff')  ('0'..'9'|'a'..'z'|'A'..'Z'|'_'|'\u0080'..'\u00ff')*) |
+		            (('-')? ('.' ('0'..'9')+ ) | (('0'..'9')+ ('.' ('0'..'9')*)?)) |
+		            (('"' ('\\\"' | (~('"'|'\uffff')))* '"') ('+' ('"' ('\\\"' | (~('"'|'\uffff')))* '"'))*) |
+		            ('<' .* '>') $;                 
+                             
+	    DEFINE SL_COMMENT $ '//'(~('\n'|'\r'|'\uffff'))* $ COLLECT IN comments;
+	    DEFINE ML_COMMENT $ '/*'.*'*/'$ COLLECT IN comments;
+		DEFINE PREPROCESSOR $ '#' (~('\n'|'\r'))* $ COLLECT IN comments;
+		
+		                 
+		DEFINE WHITESPACE $ (' '|'\t'|'\f'|'\\\r'|'\\\n'|'\\\r\n') $;
+		DEFINE TEXT $ ('A'..'Z'|'a'..'z'|'0'..'9'|'_'|'-')+ $;
+		DEFINE LINEBREAKS $ ('\r\n'|'\r'|'\n') $;
+}
+
+TOKENSTYLES {
+   "GRAPHTYPE" COLOR #912A72, BOLD;
+   "STRICT" COLOR #912A72, BOLD;
+   "SUBGRAPHTYPE" COLOR #912A72, BOLD;
+   "EDGEOP" COLOR #912A72;
+   "COMPASS" COLOR #912A72;
+   "CONTEXT" COLOR #912A72, BOLD;
+   
+   "ID" COLOR #000000;
+   "TEXT" COLOR #000000;
+   "ML_COMMENT" COLOR #008000, ITALIC;
+   "SL_COMMENT" COLOR #000080, ITALIC;
+   "PREPROCESSOR" COLOR #A0A0A0;
+}
+
+
+RULES{
+		
+		Graph ::= (strict[STRICT])? type[GRAPHTYPE] (id[ID])? "{"  (statements)? "}" ;
+		
+		StatementList ::= statement (";")? (tail)? ; 
+		
+		NodeStatement ::=  node_id (attributes)? ;
+		
+		EdgeStatement ::= source target (attributes)? ;
+		
+		AttributeStatement ::= context[CONTEXT] attributes;
+		
+		AssignmentStatement ::= left[ID] "=" right[ID];
+		
+		Subgraph ::= ( type[SUBGRAPHTYPE] (id[ID])? )? "{" (statements)? "}" ;
+		
+		Target ::= operation[EDGEOP] target (next_target)? ;  
+		
+		NodeID ::= id[ID] (port)? ;
+		
+		AttributeList ::=  "[" list "]" (next)? ;
+		 
+		AList ::= attribute (",")? (tail)? ;
+		
+		Attribute ::= key[ID] ( "=" value[ID] )? ;
+		 
+		Port ::= ":" id[ID] (":" compass[COMPASS])? |
+		         ":" compass[COMPASS];
+		
+}
