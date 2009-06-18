7ff692635071924250311e6c3c45350425408faf
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-18 09:41:44 +0000

    added test project for DOT language

---
+++ org.emftext.language.dot.test/src/org/emftext/language/dot/test/BulkGraphTest.java
@@ -0,0 +1,10 @@
+package org.emftext.language.dot.test;
+
+import junit.framework.TestCase;
+
+public class BulkGraphTest extends TestCase {
+
+	public void testInputGraphs() {
+		// TODO mseifert
+	}
+}
