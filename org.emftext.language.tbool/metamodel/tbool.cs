1e6932f0c73568e4e99f3d9e25f55a069cf768d7
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-19 08:11:07 +0000

    added initial version of the templified BOOL language

---
+++ org.emftext.language.tbool/metamodel/tbool.cs
@@ -0,0 +1,33 @@
+SYNTAXDEF tbool
+FOR <http://www.emftext.org/language/tbool>
+START ClassTemplate
+
+IMPORTS {
+	bool : <http://www.emftext.org/language/bool> <../../org.emftext.language.bool/metamodel/bool.genmodel> 
+		WITH SYNTAX bool <../../org.emftext.language.bool/metamodel/bool.cs>
+}
+
+OPTIONS {
+	usePredefinedTokens = "false";
+	overrideManifest = "false";
+}
+
+TOKENSTYLES {
+	"<%TEMPLATE" COLOR #C00000, BOLD; 
+	"INPUT=" COLOR #C00000, BOLD;
+	"<%IF" COLOR #C00000, BOLD;
+	"<%FOR" COLOR #C00000, BOLD;
+	"<%=" COLOR #C00000, BOLD;
+	"%>" COLOR #C00000, BOLD;
+	"<%ENDFOR%>" COLOR #C00000, BOLD;
+	"<%ENDIF%>" COLOR #C00000, BOLD;
+	"SL_COMMENT" COLOR #00A000, ITALIC;
+}
+
+RULES {
+	ClassTemplate ::= "<%TEMPLATE" "INPUT=" inputMetaClass['"','"'] "%>" body;
+	IfElseTypedElement ::= "<%IF" expression['"','"'] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfElseNamedElement ::= "<%IF" expression['"','"'] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfClass ::= "<%IF" expression['"','"'] "%>" body "<%ENDIF%>";
+	ForClass ::= "<%FOR" expression['"','"'] "%>" body "<%ENDFOR%>";
+}
