31ef600340385b47392a3578f3fba7e6992c275f
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-19 19:20:44 +0000

    added second test to reproduce NPE in template interpreter

--- org.emftext.language.tbool.test/src/org/emftext/language/tbool/test/TBoolInterpretationTest.java
+++ org.emftext.language.tbool.test/src/org/emftext/language/tbool/test/TBoolInterpretationTest.java
@@ -7,8 +7,8 @@ import org.emftext.language.template_concepts.interpreter.test.AbstractInterpret
 public class TBoolInterpretationTest extends AbstractInterpreterTest {
 
 	public void testInterpretation() {
-		// basic test (input model is not used)
-		testInterpretation("test1.tbool", "test1.ecore", "public class MyClass { private MyClass attribute ; }");
+		testInterpretation("test1.tbool", "test1a.ecore", "public class MyClass { private MyClass attribute ; }");
+		testInterpretation("test1.tbool", "test1b.ecore", "public class MyClass { private MyClass attribute ; }");
 	}
 
 	protected void registerResourceFactories() {
