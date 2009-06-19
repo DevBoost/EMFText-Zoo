435f620b80a475b270398e2c743879a423540b64
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-19 19:18:39 +0000

    added test project for interpretation of TBOOL templates

---
+++ org.emftext.language.tbool.test/src/org/emftext/language/tbool/test/TBoolInterpretationTest.java
@@ -0,0 +1,35 @@
+package org.emftext.language.tbool.test;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.emftext.language.template_concepts.interpreter.test.AbstractInterpreterTest;
+
+public class TBoolInterpretationTest extends AbstractInterpreterTest {
+
+	public void testInterpretation() {
+		// basic test (input model is not used)
+		testInterpretation("test1.tbool", "test1.ecore", "public class MyClass { private MyClass attribute ; }");
+	}
+
+	protected void registerResourceFactories() {
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"bool",
+				new org.emftext.language.bool.resource.bool.BoolResourceFactory());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"tbool",
+				new org.emftext.language.tbool.resource.tbool.TboolResourceFactory());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				"ecore",
+				new EcoreResourceFactoryImpl());
+	}
+
+	@Override
+	protected String getInputFolder() {
+		return "input";
+	}
+
+	@Override
+	protected String getOutputFileExtension() {
+		return "bool";
+	}
+}
