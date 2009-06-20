90131b2af82c76369a8449bc1fd7826037e98266
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-20 14:08:05 +0000

    refined syntax for file systems

--- org.emftext.language.filesystem/metamodel/filesystem.cs
+++ org.emftext.language.filesystem/metamodel/filesystem.cs
@@ -2,11 +2,17 @@ SYNTAXDEF fs
 FOR <http://www.emftext.org/language/filesystem>
 START Folder
 
+OPTIONS {
+	usePredefinedTokens = "false";
+}
+
 TOKENS {
 	DEFINE COMMENT $'//'(~('\n'|'\r'))*$;
+	DEFINE WHITESPACE $(' '|'\t'|'\f')$;
+	DEFINE LINEBREAKS $('\r\n'|'\r'|'\n')$;
 }
 
 RULES {
-	Folder::= "Folder" "{" contents* "}" ;
-	PlainFile::= "PlainFile"  "{"  "}" ;
+	Folder    ::= "Folder" name['"','"'] "{" contents* "}" ;
+	PlainFile ::= "File" name['"','"'] ;
 }
