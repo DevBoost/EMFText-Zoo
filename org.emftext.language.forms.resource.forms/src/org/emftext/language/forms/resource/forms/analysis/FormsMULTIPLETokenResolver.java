1dc35f0c439963c52cc03c2fbc14df88b88f05c7
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-22 19:30:26 +0000

    removed unused token resolver

--- org.emftext.language.forms.resource.forms/src/org/emftext/language/forms/resource/forms/analysis/FormsMULTIPLETokenResolver.java
+++
@@ -1,7 +0,0 @@
-package org.emftext.language.forms.resource.forms.analysis;
-
-import org.emftext.language.forms.resource.forms.analysis.helper.BooleanAttributeResolver;
-
-public class FormsMULTIPLETokenResolver extends BooleanAttributeResolver {
-	
-}
