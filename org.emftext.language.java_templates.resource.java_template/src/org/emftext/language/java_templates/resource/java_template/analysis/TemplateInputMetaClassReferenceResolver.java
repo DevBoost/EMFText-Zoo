c81cf6abc915616acd21210503eabb9dcc8d6b10
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-26 15:19:03 +0000

    fixed error in template language due to the change of reference inputMetaclass

--- org.emftext.language.java_templates.resource.java_template/src/org/emftext/language/java_templates/resource/java_template/analysis/TemplateInputMetaClassReferenceResolver.java
+++ org.emftext.language.java_templates.resource.java_template/src/org/emftext/language/java_templates/resource/java_template/analysis/TemplateInputMetaClassReferenceResolver.java
@@ -22,17 +22,22 @@ package org.emftext.language.java_templates.resource.java_template.analysis;
 
 import java.util.Map;
 
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EReference;
 import org.emftext.language.template_concepts.InputMetaClassReferenceResolver;
+import org.emftext.language.template_concepts.Template;
+import org.emftext.runtime.resource.IReferenceResolveResult;
+import org.emftext.runtime.resource.impl.AbstractReferenceResolver;
 
-public class TemplateInputMetaClassReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.emftext.language.template_concepts.Template, org.eclipse.emf.ecore.EObject> {
+public class TemplateInputMetaClassReferenceResolver extends AbstractReferenceResolver<org.emftext.language.template_concepts.Template, EClass> {
 	
 	private InputMetaClassReferenceResolver resolverDelegate = new InputMetaClassReferenceResolver();
 
-	public java.lang.String deResolve(org.eclipse.emf.ecore.EObject element, org.emftext.language.template_concepts.Template container, org.eclipse.emf.ecore.EReference reference) {
+	public java.lang.String deResolve(EClass element, org.emftext.language.template_concepts.Template container, EReference reference) {
 		return resolverDelegate.deResolve(element, container, reference);
 	}
 	
-	public void resolve(java.lang.String identifier, org.emftext.language.template_concepts.Template container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IReferenceResolveResult<org.eclipse.emf.ecore.EObject> result) {
+	public void resolve(String identifier, Template container, EReference reference, int position, boolean resolveFuzzy, IReferenceResolveResult<EClass> result) {
 		resolverDelegate.resolve(identifier, container, reference, position, resolveFuzzy, result);
 	}
 
