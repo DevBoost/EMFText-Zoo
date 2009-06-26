d89350bbed8f8633b85ba9ce21f3fedf47fbde40
Mirko Seifert <mirko.seifert@devboost.de>
2009-06-26 14:10:26 +0000

    adjusted resolvers

--- org.emftext.language.bool.resource.bool/src/org/emftext/language/bool/resource/bool/analysis/FieldTypeReferenceResolver.java
+++ org.emftext.language.bool.resource.bool/src/org/emftext/language/bool/resource/bool/analysis/FieldTypeReferenceResolver.java
@@ -5,17 +5,17 @@ import java.util.Collection;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.emftext.language.bool.BoolPackage;
+import org.emftext.language.bool.FieldType;
 import org.emftext.language.bool.Name;
 import org.emftext.language.bool.NamedElementName;
-import org.emftext.language.bool.TypedElementType;
 import org.emftext.runtime.util.EObjectUtil;
 
-public class TypedElementTypeReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.emftext.language.bool.TypedElement, org.emftext.language.bool.TypedElementType> {
+public class FieldTypeReferenceResolver extends org.emftext.runtime.resource.impl.AbstractReferenceResolver<org.emftext.language.bool.Field, org.emftext.language.bool.FieldType> {
 	
-	public void resolve(java.lang.String identifier, org.emftext.language.bool.TypedElement container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IReferenceResolveResult<org.emftext.language.bool.TypedElementType> result) {
+	public void resolve(java.lang.String identifier, org.emftext.language.bool.Field container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, org.emftext.runtime.resource.IReferenceResolveResult<org.emftext.language.bool.FieldType> result) {
 		TreeIterator<EObject> iterator = container.eResource().getAllContents();
-		Collection<TypedElementType> typedElements = EObjectUtil.getObjectsByType(iterator, BoolPackage.eINSTANCE.getTypedElementType());
-		for (TypedElementType type : typedElements) {
+		Collection<FieldType> typedElements = EObjectUtil.getObjectsByType(iterator, BoolPackage.eINSTANCE.getFieldType());
+		for (FieldType type : typedElements) {
 			if (!(type instanceof org.emftext.language.bool.Class)) {
 				continue;
 			}
@@ -40,7 +40,7 @@ public class TypedElementTypeReferenceResolver extends org.emftext.runtime.resou
 		}
 	}
 	
-	public java.lang.String deResolve(org.emftext.language.bool.TypedElementType element, org.emftext.language.bool.TypedElement container, org.eclipse.emf.ecore.EReference reference) {
+	public java.lang.String deResolve(org.emftext.language.bool.FieldType element, org.emftext.language.bool.Field container, org.eclipse.emf.ecore.EReference reference) {
 		assert element instanceof org.emftext.language.bool.Class;
 		org.emftext.language.bool.Class clazz = (org.emftext.language.bool.Class) element;
 		NamedElementName nameRef = clazz.getName();
