c8ed96912358ae4dec07af613db5b0246c155b7b
Matthias Range <s9586336@mail.inf.tu-dresden.de>
2009-07-01 16:07:17 +0000

    SPARQL: add grammer-reference.txt for comments and overview add TOKENS for IRI_REF (predone), integer-types (predone) reduce some problems in the plugin manifest relocate some path-structures

--- org.emftext.language.sparql/metamodel/SPARQL.cs
+++ org.emftext.language.sparql/metamodel/SPARQL.cs
@@ -15,20 +15,29 @@ OPTIONS{
 }
 
 TOKENS{
-	DEFINE COMMENT$'//'(~('\n'|'\r'))*$;
-	DEFINE INTEGER$('-')?('1'..'9')('0'..'9')*|'0'$;
-	DEFINE FLOAT$('-')?(('1'..'9') ('0'..'9')* | '0') '.' ('0'..'9')+ $;
+	//DEFINE IRI_REF$'<' ( '^' | '<' | '>' | '"' | '{' | '}' | '^' | '`' | '\\' | ('\\''x'('0'..'2')('0'..'9'|'a'..'f'|'A'..'F'))* '>'$;
+	
+	DEFINE DEF_INTEGER$('+'|'-')?('0'..'9')+$;
+	
+	
+	//DEFINE COMMENT$'//'(~('\n'|'\r'))*$;
+	//DEFINE INTEGER$('-')?('1'..'9')('0'..'9')*|'0'$;
+	//DEFINE FLOAT$('-')?(('1'..'9') ('0'..'9')* | '0') '.' ('0'..'9')+ $;
 }
 
 RULES{
 	
-	SparqlQueries::=prologue query  ;
+	SparqlQueries::=  prologue;
+	//prologue query  ;
 	
-	Prologue::=basedeclaration? prefixdeclaration  ;
+	//Prologue::=basedeclaration? prefixdeclaration  ;
 	
-	BaseDecl::= "BASE" iriref  ;
+	//BaseDecl::= "BASE" iriref  ;
 	
-	PrefixDecl::= "PREFIX" pnamens iriref  ;
+	//PrefixDecl::= "PREFIX" pnamens iriref  ;
+	
+	
+	INTEGER::= integer[DEF_INTEGER];
 	
 	
 }
