2fe13282f1846e2bbd8f6003f78cf840b63e6b0f
Mirko Seifert <mirko.seifert@devboost.de>
2009-08-06 09:43:30 +0000

    fixed regular expressions (escaped backslashes)

--- org.emftext.language.models/metamodel/models.cs
+++ org.emftext.language.models/metamodel/models.cs
@@ -8,13 +8,13 @@ OPTIONS {
 }
 
 TOKENS {
-	DEFINE SL_COMMENT $'//'(~('\n'|'\r'|'\uffff'))* $ COLLECT IN comments;
+	DEFINE SL_COMMENT $'//'(~('\\n'|'\\r'|'\\uffff'))* $ COLLECT IN comments;
 	DEFINE ML_COMMENT $'/*'.*'*/'$ COLLECT IN comments;
 	DEFINE TEXT $('a'..'z')*$;
 	DEFINE NUMBER $('0'..'9')*$;
 
-	DEFINE WHITESPACE $(' '|'\t'|'\f')$;
-	DEFINE LINEBREAKS $('\r\n'|'\r'|'\n')$;
+	DEFINE WHITESPACE $(' '|'\\t'|'\\f')$;
+	DEFINE LINEBREAKS $('\\r\\n'|'\\r'|'\\n')$;
 }
 
 TOKENSTYLES {
