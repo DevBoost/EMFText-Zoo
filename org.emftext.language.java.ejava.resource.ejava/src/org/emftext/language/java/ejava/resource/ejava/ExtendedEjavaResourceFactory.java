739e371c2e3ff4f718b166e5e120a5c584585e76
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-08-14 09:18:57 +0000

    changed to custom impl. #1

---
+++ org.emftext.language.java.ejava.resource.ejava/src/org/emftext/language/java/ejava/resource/ejava/ExtendedEjavaResourceFactory.java
@@ -0,0 +1,12 @@
+package org.emftext.language.java.ejava.resource.ejava;
+
+public class ExtendedEjavaResourceFactory implements org.eclipse.emf.ecore.resource.Resource.Factory {
+	
+	public ExtendedEjavaResourceFactory() {
+		super();
+	}
+	
+	public org.eclipse.emf.ecore.resource.Resource createResource(org.eclipse.emf.common.util.URI uri) {
+		return new org.emftext.language.java.ejava.resource.ejava.ExtendedEjavaResource(uri);
+	}
+}
