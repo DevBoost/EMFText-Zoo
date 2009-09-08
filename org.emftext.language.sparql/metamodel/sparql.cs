1e8fe20da7e2d79cc7ccfaf02358e7faadbecb14
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-08 12:47:30 +0000

    fixed token that matched the empty string

--- org.emftext.language.sparql/metamodel/sparql.cs
+++ org.emftext.language.sparql/metamodel/sparql.cs
@@ -18,7 +18,7 @@ TOKENS{
 	
 	DEFINE DEF_IRI_REF $'<' (~( '^' | '<' | '>' | '"' | '{' | '}' | '`' | '\\' | '\u0000'..'\u0020' ))* '>'$;
 	
-	DEFINE DEF_INTEGER $('+'|'-')? ('0'..'9')*$;
+	DEFINE DEF_INTEGER $('+'|'-')? ('0'..'9')+$;
 	
 	//folgende 3 Definition sind in VARNAME, PN_PREFIX und PN_LOCAL eingearbeitet
 	// PN_CHARS_BASE $ ('A'..'Z' | 'a'..'z' | '\u00C0'..'\u00D6'| '\u00D8'..'\u00F6' | '\u00F8'..'\u02FF' | '\u0370'..'\u037D' | '\u037F'..'\u1FFF' | '\u200C'..'\u200D' | '\u2070'..'\u218F' | '\u2C00'..'\u2FEF' | '\u3001'..'\uD7FF' | '\uF900'..'\uFDCF' | '\uFDF0'..'\uFFFD' | '\u10000'..'\uEFFFF') $;
