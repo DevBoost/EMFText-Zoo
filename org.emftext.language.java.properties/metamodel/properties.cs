b5d8a1ebb6ab5b37a963fb7c75f30170363ca3c8
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-10 13:25:12 +0000

    added properties extension for Java

---
+++ org.emftext.language.java.properties/metamodel/properties.cs
@@ -0,0 +1,28 @@
+SYNTAXDEF propjava
+FOR <http://www.emftext.org/language/java/properties>
+START java.Containers.CompilationUnit
+
+IMPORTS {
+	java : <http://www.emftext.org/java> WITH SYNTAX java <../../org.emftext.language.java/metamodel/java.cs>
+}
+
+OPTIONS {
+	defaultTokenName = "IDENTIFIER";
+	usePredefinedTokens = "false";
+	srcFolder = "src-gen";
+	overrideManifest = "false";
+	overridePluginXML = "false";
+	overrideClasspath = "false";
+}
+
+TOKENS {
+	DEFINE T_READONLY $'readonly'$;
+}
+
+TOKENSTYLES {
+	"T_READONLY" COLOR #7F0055, BOLD;
+}
+
+RULES {
+	Property ::= ( readonly[T_READONLY] )? "property" typeReference name[];
+}
