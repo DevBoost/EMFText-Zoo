c963a89be30e96675431f7f3ee184e244bba2d42
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-10 13:28:45 +0000

    added examples project for properties extension for Java

---
+++ org.emftext.language.java.properties.examples/src/example_package/Simple.java
@@ -0,0 +1,16 @@
+package example_package ; 
+
+
+public class Simple { 
+	private Object p1 ; 
+	; 
+	private Object p2 ; 
+	; 
+	Object getP1 () {
+		return null;
+	} 
+	Object getP2 () { 
+		return null;
+	} 
+} 
+
