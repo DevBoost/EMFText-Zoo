bf842c283dd2de51af38e5524d539f077b00f7aa
Christian Werner <s7436532@mail.inf.tu-dresden.de>
2009-09-19 15:25:08 +0000

    Metamodel for Projects (example was a Toy Train). There are extentions to use it with annotations.

---
+++ org.emftext.language.project/metamodel/project.cs
@@ -0,0 +1,41 @@
+ SYNTAXDEF project
+FOR <http://org.emftext/project>
+START Project
+
+OPTIONS{
+    generateCodeFromGeneratorModel = "true";
+    reloadGeneratorModel = "true";
+}
+
+TOKENS{
+	DEFINE COMMENT$'//'(~('\n'|'\r'))*$;
+}
+
+TOKENSTYLES{
+	"Train" COLOR #7F0055, BOLD;
+	"P" COLOR #7F0055, BOLD;
+	"T" COLOR #7F0055, BOLD;
+	"S" COLOR #7F0055, BOLD;
+	"C" COLOR #7F0055, BOLD;
+	"O" COLOR #7F0055, BOLD;
+	"I" COLOR #7F0055, BOLD;
+	"->" COLOR #7F0055, BOLD;
+}
+
+RULES{
+	
+	Train::= "Train" (name['"','"'])? ; 
+	
+	Project::= "P" "{" (name['"','"'])? "(" (components)* ")" "(" (connections)* ")" "}";
+	
+	Track::= "T" "{" (name['"','"'])? "(" (ports)* ")" "(" (trains)* ")" "}" ;
+	
+	Switch::= "S" "{" (name['"','"'])? "(" (ports)* ")" "(" (trains)* ")" "}" ;
+	
+	Connection::= "C" (name['"','"'])? source[] "->" target[] ;
+	
+	Out::= "O" (name['"','"'])?  ;
+	
+	In::= "I" (name['"','"'])? ;
+	
+}
