4ea4133198ac48a3331d709f6e1e915c575846ad
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-21 12:43:56 +0000

    regenerated with new SDK adjusted ignore list

--- org.emftext.language.bool.resource.bool/src/org/emftext/language/bool/resource/bool/analysis/BoolTEXTTokenResolver.java
+++ org.emftext.language.bool.resource.bool/src/org/emftext/language/bool/resource/bool/analysis/BoolTEXTTokenResolver.java
@@ -1,15 +1,23 @@
 package org.emftext.language.bool.resource.bool.analysis;
 
-public class BoolTEXTTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
+import java.util.Map;
+
+import org.emftext.language.bool.resource.bool.IBoolTokenResolveResult;
+import org.emftext.language.bool.resource.bool.IBoolTokenResolver;
+
+public class BoolTEXTTokenResolver implements IBoolTokenResolver {
 	
-	@Override	
+	private BoolDefaultTokenResolver defaultResolver = new BoolDefaultTokenResolver();
+	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
+		java.lang.String result = defaultResolver.deResolve(value, feature, container);
 		return result;
 	}
 	
-	@Override	
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
-		super.resolve(lexem, feature, result);
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, IBoolTokenResolveResult result) {
+		defaultResolver.resolve(lexem, feature, result);
+	}
+
+	public void setOptions(Map<?, ?> options) {
 	}
 }
