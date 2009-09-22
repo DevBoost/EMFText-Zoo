360e594b92ef6dcf58308f23d6f4373a60cda036
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-09-22 16:14:30 +0000

    regenerated everything

--- org.emftext.language.dot.resource.dot/src/org/emftext/language/dot/resource/dot/analysis/DotTEXTTokenResolver.java
+++ org.emftext.language.dot.resource.dot/src/org/emftext/language/dot/resource/dot/analysis/DotTEXTTokenResolver.java
@@ -1,15 +1,20 @@
 package org.emftext.language.dot.resource.dot.analysis;
 
-public class DotTEXTTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
+public class DotTEXTTokenResolver implements org.emftext.language.dot.resource.dot.IDotTokenResolver {
 	
-	@Override	
+	private org.emftext.language.dot.resource.dot.analysis.DotDefaultTokenResolver defaultTokenResolver = new org.emftext.language.dot.resource.dot.analysis.DotDefaultTokenResolver();
+	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
+		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
 		return result;
 	}
 	
-	@Override	
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
-		super.resolve(lexem, feature, result);
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.dot.resource.dot.IDotTokenResolveResult result) {
+		defaultTokenResolver.resolve(lexem, feature, result);
 	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
 }
