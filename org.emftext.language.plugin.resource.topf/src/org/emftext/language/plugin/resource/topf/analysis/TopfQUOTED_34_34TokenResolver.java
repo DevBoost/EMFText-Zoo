dc11b02d01ebb9fc7c18d94dc9cd94b5d85bc9ef
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-22 18:37:18 +0000

    regenerated with latest SDK fixed resolvers

--- org.emftext.language.plugin.resource.topf/src/org/emftext/language/plugin/resource/topf/analysis/TopfQUOTED_34_34TokenResolver.java
+++ org.emftext.language.plugin.resource.topf/src/org/emftext/language/plugin/resource/topf/analysis/TopfQUOTED_34_34TokenResolver.java
@@ -1,21 +1,29 @@
 package org.emftext.language.plugin.resource.topf.analysis;
 
-public class TopfQUOTED_34_34TokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
+import java.util.Map;
+
+import org.emftext.language.plugin.resource.topf.ITopfTokenResolveResult;
+import org.emftext.language.plugin.resource.topf.ITopfTokenResolver;
+
+public class TopfQUOTED_34_34TokenResolver implements ITopfTokenResolver {
 	
-	@Override	
+	private TopfDefaultTokenResolver defaultResolver = new TopfDefaultTokenResolver();
+	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
+		java.lang.String result = defaultResolver.deResolve(value, feature, container);
 		result = result.replaceAll(java.util.regex.Pattern.quote("\""),"\\\\\"");
 		result += "\"";
 		result = "\"" + result;
 		return result;
 	}
 	
-	@Override	
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, ITopfTokenResolveResult result) {
 		lexem = lexem.substring(1);
 		lexem = lexem.substring(0, lexem.length() - 1);
 		lexem = lexem.replaceAll("\\\\"+java.util.regex.Pattern.quote("\""),"\"");
-		super.resolve(lexem, feature, result);
+		defaultResolver.resolve(lexem, feature, result);
+	}
+
+	public void setOptions(Map<?, ?> options) {
 	}
 }
--- org.emftext.language.plugin.resource.topf/src/org/emftext/language/plugin/resource/topf/analysis/TopfTEXTTokenResolver.java
+++ org.emftext.language.plugin.resource.topf/src/org/emftext/language/plugin/resource/topf/analysis/TopfTEXTTokenResolver.java
@@ -1,15 +1,23 @@
 package org.emftext.language.plugin.resource.topf.analysis;
 
-public class TopfTEXTTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
+import java.util.Map;
+
+import org.emftext.language.plugin.resource.topf.ITopfTokenResolveResult;
+import org.emftext.language.plugin.resource.topf.ITopfTokenResolver;
+
+public class TopfTEXTTokenResolver implements ITopfTokenResolver {
 	
-	@Override	
+	private TopfDefaultTokenResolver defaultResolver = new TopfDefaultTokenResolver();
+	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
+		java.lang.String result = defaultResolver.deResolve(value, feature, container);
 		return result;
 	}
 	
-	@Override	
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
-		super.resolve(lexem, feature, result);
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, ITopfTokenResolveResult result) {
+		defaultResolver.resolve(lexem, feature, result);
+	}
+
+	public void setOptions(Map<?, ?> options) {
 	}
 }
