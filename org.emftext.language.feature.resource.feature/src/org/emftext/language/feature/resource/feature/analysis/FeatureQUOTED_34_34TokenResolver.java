f3f984ff40888e712f0e1e9edf88a52229ba069b
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-09-22 16:40:06 +0000

    regenerated everything

--- org.emftext.language.feature.resource.feature/src/org/emftext/language/feature/resource/feature/analysis/FeatureQUOTED_34_34TokenResolver.java
+++ org.emftext.language.feature.resource.feature/src/org/emftext/language/feature/resource/feature/analysis/FeatureQUOTED_34_34TokenResolver.java
@@ -1,41 +1,24 @@
-/*******************************************************************************
- * Copyright (c) 2006-2009 
- * Software Technology Group, Dresden University of Technology
- * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option) any
- * later version. This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * See the GNU Lesser General Public License for more details. You should have
- * received a copy of the GNU Lesser General Public License along with this
- * program; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA  02111-1307 USA
- * 
- * Contributors:
- *   Software Technology Group - TU Dresden, Germany 
- *   - initial API and implementation
- ******************************************************************************/
 package org.emftext.language.feature.resource.feature.analysis;
 
-public class FeatureQUOTED_34_34TokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
+public class FeatureQUOTED_34_34TokenResolver implements org.emftext.language.feature.resource.feature.IFeatureTokenResolver {
 	
-	@Override	
+	private org.emftext.language.feature.resource.feature.analysis.FeatureDefaultTokenResolver defaultTokenResolver = new org.emftext.language.feature.resource.feature.analysis.FeatureDefaultTokenResolver();
+	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
-		result = result.replaceAll(java.util.regex.Pattern.quote("\""),"\\\\\"");
+		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
 		result += "\"";
 		result = "\"" + result;
 		return result;
 	}
 	
-	@Override	
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.feature.resource.feature.IFeatureTokenResolveResult result) {
 		lexem = lexem.substring(1);
 		lexem = lexem.substring(0, lexem.length() - 1);
-		lexem = lexem.replaceAll("\\\\"+java.util.regex.Pattern.quote("\""),"\"");
-		super.resolve(lexem, feature, result);
+		defaultTokenResolver.resolve(lexem, feature, result);
 	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
 }
