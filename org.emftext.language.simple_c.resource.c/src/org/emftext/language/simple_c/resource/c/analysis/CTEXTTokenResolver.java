62e5a0078d0405aea41c538fa1aaafbf2d12a378
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-22 18:51:33 +0000

    regenerated with latest SDK fixed resolvers

--- org.emftext.language.simple_c.resource.c/src/org/emftext/language/simple_c/resource/c/analysis/CTEXTTokenResolver.java
+++ org.emftext.language.simple_c.resource.c/src/org/emftext/language/simple_c/resource/c/analysis/CTEXTTokenResolver.java
@@ -20,15 +20,24 @@
  ******************************************************************************/
 package org.emftext.language.simple_c.resource.c.analysis;
 
-public class CTEXTTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
-	@Override
+import java.util.Map;
+
+import org.emftext.language.simple_c.resource.c.ICTokenResolveResult;
+import org.emftext.language.simple_c.resource.c.ICTokenResolver;
+
+public class CTEXTTokenResolver implements ICTokenResolver {
+	
+	private CDefaultTokenResolver defaultResolver = new CDefaultTokenResolver();
+
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
+		java.lang.String result = defaultResolver.deResolve(value, feature, container);
 		return result;
 	}
 
-	@Override
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
-		super.resolve(lexem, feature, result);
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, ICTokenResolveResult result) {
+		defaultResolver.resolve(lexem, feature, result);
+	}
+
+	public void setOptions(Map<?, ?> options) {
 	}
 }
