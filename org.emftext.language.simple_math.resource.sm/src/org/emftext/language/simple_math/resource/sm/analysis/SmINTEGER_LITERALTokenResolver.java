1d383eb1cbd1abbf7e3fd2c2f58330c51290f839
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-23 13:24:15 +0000

    regenerated with new SDK fixed resolvers

--- org.emftext.language.simple_math.resource.sm/src/org/emftext/language/simple_math/resource/sm/analysis/SmINTEGER_LITERALTokenResolver.java
+++ org.emftext.language.simple_math.resource.sm/src/org/emftext/language/simple_math/resource/sm/analysis/SmINTEGER_LITERALTokenResolver.java
@@ -1,34 +1,20 @@
-/*******************************************************************************
- * Copyright (c) 2006-2009 
- * Software Technology Group, Dresden University of Technology
- * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option) any
- * later version. This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * 
- * See the GNU Lesser General Public License for more details. You should have
- * received a copy of the GNU Lesser General Public License along with this
- * program; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA  02111-1307 USA
- * 
- * Contributors:
- *   Software Technology Group - TU Dresden, Germany 
- *   - initial API and implementation
- ******************************************************************************/
 package org.emftext.language.simple_math.resource.sm.analysis;
 
-public class SmINTEGER_LITERALTokenResolver extends org.emftext.runtime.resource.impl.JavaBasedTokenResolver implements org.emftext.runtime.resource.ITokenResolver {
-	@Override
+public class SmINTEGER_LITERALTokenResolver implements org.emftext.language.simple_math.resource.sm.ISmTokenResolver {
+	
+	private org.emftext.language.simple_math.resource.sm.analysis.SmDefaultTokenResolver defaultTokenResolver = new org.emftext.language.simple_math.resource.sm.analysis.SmDefaultTokenResolver();
+	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = super.deResolve(value, feature, container);
+		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
 		return result;
 	}
-
-	@Override
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.runtime.resource.ITokenResolveResult result) {
-		super.resolve(lexem, feature, result);
+	
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.simple_math.resource.sm.ISmTokenResolveResult result) {
+		defaultTokenResolver.resolve(lexem, feature, result);
 	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
 }
