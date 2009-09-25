6b53d259e7222eb772057c37399df9f3b8499527
Mirko Seifert <mirko.seifert@devboost.de>
2009-09-25 20:18:08 +0000

    regenerated with latest SDK

---
+++ org.emftext.language.java.properties.resource.propjava/src/org/emftext/language/java/properties/resource/propjava/analysis/JavaBOOLEAN_LITERALTokenResolver.java
@@ -0,0 +1,37 @@
+package org.emftext.language.java.properties.resource.propjava.analysis;
+
+public class JavaBOOLEAN_LITERALTokenResolver implements org.emftext.language.java.properties.resource.propjava.IPropjavaTokenResolver {
+	
+	private org.emftext.language.java.resource.java.analysis.JavaBOOLEAN_LITERALTokenResolver importedResolver = new org.emftext.language.java.resource.java.analysis.JavaBOOLEAN_LITERALTokenResolver();
+	
+	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		java.lang.String result = importedResolver.deResolve(value, feature, container);
+		return result;
+	}
+	
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, final org.emftext.language.java.properties.resource.propjava.IPropjavaTokenResolveResult result) {
+		importedResolver.resolve(lexem, feature, new org.emftext.language.java.resource.java.IJavaTokenResolveResult() {
+			public String getErrorMessage() {
+				return result.getErrorMessage();
+			}
+			
+			public Object getResolvedToken() {
+				return result.getResolvedToken();
+			}
+			
+			public void setErrorMessage(String message) {
+				result.setErrorMessage(message);
+			}
+			
+			public void setResolvedToken(Object resolvedToken) {
+				result.setResolvedToken(resolvedToken);
+			}
+			
+		});
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		importedResolver.setOptions(options);
+	}
+	
+}
