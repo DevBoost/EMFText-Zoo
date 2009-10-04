f9356187325d84a1e8e2607a19dd13d62cd1ee01
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-10-04 19:46:24 +0000

    reverted accidental change

--- org.emftext.language.java.test/input/StaticImports.java
+++ org.emftext.language.java.test/input/StaticImports.java
@@ -1,8 +1,5 @@
-import static pkg.EscapedStrings.escapes1;
-import static pkg.EscapedStrings.escapes2;
-import static pkg.EscapedStrings.m1;
+import static pkg.EscapedStrings.*;
 import pkg.EmptyClass;
-import pkg.EscapedStrings.C1;
 
 public class StaticImports {
 	{
