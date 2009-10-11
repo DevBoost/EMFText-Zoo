081258be8159357fc6a054bf77362a30b91fd395
Mirko Seifert <mirko.seifert@devboost.de>
2009-10-11 13:35:20 +0000

    added JUnit test Riehle example model

---
+++ org.emftext.language.rolecore.examples/src-test/org/emftext/language/rolecore/examples/riehle97/RiehleTest.java
@@ -0,0 +1,29 @@
+package org.emftext.language.rolecore.examples.riehle97;
+
+import org.eclipse.emf.ecore.EClass;
+
+import junit.framework.TestCase;
+
+public class RiehleTest extends TestCase {
+
+	public void testRoleHandling() {
+		// create a clean core object
+		Customer fred = Riehle97Factory.eINSTANCE.createCustomerCore();
+		boolean isBorrower = fred.hasRole(Borrower.class);
+		assertFalse(isBorrower);
+
+		// add a role
+		Borrower fredsBorrowerRole = Riehle97Factory.eINSTANCE.createBorrower();
+		fred.addRole(fredsBorrowerRole);
+		
+		// check that the role is still there
+		isBorrower = fred.hasRole(Borrower.class);
+		assertFalse(isBorrower);
+		
+		// get the role object
+		Borrower fredPlayingBorrower = fred.getRole(Borrower.class);
+		assertNotNull(fredPlayingBorrower);
+		// call a role specific method
+		fredPlayingBorrower.getCredits();
+	}
+}
