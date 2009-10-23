80de2ee666f5ed9e7b1710eaa10cc47f5b65a34d
Jendrik Johannes <jendrik.johannes@devboost.de>
2009-10-23 15:47:22 +0000

    de-activated most of the java template language to allow build to succeed

--- org.emftext.language.java_templates/metamodel/java_templates.cs
+++ org.emftext.language.java_templates/metamodel/java_templates.cs
@@ -25,42 +25,4 @@ TOKENSTYLES {
 
 RULES {
 	JavaTemplate ::= "<%TEMPLATE" "INPUT=" inputMetaClass[STRING_LITERAL] "%>" body;
-	
-	PhNamedElementName ::= "<%=" expression[STRING_LITERAL] "%>";
-	
-	IfElseAssignmentExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfElseUnaryExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfForLoop ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfElseStatementContainer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfParametrizable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForParametrizable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfMemberContainer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForMemberContainer ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfElseUnaryModificationExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfExplicitConstructorCall ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfConditionalExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfElseInstanceOfExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfStatementListContainer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForStatementListContainer ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfCondition ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfElseTypeReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfElsePrimitiveTypeReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfElseAnnotationInstance ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfTypeArgumentable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForTypeArgumentable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfModifiable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForModifiable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfElseSingleAnnotationParameter ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfImportingElement ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForImportingElement ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfElseMember ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfElseNamedElement ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
-	IfAnnotableAndModifiable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForAnnotableAndModifiable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfAnnotationInstance ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	IfArrayInitializer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForArrayInitializer ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
-	IfSwitch ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
-	ForSwitch ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
 }
---
+++ org.emftext.language.java_templates/metamodel/rev6618/java_templates.cs
@@ -0,0 +1,66 @@
+SYNTAXDEF java_template
+FOR <http://www.emftext.org/language/java_templates>
+START JavaTemplate
+
+IMPORTS {
+	java : <http://www.emftext.org/java> <../../org.emftext.language.java/metamodel/java.genmodel> 
+		WITH SYNTAX java <../../org.emftext.language.java/metamodel/java.cs>
+}
+
+OPTIONS {
+	usePredefinedTokens = "false";
+}
+
+TOKENSTYLES {
+	"<%TEMPLATE" COLOR #C00000, BOLD; 
+	"INPUT=" COLOR #C00000, BOLD;
+	"<%IF" COLOR #C00000, BOLD;
+	"<%FOR" COLOR #C00000, BOLD;
+	"<%=" COLOR #C00000, BOLD;
+	"%>" COLOR #C00000, BOLD;
+	"<%ENDFOR%>" COLOR #C00000, BOLD;
+	"<%ENDIF%>" COLOR #C00000, BOLD;
+	"SL_COMMENT" COLOR #00A000, ITALIC;
+}
+
+RULES {
+	JavaTemplate ::= "<%TEMPLATE" "INPUT=" inputMetaClass[STRING_LITERAL] "%>" body;
+	
+	PhNamedElementName ::= "<%=" expression[STRING_LITERAL] "%>";
+	
+	IfElseAssignmentExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfElseUnaryExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfForLoop ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfElseStatementContainer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfParametrizable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForParametrizable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfMemberContainer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForMemberContainer ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfElseUnaryModificationExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfExplicitConstructorCall ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfConditionalExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfElseInstanceOfExpression ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfStatementListContainer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForStatementListContainer ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfCondition ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfElseTypeReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfElsePrimitiveTypeReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfElseAnnotationInstance ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfReference ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfTypeArgumentable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForTypeArgumentable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfModifiable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForModifiable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfElseSingleAnnotationParameter ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfImportingElement ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForImportingElement ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfElseMember ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfElseNamedElement ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ELSE%>" elseBody "<%ENDIF%>";
+	IfAnnotableAndModifiable ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForAnnotableAndModifiable ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfAnnotationInstance ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	IfArrayInitializer ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForArrayInitializer ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+	IfSwitch ::= "<%IF" expression[STRING_LITERAL] "%>" body "<%ENDIF%>";
+	ForSwitch ::= "<%FOR" expression[STRING_LITERAL] "%>" body "<%ENDFOR%>";
+}
