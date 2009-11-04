d4d05a8424ede13a1a409f4cc053bf888395449c
Christian Wende <christian.wende@devboost.de>
2009-11-04 08:19:35 +0000

    changed to EPL copyright

--- org.emftext.language.ecore.resource.ecore/metamodel/ecore.cs
+++ org.emftext.language.ecore.resource.ecore/metamodel/ecore.cs
@@ -1,3 +1,17 @@
+//*******************************************************************************
+// Copyright (c) 2006-2009 
+// Software Technology Group, Dresden University of Technology
+// 
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0 
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+//   Software Technology Group - TU Dresden, Germany 
+//      - initial API and implementation
+// ******************************************************************************/
+
 //This is a dummy CS used to generate a ResourceFactoryDelegator and a 'additional_extension_parser' EP for 'ecore'.
 //Since the default syntax for ecore is XMI, the generated parser etc. are not used.
 //The generated ResourceFactory is replaced by the default EcoreResourceFactoryImpl using the extension point in another plugin.
