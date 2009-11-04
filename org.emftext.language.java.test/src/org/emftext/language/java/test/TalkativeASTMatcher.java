2e36d459c3553ee108e930f195b59e83eee78150
Christian Wende <christian.wende@devboost.de>
2009-11-04 08:26:05 +0000

    changed to EPL copyright

--- org.emftext.language.java.test/src/org/emftext/language/java/test/TalkativeASTMatcher.java
+++ org.emftext.language.java.test/src/org/emftext/language/java/test/TalkativeASTMatcher.java
@@ -1,13 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2006-2009 
+ * Software Technology Group, Dresden University of Technology
+ * 
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License v1.0 
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ *   Software Technology Group - TU Dresden, Germany 
+ *      - initial API and implementation
+ ******************************************************************************/
 package org.emftext.language.java.test;
 
 import java.math.BigInteger;
