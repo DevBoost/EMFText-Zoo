2315e806be1941493fd1cc96deeccb90a1364d56
Mirko Seifert <mirko.seifert@devboost.de>
2009-11-06 09:18:19 +0000

    extended test case for regexp language

--- org.emftext.language.regexp.test/src/org/emftext/language/regexp/RegExpTest.java
+++ org.emftext.language.regexp.test/src/org/emftext/language/regexp/RegExpTest.java
@@ -13,9 +13,43 @@
  ******************************************************************************/
 package org.emftext.language.regexp;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+
 import junit.framework.TestCase;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.emftext.sdk.concretesyntax.TokenDefinition;
+import org.emftext.test.ConcreteSyntaxTestHelper;
+import org.junit.Test;
 
 // TODO mseifert: add some basic tests for parsing regular expressions
 public class RegExpTest extends TestCase {
 
+	@Test
+	public void testExpsFromGrammars() throws IOException {
+		
+		Collection<String> grammars = ConcreteSyntaxTestHelper.findAllGrammars(new File(".."));
+		System.out.println(RegExpTest.class.getSimpleName() + ".testExpsFromGrammars() found " + grammars.size() + " grammar files.");
+		// make sure all grammars are found
+		assertTrue(grammars.size() > 140);
+		
+		for (String grammar : grammars) {
+			System.out.println("AntlrTranslationTest.testExpsFromGrammars() testing " + grammar);
+			Resource resource = ConcreteSyntaxTestHelper.loadCsResource(grammar);
+
+			TreeIterator<EObject> contents = resource.getAllContents();
+			while (contents.hasNext()) {
+				EObject object = (EObject) contents.next();
+				if (object instanceof TokenDefinition) {
+					TokenDefinition td = (TokenDefinition) object;
+					String regex = td.getRegex();
+					System.out.println(regex);
+				}
+			}
+		}
+	}
 }
