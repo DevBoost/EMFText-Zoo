7ed1189007e0227bcb204de41f5e555eb01627db
Mirko Seifert <mirko.seifert@devboost.de>
2009-11-09 17:30:21 +0000

    fixed bug 950: PropertiesJava: PostProcessor is not fully implemented

--- org.emftext.language.java.properties.resource/src/org/emftext/language/java/properties/resource/PropertiesJavaPostProcessor.java
+++ org.emftext.language.java.properties.resource/src/org/emftext/language/java/properties/resource/PropertiesJavaPostProcessor.java
@@ -23,12 +23,16 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.emftext.language.java.literals.LiteralsFactory;
+import org.emftext.language.java.expressions.AssignmentExpression;
+import org.emftext.language.java.expressions.ExpressionsFactory;
 import org.emftext.language.java.members.ClassMethod;
 import org.emftext.language.java.members.Field;
 import org.emftext.language.java.members.MemberContainer;
 import org.emftext.language.java.members.MembersFactory;
 import org.emftext.language.java.modifiers.ModifiersFactory;
+import org.emftext.language.java.operators.OperatorsFactory;
+import org.emftext.language.java.parameters.OrdinaryParameter;
+import org.emftext.language.java.parameters.ParametersFactory;
 import org.emftext.language.java.properties.PropertiesPackage;
 import org.emftext.language.java.properties.Property;
 import org.emftext.language.java.properties.resource.propjava.IPropjavaOptionProvider;
@@ -38,9 +42,13 @@ import org.emftext.language.java.properties.resource.propjava.IPropjavaResourceP
 import org.emftext.language.java.properties.resource.propjava.mopp.PropjavaResource;
 import org.emftext.language.java.properties.resource.propjava.util.PropjavaEObjectUtil;
 import org.emftext.language.java.properties.resource.propjava.util.PropjavaStringUtil;
+import org.emftext.language.java.references.IdentifierReference;
+import org.emftext.language.java.references.ReferencesFactory;
+import org.emftext.language.java.statements.ExpressionStatement;
 import org.emftext.language.java.statements.Return;
 import org.emftext.language.java.statements.StatementsFactory;
 import org.emftext.language.java.types.TypeReference;
+import org.emftext.language.java.types.TypesFactory;
 
 public class PropertiesJavaPostProcessor implements IPropjavaResourcePostProcessor,
 	IPropjavaResourcePostProcessorProvider, IPropjavaOptionProvider {
@@ -79,26 +87,72 @@ public class PropertiesJavaPostProcessor implements IPropjavaResourcePostProcess
 	}
 
 	private void convertToField(Property property) {
+		Field field = createField(property);
+		// create getter
+		createGetter(property, field);
+		// create setter
+		if (!property.isReadonly()) {
+			createSetter(property, field);
+		}
+		
+		EcoreUtil.replace(property, field);
+	}
+
+	private void createSetter(Property property, Field field) {
+		ClassMethod setterMethod = MembersFactory.eINSTANCE.createClassMethod();
+		setterMethod.setName("set" + PropjavaStringUtil.capitalize(property.getName()));
+		setterMethod.setTypeReference(TypesFactory.eINSTANCE.createVoid());
+		
+		OrdinaryParameter parameter = ParametersFactory.eINSTANCE.createOrdinaryParameter();
+		parameter.setName("newValue");
+		TypeReference typeReference = field.getTypeReference();
+		TypeReference tReferenceCopy = (TypeReference) EcoreUtil.copy(typeReference);
+		parameter.setTypeReference(tReferenceCopy);
+		setterMethod.getParameters().add(parameter);
+		
+		AssignmentExpression assignment = ExpressionsFactory.eINSTANCE.createAssignmentExpression();
+		assignment.setAssignmentOperator(OperatorsFactory.eINSTANCE.createAssignment());
+		
+		IdentifierReference parameterRef = ReferencesFactory.eINSTANCE.createIdentifierReference();
+		parameterRef.setTarget(parameter);
+		assignment.setValue(parameterRef);
+		IdentifierReference fieldRef = ReferencesFactory.eINSTANCE.createIdentifierReference();
+		fieldRef.setTarget(field);
+		assignment.setChild(fieldRef);
+		
+		ExpressionStatement assignmentStatement = StatementsFactory.eINSTANCE.createExpressionStatement();
+		assignmentStatement.setExpression(assignment);
+		setterMethod.getStatements().add(assignmentStatement);
+		((MemberContainer) property.eContainer()).getMembers().add(setterMethod);
+	}
+
+	private void createGetter(Property property, Field field) {
+		// create getter
+		ClassMethod getterMethod = MembersFactory.eINSTANCE.createClassMethod();
+		getterMethod.setName("get" + PropjavaStringUtil.capitalize(property.getName()));
+		
+		Return returnStatement = StatementsFactory.eINSTANCE.createReturn();
+		IdentifierReference fieldRef = ReferencesFactory.eINSTANCE.createIdentifierReference();
+		fieldRef.setTarget(field);
+		returnStatement.setReturnValue(fieldRef);
+		
+		getterMethod.getStatements().add(returnStatement);
+		
+		TypeReference typeReference = field.getTypeReference();
+		TypeReference tReferenceCopy = (TypeReference) EcoreUtil.copy(typeReference);
+		getterMethod.setTypeReference(tReferenceCopy);
+		
+		((MemberContainer) property.eContainer()).getMembers().add(getterMethod);
+	}
+
+	private Field createField(Property property) {
+		// create field
 		Field field = MembersFactory.eINSTANCE.createField();
 		field.setName(property.getName());
 		TypeReference typeReference = property.getTypeReference();
 		field.setTypeReference(typeReference);
 		field.getAnnotationsAndModifiers().add(ModifiersFactory.eINSTANCE.createPrivate());
-		
-		ClassMethod getterMethod = MembersFactory.eINSTANCE.createClassMethod();
-		getterMethod.setName("get" + PropjavaStringUtil.capitalize(property.getName()));
-		
-		Return returnStatement = StatementsFactory.eINSTANCE.createReturn();
-		// TODO return field instead of null
-		returnStatement.setReturnValue(LiteralsFactory.eINSTANCE.createNullLiteral());
-		
-		getterMethod.getStatements().add(returnStatement);
-		
-		TypeReference tReferenceCopy = (TypeReference) EcoreUtil.copy(typeReference);
-		getterMethod.setTypeReference(tReferenceCopy);
-		
-		((MemberContainer)property.eContainer()).getMembers().add(getterMethod);
-		EcoreUtil.replace(property, field);
+		return field;
 	}
 
 	public IPropjavaResourcePostProcessor getResourcePostProcessor() {
