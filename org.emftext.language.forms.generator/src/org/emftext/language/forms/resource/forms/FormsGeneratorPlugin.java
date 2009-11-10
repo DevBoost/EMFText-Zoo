a11ad61e9957aaee6e6310729123df1a6f6a2212
Christian Wende <christian.wende@devboost.de>
2009-11-10 20:07:29 +0000

    fixed 937 and 938 for forms language

---
+++ org.emftext.language.forms.generator/src/org/emftext/language/forms/resource/forms/FormsGeneratorPlugin.java
@@ -0,0 +1,43 @@
+package org.emftext.language.forms.resource.forms;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class FormsGeneratorPlugin extends Plugin {
+
+	public static final String PLUGIN_ID = "org.emftext.language.forms.generator";
+	public static final String CS_FILE_EXTENSION = "cs";
+	
+	private static FormsGeneratorPlugin plugin;
+	
+	public FormsGeneratorPlugin() {
+		super();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static FormsGeneratorPlugin getDefault() {
+		return plugin;
+	}
+}
--- org.emftext.language.forms.generator/src/org/emftext/language/forms/resource/forms/custom/GeneratingResourceProcessor.java
+++ org.emftext.language.forms.generator/src/org/emftext/language/forms/resource/forms/custom/GeneratingResourceProcessor.java
@@ -19,11 +19,22 @@ import generator.html.IPhoneIndexGenerator;
 import generator.xml.FOFormGenerator;
 import generator.xml.XMLFormGenerator;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
@@ -38,36 +49,38 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.emftext.language.forms.Form;
+import org.emftext.language.forms.resource.forms.FormsGeneratorPlugin;
 import org.emftext.language.forms.resource.forms.IFormsResourcePostProcessor;
 import org.emftext.language.forms.resource.forms.IFormsResourcePostProcessorProvider;
 import org.emftext.language.forms.resource.forms.mopp.FormsResource;
 
-public class GeneratingResourceProcessor implements IFormsResourcePostProcessor,
-		IFormsResourcePostProcessorProvider {
+public class GeneratingResourceProcessor implements
+		IFormsResourcePostProcessor, IFormsResourcePostProcessorProvider {
 
 	public void process(FormsResource resource) {
 		EList<EObject> contents = resource.getContents();
-		
+
 		Set<EObject> distinctObjects = new HashSet<EObject>();
 		distinctObjects.addAll(contents);
 		for (EObject eobject : distinctObjects) {
 			if (eobject instanceof Form) {
-				final Form form = (Form)eobject;
-				final IProject project = WorkspaceSynchronizer.getFile(form.eResource()).getProject();
+				final Form form = (Form) eobject;
+				final IProject project = WorkspaceSynchronizer.getFile(
+						form.eResource()).getProject();
 				Job creationJob = new Job("Generating Forms") {
 
 					@Override
 					protected IStatus run(IProgressMonitor monitor) {
-						createHTMLForm(project,form);
-						createIPhoneForm(project,form);
-						createXMLForm(project,form);
-						IFile result = createFoForm(project,form);
-						createPSForm(result,form.getCaption());
+						createHTMLForm(project, form);
+						createIPhoneForm(project, form);
+						createXMLForm(project, form);
+						IFile result = createFoForm(project, form);
+						createPSForm(result, getTrimedFilename(form));
 						return Status.OK_STATUS;
 					}
-					
+
 				};
-				
+
 				creationJob.schedule();
 			}
 		}
@@ -75,42 +88,126 @@ public class GeneratingResourceProcessor implements IFormsResourcePostProcessor,
 	}
 
 	private void createIPhoneForm(IProject project, Form form) {
+		String iphone = "iphone";
+
 		final IPhoneFormGenerator iphoneFormGen = new IPhoneFormGenerator();
-		generateForm(project,form, "iphone",form.getCaption()+".html", iphoneFormGen);
+		generateForm(project, form, iphone, getTrimedFilename(form) + "html",
+				iphoneFormGen);
 		final IPhoneIndexGenerator iphoneIndexGen = new IPhoneIndexGenerator();
-		generateForm(project,form, "iphone","index.html", iphoneIndexGen);
-		
+		generateForm(project, form, iphone, "index.html", iphoneIndexGen);
+		copyAndExtractWebAppFramework(project.getFolder(iphone));
+
+	}
+
+	private String getTrimedFilename(Form form) {
+		String filename = form.eResource().getURI().lastSegment();
+		filename = filename.substring(0, filename.length()
+				- form.eResource().getURI().fileExtension().length());
+		return filename;
+	}
+
+	private void copyAndExtractWebAppFramework(IFolder iFolder) {
+		IFile destFile = iFolder.getFile("WebApp.zip");
+		if (destFile.exists())
+			return;
+		URL entry = FormsGeneratorPlugin.getDefault().getBundle().getEntry(
+				"/src/WebApp.zip");
+
+		try {
+			InputStream in = entry.openStream();
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			byte buf[] = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0)
+				out.write(buf, 0, len);
+			out.flush();
+			destFile.create(new ByteArrayInputStream(out.toByteArray()), false,
+					new NullProgressMonitor());
+			in.close();
+			iFolder.refreshLocal(IFolder.DEPTH_INFINITE,
+					new NullProgressMonitor());
+			unzip(destFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void unzip(IFile zioContainer) {
+		File localFile = zioContainer.getLocation().toFile();
+		File containingFolder = localFile.getParentFile();
+		ZipFile zipFile;
+		try {
+			zipFile = new ZipFile(localFile);
+			List<ZipEntry> files = new ArrayList<ZipEntry>();
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			while (entries.hasMoreElements()) {
+				ZipEntry nextElement = entries.nextElement();
+				if (nextElement.isDirectory()) {
+					File dir = new File(containingFolder + File.separator
+							+ nextElement.getName());
+					dir.mkdir();
+				} else {
+					files.add(nextElement);
+				}
+			}
+
+			for (ZipEntry fileEntry : files) {
+				int size;
+				byte[] buffer = new byte[2048];
+				BufferedInputStream bis = new BufferedInputStream(zipFile
+						.getInputStream(fileEntry));
+				BufferedOutputStream bos = new BufferedOutputStream(
+						new FileOutputStream(containingFolder + File.separator
+								+ fileEntry.getName()), buffer.length);
+				while ((size = bis.read(buffer, 0, buffer.length)) != -1) {
+					bos.write(buffer, 0, size);
+				}
+				bos.flush();
+				bos.close();
+				bis.close();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	private void createHTMLForm(IProject project, Form form) {
 		final HTMLFormGenerator htmlFormGen = new HTMLFormGenerator();
-		generateForm(project,form, "html",form.getCaption()+".html",htmlFormGen);
-	}
-	
-	private void createXMLForm(IProject project, Form form) {
-		XMLFormGenerator xmlFormGenerator = new XMLFormGenerator();
-		generateForm(project,form, "xml",form.getCaption()+".xml",xmlFormGenerator);
-	}
-	
-	private IFile createFoForm(IProject project, Form form) {
-		FOFormGenerator foFormGenerator = new FOFormGenerator();
-		return generateForm(project,form,"xsl-fo",form.getCaption()+".xml",foFormGenerator);
-	}
-	
-	private IFile createPSForm(IFile foFile,String titel){
-		PDFFormGenerator psFormGenerator = new PDFFormGenerator();
-		return generateForm(foFile.getProject(),foFile,"pdf",titel+".pdf",psFormGenerator);
+		generateForm(project, form, "html", getTrimedFilename(form) + "html",
+				htmlFormGen);
 	}
 
-	
-	private IFile generateForm(IProject targetProject,Object argument, String folderName, String filename,
-			final IGenerator generator) {
+	private void createXMLForm(IProject project, Form form) {
+		XMLFormGenerator xmlFormGenerator = new XMLFormGenerator();
+		generateForm(project, form, "xml", getTrimedFilename(form) + "xml",
+				xmlFormGenerator);
+	}
+
+	private IFile createFoForm(IProject project, Form form) {
+		FOFormGenerator foFormGenerator = new FOFormGenerator();
+		return generateForm(project, form, "xsl-fo", getTrimedFilename(form)
+				+ "xml", foFormGenerator);
+	}
+
+	private IFile createPSForm(IFile foFile, String titel) {
+		PDFFormGenerator psFormGenerator = new PDFFormGenerator();
+		return generateForm(foFile.getProject(), foFile, "pdf", titel + "pdf",
+				psFormGenerator);
+	}
+
+	private IFile generateForm(IProject targetProject, Object argument,
+			String folderName, String filename, final IGenerator generator) {
 		try {
-			
+
 			IFolder folder = targetProject.getFolder(folderName);
 			if (!folder.exists()) {
 				try {
 					folder.create(true, true, new NullProgressMonitor());
+					folder.refreshLocal(IFolder.DEPTH_INFINITE,
+							new NullProgressMonitor());
 				} catch (CoreException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -121,15 +218,19 @@ public class GeneratingResourceProcessor implements IFormsResourcePostProcessor,
 			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 			outStream.write(generator.generate(argument));
 			outStream.flush();
-			
-			if(!file.exists()){
-				file.create(new ByteArrayInputStream(outStream.toByteArray()),false,new NullProgressMonitor());
-			}
-			else{
-				file.setContents(new ByteArrayInputStream(outStream.toByteArray()),false,true,new NullProgressMonitor());
+
+			if (!file.exists()) {
+				file.create(new ByteArrayInputStream(outStream.toByteArray()),
+						false, new NullProgressMonitor());
+			} else {
+				file
+						.setContents(new ByteArrayInputStream(outStream
+								.toByteArray()), false, true,
+								new NullProgressMonitor());
 			}
 
-			folder.refreshLocal(IFolder.DEPTH_INFINITE,new NullProgressMonitor());
+			folder.refreshLocal(IFolder.DEPTH_INFINITE,
+					new NullProgressMonitor());
 			return file;
 		} catch (IOException e) {
 			e.printStackTrace();
