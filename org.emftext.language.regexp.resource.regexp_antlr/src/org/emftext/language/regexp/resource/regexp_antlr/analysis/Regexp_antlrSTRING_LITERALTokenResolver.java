730947c13633b73f10431e2fc42ae29377fa2970
Mirko Seifert <mirko.seifert@devboost.de>
2009-11-11 21:13:13 +0000

    fixed token resolvers to make the translation test succeed

--- org.emftext.language.regexp.resource.regexp_antlr/src/org/emftext/language/regexp/resource/regexp_antlr/analysis/Regexp_antlrSTRING_LITERALTokenResolver.java
+++ org.emftext.language.regexp.resource.regexp_antlr/src/org/emftext/language/regexp/resource/regexp_antlr/analysis/Regexp_antlrSTRING_LITERALTokenResolver.java
@@ -2,19 +2,14 @@ package org.emftext.language.regexp.resource.regexp_antlr.analysis;
 
 public class Regexp_antlrSTRING_LITERALTokenResolver implements org.emftext.language.regexp.resource.regexp_antlr.IRegexp_antlrTokenResolver {
 	
-	private org.emftext.language.regexp.resource.regexp_antlr.analysis.Regexp_antlrDefaultTokenResolver defaultTokenResolver = new org.emftext.language.regexp.resource.regexp_antlr.analysis.Regexp_antlrDefaultTokenResolver();
-	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
-		return result;
+		return "'" + value + "'";
 	}
 	
 	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.regexp.resource.regexp_antlr.IRegexp_antlrTokenResolveResult result) {
-		defaultTokenResolver.resolve(lexem, feature, result);
+		result.setResolvedToken(lexem.substring(1, lexem.length() - 1));
 	}
 	
 	public void setOptions(java.util.Map<?,?> options) {
-		defaultTokenResolver.setOptions(options);
 	}
-	
 }
