b10cd4afcbe99504833a34c2992a57982ed0d90d
Christian Werner <s7436532@mail.inf.tu-dresden.de>
2010-01-18 22:51:49 +0000

    fixed database language and its TGG rule annotation extension

---
+++ org.emftext.language.database.tgg_rule.resource.database.tgg_rule/src/org/emftext/language/database/tgg_rule/resource/database/tgg_rule/analysis/Database_tgg_ruleTEXTTokenResolver.java
@@ -0,0 +1,26 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.emftext.language.database.tgg_rule.resource.database.tgg_rule.analysis;
+
+public class Database_tgg_ruleTEXTTokenResolver implements org.emftext.language.database.tgg_rule.resource.database.tgg_rule.IDatabase_tgg_ruleTokenResolver {
+	
+	private org.emftext.language.database.tgg_rule.resource.database.tgg_rule.analysis.Database_tgg_ruleDefaultTokenResolver defaultTokenResolver = new org.emftext.language.database.tgg_rule.resource.database.tgg_rule.analysis.Database_tgg_ruleDefaultTokenResolver();
+	
+	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
+		return result;
+	}
+	
+	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.database.tgg_rule.resource.database.tgg_rule.IDatabase_tgg_ruleTokenResolveResult result) {
+		defaultTokenResolver.resolve(lexem, feature, result);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
+}
