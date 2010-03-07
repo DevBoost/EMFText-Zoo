883f0aa6a4694a966084fb51d60584c9818514ce
Mirko Seifert <mirko.seifert@devboost.de>
2010-03-07 19:49:53 +0000

    add Java Property File syntax to test registry

--- org.emftext.language.test/src/org/emftext/language/test/TestLanguageRegistry.java
+++ org.emftext.language.test/src/org/emftext/language/test/TestLanguageRegistry.java
@@ -65,6 +65,8 @@ public class TestLanguageRegistry {
 		metaInformations.add(
 				new org.emftext.language.java.treejava.resource.treejava.mopp.TreejavaMetaInformation());
 		metaInformations.add(
+				new org.emftext.language.java_properties.resource.properties.mopp.PropertiesMetaInformation());
+		metaInformations.add(
 				new org.emftext.language.regexp.resource.regexp_antlr.mopp.Regexp_antlrMetaInformation());
 		metaInformations.add(
 				new org.emftext.language.regexp.resource.regexp_sdf.mopp.Regexp_sdfMetaInformation());
