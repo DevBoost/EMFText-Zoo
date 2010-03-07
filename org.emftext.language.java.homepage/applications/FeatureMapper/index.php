41c9820294fe0a8cc0aaeb345765284e9daeed9a
Mirko Seifert <mirko.seifert@devboost.de>
2010-03-07 19:44:46 +0000

    removed obsolete JaMoPP homepage files (all content has been migrated to the Wiki)

--- org.emftext.language.java.homepage/applications/FeatureMapper/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header( 'Location: http://www.reuseware.org/index.php/EMFText_JaMoPP_Applications_FeatureMapper' );
-?>
--- org.emftext.language.java.homepage/applications/reuseware/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header( 'Location: http://reuseware.org/index.php/EMFText_JaMoPP_Applications_Reuseware' );
-?>
