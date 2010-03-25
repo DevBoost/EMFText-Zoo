a6afc6c3405e4bb2bac678be5b848af1233a8cb6
Tan-Ky Hoang-Kim <s1010530@mail.inf.tu-dresden.de>
2010-03-25 14:51:51 +0000

    added example ChangeDescriptor for the interpretation

---
+++ org.emftext.language.rolecore.dependencies.test/src/org/emftext/language/rolecore/dependencies/test/ChangeDescriptionCreator.java
@@ -0,0 +1,163 @@
+package org.emftext.language.rolecore.dependencies.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EFactory;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.change.ChangeDescription;
+import org.eclipse.emf.ecore.change.ChangeFactory;
+import org.eclipse.emf.ecore.change.FeatureChange;
+import org.eclipse.emf.ecore.change.ListChange;
+import org.eclipse.emf.ecore.change.ResourceChange;
+import org.eclipse.emf.ecore.change.impl.EObjectToChangesMapEntryImpl;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.emftext.language.rolecore.dependencies.interpreter.DomainRoot;
+import org.emftext.language.rolecore.dependencies.interpreter.InterpretationContext;
+import org.emftext.language.rolecore.dependencies.interpreter.InterpreterPackage;
+import org.emftext.language.rolecore.families.FamiliesPackage;
+import org.emftext.language.rolecore.families.FamilyDomainCore;
+import org.emftext.language.rolecore.examples.riehle97.Riehle97Package;
+
+public class ChangeDescriptionCreator {
+
+	private InterpretationContext context;
+	private String inputURI;
+
+	ChangeDescriptionCreator(InterpretationContext interpretationContext, String inputURI) {
+		context = interpretationContext;
+		this.inputURI = inputURI;
+	}
+	/**
+	 * Create a change description for the given resource which add a Customer.
+	 * Save this change description in the inputURI.
+	 * @param cResource
+	 * @return
+	 */
+	public ChangeDescription createAddCustomerToResourceCD(Resource cResource) {
+		DomainRoot domainRoot = null;
+		if (cResource.getContents().size()==0){
+			domainRoot = context.findOrCreateDomainRoot("customersA");
+			cResource.getContents().add(domainRoot);
+			context.saveResource(cResource, null);
+		}else
+			domainRoot = (DomainRoot) cResource.getContents().get(0);
+		Riehle97Package cEPackage = Riehle97Package.eINSTANCE;
+		EObject customerCore = context.createCoreClass(cEPackage.getCustomerCore());
+		
+		ChangeDescription cd = ChangeFactory.eINSTANCE.createChangeDescription();
+		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(domainRoot);
+		FeatureChange fc = ChangeFactory.eINSTANCE.createFeatureChange();
+		ListChange lc = ChangeFactory.eINSTANCE.createListChange();
+		cd.getObjectChanges().add(entry);
+		entry.getValue().add(fc);
+		fc.setFeature(InterpreterPackage.eINSTANCE.getDomainRoot_CoreClasses());
+		fc.getListChanges().add(lc);
+		lc.getReferenceValues().add(customerCore);
+		cd.getObjectsToAttach().add(customerCore);
+		context.saveResource(cd, inputURI + "addCustomerToCustomersA.change.xmi");
+		return cd;
+	}
+	/**
+	 * TODO recode
+	 */
+	@SuppressWarnings("unused")
+	private void createAddFamilyChangeDescription() {
+		FamiliesPackage familiesEPackage = (FamiliesPackage) EPackage.Registry.INSTANCE
+		.getEPackage("http://www.emftext.org/language/rolecore/families");
+		List<EObject> eObjectList = new ArrayList<EObject>();
+		Resource familiesResource = context.saveResource(eObjectList, inputURI + "families.xmi");
+		
+		FamilyDomainCore familyDomainCore = (FamilyDomainCore) familiesResource.getContents().get(0);
+		FamiliesPackage ePackage = (FamiliesPackage) EPackage.Registry.INSTANCE
+				.getEPackage("http://www.emftext.org/language/rolecore/families");
+		EClass familyRole = ePackage.getFamilydomainFamilyFamiliesRole();
+		EClass familyCoreEClass = (EClass) ePackage.getEClassifier("FamilyCore");
+		EObject familyCoreClass = context.createCoreClass(familyCoreEClass);
+		EClass lastNameRole = (EClass) ePackage.getEClassifier("LastName");
+		context.addRoleToCore(familyCoreClass, lastNameRole, "March", "lastName");
+		eObjectList.add(getAddRoleToCoreChangeDescription(familyDomainCore, familyRole, context
+				.getRootCore(familyCoreClass), ePackage.getFamilydomainFamilyFamiliesRole_Families().getName()));
+
+		EClass fatherCoreEClass = (EClass) ePackage.getEClassifier("MemberCore");
+		EObject fatherCoreClass = context.createCoreClass(fatherCoreEClass);
+
+		EClass fatherNameEClass = (EClass) ePackage.getEClassifier("FirstName");
+		context.addRoleToCore(fatherCoreClass, fatherNameEClass, "Jim", "firstName");
+		EClass fatherFamilyRoleEClass = (EClass) ePackage.getEClassifier("MemberFamilyFamilyfatherRole");
+		context.addRoleToCore(fatherCoreClass, fatherFamilyRoleEClass, familyCoreClass, "familyFather");
+		EClass familyFatherRoleEClass = (EClass) ePackage.getEClassifier("FamilyMemberFatherRole");
+		context.addRoleToCore(familyCoreClass, familyFatherRoleEClass, fatherCoreClass, "father");
+
+		EClass motherCoreEClass = (EClass) ePackage.getEClassifier("MemberCore");
+		EObject motherCoreClass = context.createCoreClass(motherCoreEClass);
+
+		EClass motherNameEClass = (EClass) ePackage.getEClassifier("FirstName");
+		context.addRoleToCore(motherCoreClass, motherNameEClass, "Cindy", "firstName");
+		EClass motherFamilyRoleEClass = (EClass) ePackage.getEClassifier("MemberFamilyFamilymotherRole");
+		context.addRoleToCore(motherCoreClass, motherFamilyRoleEClass, familyCoreClass, "familyMother");
+		EClass familyMotherRoleEClass = (EClass) ePackage.getEClassifier("FamilyMemberMotherRole");
+		context.addRoleToCore(familyCoreClass, familyMotherRoleEClass, motherCoreClass, "mother");
+
+		context.saveResource(eObjectList, inputURI + "addFamily.change");
+	}
+
+	/**
+	 * TODO recode
+	 * @param coreClass
+	 * @param roleEClass
+	 * @param eObject
+	 * @param featureName
+	 * @return
+	 */
+	public ChangeDescription getAddRoleToCoreChangeDescription(EObject coreClass, EClass roleEClass, EObject eObject,
+			String featureName) {
+		EFactory eFactory = roleEClass.getEPackage().getEFactoryInstance();
+		EClass coreOfRoleEClass = (EClass) context.getCoreStructuralFeature(roleEClass).getEType();
+		if (coreClass.eClass().equals(coreOfRoleEClass)) {
+			// Add a role to the core class, multiple valued
+			EObject roleClass = eFactory.create(roleEClass);
+			ChangeDescription changeDescription = ChangeFactory.eINSTANCE.createChangeDescription();
+			changeDescription.getObjectsToAttach().add(roleClass);
+			Entry<EObject, EList<FeatureChange>> roleToCoreEntry = ChangeFactory.eINSTANCE
+					.createEObjectToChangesMapEntry(coreClass);
+			changeDescription.getObjectChanges().add(roleToCoreEntry);
+			FeatureChange roleToCoreFeatureChange = ChangeFactory.eINSTANCE.createFeatureChange();
+			roleToCoreFeatureChange.setFeature(context.getRolesStructuralFeature(coreClass.eClass()));
+			roleToCoreEntry.getValue().add(roleToCoreFeatureChange);
+			ListChange listChange = ChangeFactory.eINSTANCE.createListChange();
+			listChange.getReferenceValues().add(roleClass);
+			roleToCoreFeatureChange.getListChanges().add(listChange);
+
+			// Add the core to the role class. single valued
+			changeDescription.getObjectsToAttach().add(coreClass);
+			Entry<EObject, EList<FeatureChange>> coreToRoleEntry = ChangeFactory.eINSTANCE
+					.createEObjectToChangesMapEntry(roleClass);
+			FeatureChange coreToRoleFeatureChange = ChangeFactory.eINSTANCE.createFeatureChange();
+			coreToRoleFeatureChange.setFeature(context.getCoreStructuralFeature(roleEClass));
+			coreToRoleFeatureChange.setReferenceValue(coreClass);
+			changeDescription.getObjectChanges().add(coreToRoleEntry);
+			coreToRoleEntry.getValue().add(coreToRoleFeatureChange);
+			try {
+				// set the reference of the role class, is not in
+				// ChangeDescription
+				roleClass.eSet(context.getStructuralFeature(roleEClass, featureName), eObject);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return changeDescription;
+		} else {
+			EStructuralFeature coreStructuralFeature = context.getCoreStructuralFeature(coreClass.eClass());
+			if (coreStructuralFeature == null) {
+				return null;
+			}
+			EObject superCoreClass = (EObject) coreClass.eGet(coreStructuralFeature);
+			return getAddRoleToCoreChangeDescription(superCoreClass, roleEClass, eObject, featureName);
+		}
+	}
+}
--- org.emftext.language.rolecore.dependencies.test/src/org/emftext/language/rolecore/dependencies/test/InterpreterTest.java
+++ org.emftext.language.rolecore.dependencies.test/src/org/emftext/language/rolecore/dependencies/test/InterpreterTest.java
@@ -1,14 +1,24 @@
 package org.emftext.language.rolecore.dependencies.test;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.change.ChangeFactory;
-import org.eclipse.emf.ecore.change.FeatureChange;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.change.ChangeDescription;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.emftext.language.rolecore.dependencies.Graph;
 import org.emftext.language.rolecore.dependencies.interpreter.InterpretationContext;
 import org.emftext.language.rolecore.dependencies.interpreter.Interpreter;
+import org.emftext.language.rolecore.dependencies.interpreter.InterpreterPackage;
+import org.emftext.language.rolecore.dependencies.resource.dependencies.mopp.DependenciesMetaInformation;
+import org.emftext.language.rolecore.dependencies.resource.dependencies.mopp.DependenciesResourceFactory;
+import org.emftext.language.rolecore.examples.riehle97.Riehle97Package;
+import org.emftext.language.rolecore.families.FamiliesPackage;
+import org.emftext.language.rolecore.persons.PersonsPackage;
+import org.emftext.language.rolecore.resource.rolecore.mopp.RolecoreMetaInformation;
+import org.emftext.language.rolecore.resource.rolecore.mopp.RolecoreResourceFactory;
 
 import junit.framework.TestCase;
 
@@ -18,28 +28,59 @@ public class InterpreterTest extends TestCase {
 		// TODO register XMI resource factory
 		// TODO register dependencies resource factory
 	}
-	
-	public void testInterpretation() {
-		// load instance models (family model and person model)
-		EObject familyModel = loadModel("input/mueller.xmi");
-		EObject personModel = loadModel("input/people.xmi");
-		// load dependency models
-		EObject dependencyModel = loadModel("input/families2persons.dependencies");
-		
-		// create change in memory (http://www.eclipse.org/emf/2003/Change)
-		FeatureChange change = ChangeFactory.eINSTANCE.createFeatureChange();
-		// TODO set change properties
-		
-		// execute interpreter
-		Interpreter interpreter = new Interpreter();
-		interpreter.interprete(dependencyModel, new InterpretationContext());
-		
-		// check result
-	}
 
-	private EObject loadModel(String pathName) {
-		ResourceSet rs = new ResourceSetImpl();
-		Resource r = rs.getResource(URI.createFileURI(pathName), true);
-		return r.getContents().get(0);
+	public void testInterpretation() {
+		// register the Factories
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				new RolecoreMetaInformation().getSyntaxName(), new RolecoreResourceFactory());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
+				new DependenciesMetaInformation().getSyntaxName(), new DependenciesResourceFactory());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("change", new XMIResourceFactoryImpl());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
+		// register the Packages
+		register(FamiliesPackage.eINSTANCE);
+		register(PersonsPackage.eINSTANCE);
+		register(Riehle97Package.eINSTANCE);
+		register(InterpreterPackage.eINSTANCE);
+		org.eclipse.emf.ecore.change.ChangePackage.eINSTANCE.eClass();
+		InterpretationContext interpretationContext = new InterpretationContext();
+		String inputURI = "./input/";
+		String outputURI = "./output/";
+		// create the source to test, can be removed after getting the resources
+		ChangeDescriptionCreator cdCreator = new ChangeDescriptionCreator(interpretationContext, inputURI);
+
+		// load instance models
+		interpretationContext.addResource(interpretationContext.loadResource(inputURI + "customersA.xmi"));
+		interpretationContext.addResource(interpretationContext.loadResource(inputURI + "customersB.xmi"));
+
+		// load dependency models
+		Map<String, Graph> dependencies = new HashMap<String, Graph>();
+		dependencies.put("customer", (Graph) interpretationContext.loadResource(inputURI + "customer.dependencies")
+				.getContents().get(0));
+		interpretationContext.setDependencies(dependencies);
+		// create change in memory (http://www.eclipse.org/emf/2003/Change)
+		ChangeDescription addCustomerToCustomerADC = cdCreator.createAddCustomerToResourceCD(interpretationContext
+				.getResources().get(0));
+		// TODO set change properties
+		// execute interpreter
+		 Interpreter interpreter = new Interpreter();
+		 interpreter.interprete(addCustomerToCustomerADC,
+		 interpretationContext);
+		 //save things to output
+		interpretationContext.saveResource(addCustomerToCustomerADC, outputURI + "addCustomerToCustomersA.change.xmi");
+		interpretationContext.saveResource(interpretationContext.getResources().get(0), outputURI + "customersA.xmi");
+		interpretationContext.saveResource(interpretationContext.getResources().get(1), outputURI + "customersB.xmi");
+		// check result
+
 	}
+	
+    private void register(EPackage ePackage) {
+        String nsURI = ePackage.getNsURI();
+        System.out.println("LoadTest.register() " + nsURI);
+        EPackage.Registry.INSTANCE.put(nsURI, ePackage);
+        EList<EPackage> eSubpackages = ePackage.getESubpackages();
+        for (EPackage subPackage : eSubpackages) {
+            register(subPackage);
+        }
+    } 
 }
