8e25bd8868ce027e0163ff38f3797b51c8d88a7a
Jan Reimann <janreimone@gmail.com>
2010-03-31 12:15:12 +0000

    -determined in resolver in which context PLUS or MINUS is used -> when + is just used as prefix of a term then it won't be printed

--- org.emftext.language.pl0.resource.pl0/src/org/emftext/language/pl0/resource/pl0/analysis/Pl0PLUS_MINUSTokenResolver.java
+++ org.emftext.language.pl0.resource.pl0/src/org/emftext/language/pl0/resource/pl0/analysis/Pl0PLUS_MINUSTokenResolver.java
@@ -6,13 +6,21 @@
  */
 package org.emftext.language.pl0.resource.pl0.analysis;
 
+import org.emftext.language.pl0.PlusMinusOperator;
+import org.emftext.language.pl0.TermExpression;
+
 public class Pl0PLUS_MINUSTokenResolver implements org.emftext.language.pl0.resource.pl0.IPl0TokenResolver {
 	
 	private org.emftext.language.pl0.resource.pl0.analysis.Pl0DefaultTokenResolver defaultTokenResolver = new org.emftext.language.pl0.resource.pl0.analysis.Pl0DefaultTokenResolver();
 	
 	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
-		return result;
+//		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
+		if(container instanceof TermExpression){
+			if(value == PlusMinusOperator.PLUS){
+				return "";
+			} 
+		}
+		return defaultTokenResolver.deResolve(value, feature, container);
 	}
 	
 	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.pl0.resource.pl0.IPl0TokenResolveResult result) {
--- org.emftext.language.pl0/src-gen/org/emftext/language/pl0/impl/ProcedureDeclarationImpl.java
+++ org.emftext.language.pl0/src-gen/org/emftext/language/pl0/impl/ProcedureDeclarationImpl.java
@@ -7,11 +7,15 @@
 package org.emftext.language.pl0.impl;
 
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
 
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
 
+import org.emftext.language.pl0.Block;
 import org.emftext.language.pl0.PL0Package;
 import org.emftext.language.pl0.ProcedureDeclaration;
 
