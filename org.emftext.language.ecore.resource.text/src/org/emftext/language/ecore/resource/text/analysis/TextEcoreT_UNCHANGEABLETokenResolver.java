a2af8137b5826e0ccc18036eef3cb182dfb98810
Christian Wende <christian.wende@devboost.de>
2010-04-14 23:38:18 +0000

    fixed changeable implementation

---
+++ org.emftext.language.ecore.resource.text/src/org/emftext/language/ecore/resource/text/analysis/TextEcoreT_UNCHANGEABLETokenResolver.java
@@ -0,0 +1,40 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.emftext.language.ecore.resource.text.analysis;
+
+import java.util.Map;
+
+import org.emftext.language.ecore.resource.text.ITextEcoreTokenResolveResult;
+
+public class TextEcoreT_UNCHANGEABLETokenResolver implements org.emftext.language.ecore.resource.text.ITextEcoreTokenResolver {
+	
+	private org.emftext.language.ecore.resource.text.analysis.TextEcoreDefaultTokenResolver defaultTokenResolver = new org.emftext.language.ecore.resource.text.analysis.TextEcoreDefaultTokenResolver();
+	
+	public java.lang.String deResolve(java.lang.Object value,
+			org.eclipse.emf.ecore.EStructuralFeature feature,
+			org.eclipse.emf.ecore.EObject container) {
+		if ((Boolean) value) {
+			return "";
+		} else {
+			return "unchangeable";
+		}
+	}
+
+	public void resolve(java.lang.String lexem,
+			org.eclipse.emf.ecore.EStructuralFeature feature,
+			ITextEcoreTokenResolveResult result) {
+		if ("unchangeable".equalsIgnoreCase(lexem)) {
+			result.setResolvedToken(false);
+		} else {
+			result.setResolvedToken(true);
+		}
+	}
+
+	public void setOptions(Map<?, ?> options) {
+	}
+	
+}
