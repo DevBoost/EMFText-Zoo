d5813f9d446368f7b8e4a0080d67ba8fc6038ae9
Tan-Ky Hoang-Kim <s1010530@mail.inf.tu-dresden.de>
2010-04-22 00:47:37 +0000

    added a test: add a class diagram

--- org.emftext.language.rolecore.dependencies.test/src/org/emftext/language/rolecore/dependencies/test/ChangeDescriptionCreator.java
+++ org.emftext.language.rolecore.dependencies.test/src/org/emftext/language/rolecore/dependencies/test/ChangeDescriptionCreator.java
@@ -5,21 +5,33 @@ import java.util.List;
 import java.util.Map.Entry;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EFactory;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.change.ChangeDescription;
 import org.eclipse.emf.ecore.change.ChangeFactory;
 import org.eclipse.emf.ecore.change.FeatureChange;
 import org.eclipse.emf.ecore.change.ListChange;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.emftext.language.rolecore.blockclassbase.BlockclassbaseFactory;
+import org.emftext.language.rolecore.blockclassbase.BlockclassbasePackage;
+import org.emftext.language.rolecore.blockclassbase.Name;
+import org.emftext.language.rolecore.blockdomain.BlockDiagram;
+import org.emftext.language.rolecore.blockdomain.BlockdomainFactory;
+import org.emftext.language.rolecore.blockdomain.BlockdomainPackage;
+import org.emftext.language.rolecore.blockdomain.SystemBlock;
+import org.emftext.language.rolecore.classdomain.ClassDiagram;
+import org.emftext.language.rolecore.classdomain.ClassdomainFactory;
 import org.emftext.language.rolecore.dependencies.interpreter.DomainRoot;
 import org.emftext.language.rolecore.dependencies.interpreter.InterpretationContext;
 import org.emftext.language.rolecore.dependencies.interpreter.InterpreterPackage;
 import org.emftext.language.rolecore.families.FamiliesPackage;
 import org.emftext.language.rolecore.families.FamilyDomainCore;
+import org.emftext.language.rolecore.interfaces.RCCore;
 import org.emftext.language.rolecore.riehle97base.Riehle97basePackage;
 
 public class ChangeDescriptionCreator {
@@ -31,9 +43,111 @@ public class ChangeDescriptionCreator {
 		context = interpretationContext;
 		this.inputURI = inputURI;
 	}
+
+	private void addAttributeToRole(EObject role, Object attribute, ChangeDescription changeDescription) {
+		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(role);
+		String featureName = role.eClass().getName();
+		featureName = featureName.substring(0, 1).toLowerCase() + featureName.substring(1);
+		FeatureChange featureChange = null;
+		for (EAttribute feature : role.eClass().getEAttributes()) {
+			if (feature.getName().equals(featureName)) {
+				featureChange = ChangeFactory.eINSTANCE.createFeatureChange(feature, attribute, true);
+				break;
+			}
+		}
+		if (featureChange == null) {
+			return;
+		}
+		changeDescription.getObjectChanges().add(entry);
+		entry.getValue().add(featureChange);
+	}
+
+	private void addCoresToChangeDescription(EObject core, ChangeDescription cd) {
+		if (core == null) {
+			return;
+		}
+		cd.getObjectsToAttach().add(core);
+		Object coreObject = core.eGet(context.getCoreStructuralFeature());
+		if (coreObject instanceof RCCore) {
+			addCoresToChangeDescription((EObject) coreObject, cd);
+		}
+	}
+
+	private void addCoreToDomainRoot(DomainRoot domainRoot, EObject coreClass, ChangeDescription changeDescription) {
+		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(domainRoot);
+		FeatureChange featureChange = ChangeFactory.eINSTANCE.createFeatureChange();
+		ListChange listChange = ChangeFactory.eINSTANCE.createListChange();
+		changeDescription.getObjectChanges().add(entry);
+		entry.getValue().add(featureChange);
+		featureChange.setFeature(InterpreterPackage.eINSTANCE.getDomainRoot_EObjects());
+		featureChange.getListChanges().add(listChange);
+		listChange.getReferenceValues().add(coreClass);
+		addCoresToChangeDescription(coreClass, changeDescription);
+	}
+
+	private void addCoreToRole(EObject core, EObject role, ChangeDescription changeDescription) {
+		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(role);
+		FeatureChange featureChange = ChangeFactory.eINSTANCE.createFeatureChange();
+		changeDescription.getObjectChanges().add(entry);
+		entry.getValue().add(featureChange);
+		featureChange.setFeature(context.getCoreStructuralFeature());
+		featureChange.setReferenceValue(core);
+		// assume the core existed in the resource, no add to change description
+	}
+
+	// TODO test
+	private void addReferenceToRole(EObject role, EObject referenceCore, ChangeDescription changeDescription) {
+		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(role);
+		FeatureChange featureChange = ChangeFactory.eINSTANCE.createFeatureChange();
+		// ListChange listChange = ChangeFactory.eINSTANCE.createListChange();
+		// assumed a reference role has exact one reference for the core class
+		EStructuralFeature structuralFeature = role.eClass().getEReferences().get(0);
+		changeDescription.getObjectChanges().add(entry);
+		entry.getValue().add(featureChange);
+		featureChange.setFeature(structuralFeature);
+		featureChange.setReferenceValue(referenceCore);
+		if (referenceCore.eContainer() == null) {
+			changeDescription.getObjectsToAttach().add(referenceCore);
+		}
+	}
+
+	private void addRoleToCore(EObject role, EObject core, ChangeDescription changeDescription) {
+		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(core);
+		FeatureChange featureChange = ChangeFactory.eINSTANCE.createFeatureChange();
+		ListChange listChange = ChangeFactory.eINSTANCE.createListChange();
+		changeDescription.getObjectChanges().add(entry);
+		entry.getValue().add(featureChange);
+		featureChange.setFeature(context.getRolesStructuralFeature());
+		featureChange.getListChanges().add(listChange);
+		listChange.getReferenceValues().add(role);
+		changeDescription.getObjectsToAttach().add(role);
+	}
+
+	private void addRoleToCoreAndReverse(EObject role, EObject core, ChangeDescription changeDescription) {
+		addRoleToCore(role, core, changeDescription);
+		addCoreToRole(core, role, changeDescription);
+	}
+
+	public ChangeDescription createAddClassDiagram(String domainName) {
+		DomainRoot dr = context.findOrCreateDomainRoot(domainName);
+		ClassDiagram classDiagram = ClassdomainFactory.eINSTANCE.createClassDiagramCore();
+		ChangeDescription cd = ChangeFactory.eINSTANCE.createChangeDescription();
+		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(dr);
+		FeatureChange fc = ChangeFactory.eINSTANCE.createFeatureChange();
+		ListChange lc = ChangeFactory.eINSTANCE.createListChange();
+		cd.getObjectChanges().add(entry);
+		entry.getValue().add(fc);
+		fc.setFeature(InterpreterPackage.eINSTANCE.getDomainRoot_EObjects());
+		fc.getListChanges().add(lc);
+		lc.getReferenceValues().add(classDiagram);
+		cd.getObjectsToAttach().add(classDiagram);
+		return cd;
+	}
+
 	/**
 	 * Create a change description for the given resource which add a Customer.
 	 * Save this change description in the inputURI.
+	 * 
 	 * @param cResource
 	 * @return
 	 */
@@ -41,7 +155,7 @@ public class ChangeDescriptionCreator {
 		DomainRoot domainRoot = (DomainRoot) cResource.getContents().get(0);
 		Riehle97basePackage cEPackage = Riehle97basePackage.eINSTANCE;
 		EObject customerCore = context.createCoreClass(cEPackage.getCustomerCore());
-		
+
 		ChangeDescription cd = ChangeFactory.eINSTANCE.createChangeDescription();
 		Entry<EObject, EList<FeatureChange>> entry = ChangeFactory.eINSTANCE.createEObjectToChangesMapEntry(domainRoot);
 		FeatureChange fc = ChangeFactory.eINSTANCE.createFeatureChange();
@@ -55,16 +169,17 @@ public class ChangeDescriptionCreator {
 		context.saveResource(cd, inputURI + "addCustomerToCustomersA.change.xmi");
 		return cd;
 	}
+
 	/**
 	 * TODO recode
 	 */
 	@SuppressWarnings("unused")
 	private void createAddFamilyChangeDescription() {
 		FamiliesPackage familiesEPackage = (FamiliesPackage) EPackage.Registry.INSTANCE
-		.getEPackage("http://www.emftext.org/language/rolecore/families");
+				.getEPackage("http://www.emftext.org/language/rolecore/families");
 		List<EObject> eObjectList = new ArrayList<EObject>();
 		Resource familiesResource = context.saveResource(eObjectList, inputURI + "families.xmi");
-		
+
 		FamilyDomainCore familyDomainCore = (FamilyDomainCore) familiesResource.getContents().get(0);
 		FamiliesPackage ePackage = (FamiliesPackage) EPackage.Registry.INSTANCE
 				.getEPackage("http://www.emftext.org/language/rolecore/families");
@@ -99,8 +214,42 @@ public class ChangeDescriptionCreator {
 		context.saveResource(eObjectList, inputURI + "addFamily.change");
 	}
 
+	public ChangeDescription createSystemBlock(String domainName) {
+		DomainRoot dr = context.findOrCreateDomainRoot(domainName);
+		BlockDiagram bd = null;
+		for (EObject eObject : dr.getEObjects()) {
+			if (eObject instanceof BlockDiagram) {
+				bd = (BlockDiagram) eObject;
+				break;
+			}
+		}
+		SystemBlock sb = (SystemBlock) context.createCoreClass(BlockdomainPackage.eINSTANCE.getSystemBlockCore());
+		Name name = BlockclassbaseFactory.eINSTANCE.createName();
+		EObject mebdRole = BlockclassbaseFactory.eINSTANCE.createModelelementBlockdiagramRole();
+		EObject bdeRole = BlockdomainFactory.eINSTANCE.createBlockdiagramElementsRole();
+
+		ChangeDescription cd = ChangeFactory.eINSTANCE.createChangeDescription();
+		// add SystemBlock to DomainRoot
+		addCoreToDomainRoot(dr, sb, cd);
+		// add role Name to SystemBlock and SystemBlock to role Name
+		EObject modelElementCore = getSuperCore(sb, BlockclassbasePackage.eINSTANCE.getModelElementCore());
+		addRoleToCoreAndReverse(name, modelElementCore, cd);
+		// set attribute Name
+		addAttributeToRole(name, "ProSys", cd);
+		// add ModelelementBlockdiagramRole to SystemBlock and reverse
+		addRoleToCoreAndReverse(mebdRole, modelElementCore, cd);
+		// add BlockDiagram to ModelelementBlockdiagramRole
+		addReferenceToRole(mebdRole, bd, cd);
+		// add BlockdiagramElementsRole to BlockDiagram
+		addRoleToCoreAndReverse(bdeRole, bd, cd);
+		// add SystemBlock to BlockdiagramElementsRole
+		addReferenceToRole(bdeRole, sb, cd);
+		return cd;
+	}
+
 	/**
 	 * TODO recode
+	 * 
 	 * @param coreClass
 	 * @param roleEClass
 	 * @param eObject
@@ -110,7 +259,7 @@ public class ChangeDescriptionCreator {
 	public ChangeDescription getAddRoleToCoreChangeDescription(EObject coreClass, EClass roleEClass, EObject eObject,
 			String featureName) {
 		EFactory eFactory = roleEClass.getEPackage().getEFactoryInstance();
-		EClass coreOfRoleEClass = (EClass) context.getCoreStructuralFeature(roleEClass).getEType();
+		EClass coreOfRoleEClass = (EClass) context.getCoreStructuralFeature().getEType();
 		if (coreClass.eClass().equals(coreOfRoleEClass)) {
 			// Add a role to the core class, multiple valued
 			EObject roleClass = eFactory.create(roleEClass);
@@ -120,7 +269,7 @@ public class ChangeDescriptionCreator {
 					.createEObjectToChangesMapEntry(coreClass);
 			changeDescription.getObjectChanges().add(roleToCoreEntry);
 			FeatureChange roleToCoreFeatureChange = ChangeFactory.eINSTANCE.createFeatureChange();
-			roleToCoreFeatureChange.setFeature(context.getRolesStructuralFeature(coreClass.eClass()));
+			roleToCoreFeatureChange.setFeature(context.getRolesStructuralFeature());
 			roleToCoreEntry.getValue().add(roleToCoreFeatureChange);
 			ListChange listChange = ChangeFactory.eINSTANCE.createListChange();
 			listChange.getReferenceValues().add(roleClass);
@@ -131,7 +280,7 @@ public class ChangeDescriptionCreator {
 			Entry<EObject, EList<FeatureChange>> coreToRoleEntry = ChangeFactory.eINSTANCE
 					.createEObjectToChangesMapEntry(roleClass);
 			FeatureChange coreToRoleFeatureChange = ChangeFactory.eINSTANCE.createFeatureChange();
-			coreToRoleFeatureChange.setFeature(context.getCoreStructuralFeature(roleEClass));
+			coreToRoleFeatureChange.setFeature(context.getCoreStructuralFeature());
 			coreToRoleFeatureChange.setReferenceValue(coreClass);
 			changeDescription.getObjectChanges().add(coreToRoleEntry);
 			coreToRoleEntry.getValue().add(coreToRoleFeatureChange);
@@ -144,12 +293,23 @@ public class ChangeDescriptionCreator {
 			}
 			return changeDescription;
 		} else {
-			EStructuralFeature coreStructuralFeature = context.getCoreStructuralFeature(coreClass.eClass());
+			EStructuralFeature coreStructuralFeature = context.getCoreStructuralFeature();
 			if (coreStructuralFeature == null) {
 				return null;
 			}
 			EObject superCoreClass = (EObject) coreClass.eGet(coreStructuralFeature);
 			return getAddRoleToCoreChangeDescription(superCoreClass, roleEClass, eObject, featureName);
 		}
+	}
+
+	private EObject getSuperCore(EObject coreClass, EClass superCoreEClass) {
+		if (coreClass.eClass().equals(superCoreEClass)) {
+			return coreClass;
+		}
+		EObject superCoreClass = (EObject) coreClass.eGet(context.getCoreStructuralFeature());
+		if (superCoreClass != null) {
+			return getSuperCore(superCoreClass, superCoreEClass);
+		}
+		return null;
 	}
 }
--- org.emftext.language.rolecore.dependencies.test/src/org/emftext/language/rolecore/dependencies/test/InterpreterTest.java
+++ org.emftext.language.rolecore.dependencies.test/src/org/emftext/language/rolecore/dependencies/test/InterpreterTest.java
@@ -1,15 +1,26 @@
 package org.emftext.language.rolecore.dependencies.test;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import java.util.ArrayList;
+import java.util.List;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.change.ChangeDescription;
 import org.eclipse.emf.ecore.change.ChangePackage;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.emftext.language.rolecore.RolecorePackage;
+import org.emftext.language.rolecore.blockclassbase.BlockclassbasePackage;
+import org.emftext.language.rolecore.blockclassbase.Name;
+import org.emftext.language.rolecore.blockdomain.BlockDiagramCore;
+import org.emftext.language.rolecore.blockdomain.BlockdomainPackage;
+import org.emftext.language.rolecore.classdomain.ClassDiagramCore;
+import org.emftext.language.rolecore.classdomain.ClassdomainPackage;
+import org.emftext.language.rolecore.classdomain.StereotypeCore;
 import org.emftext.language.rolecore.dependencies.Graph;
 import org.emftext.language.rolecore.dependencies.interpreter.DomainRoot;
 import org.emftext.language.rolecore.dependencies.interpreter.EqualTraceLink;
@@ -28,6 +39,12 @@ import junit.framework.TestCase;
 
 public class InterpreterTest extends TestCase {
 
+	private String inputURI = "./input/";
+	private String outputURI = "./output/";
+	private ChangeDescriptionCreator cdCreator;
+	private InterpretationContext context;
+	private List<Graph> dependencies;
+	
 	public void setUp() {
 		// register the Factories
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
@@ -37,61 +54,200 @@ public class InterpreterTest extends TestCase {
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("change", new XMIResourceFactoryImpl());
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
 		// register the Packages
-		register(FamiliesPackage.eINSTANCE);
-		register(PersonsPackage.eINSTANCE);
+		/*register(FamiliesPackage.eINSTANCE);
+		register(PersonsPackage.eINSTANCE);*/
 		register(Riehle97Package.eINSTANCE);
 		register(InterpreterPackage.eINSTANCE);
 		register(ChangePackage.eINSTANCE);
+		register(BlockdomainPackage.eINSTANCE);
+		register(ClassdomainPackage.eINSTANCE);
+		register(BlockclassbasePackage.eINSTANCE);
+		register(RolecorePackage.eINSTANCE);
+		context = new InterpretationContext();
+		cdCreator = new ChangeDescriptionCreator(context, inputURI);
+		dependencies = new ArrayList<Graph>();
 	}
 
-	public void testInterpretation() {
-		InterpretationContext interpretationContext = new InterpretationContext();
-		String inputURI = "./input/";
-		String outputURI = "./output/";
-		// create the source to test, can be removed after getting the resources
-		ChangeDescriptionCreator cdCreator = new ChangeDescriptionCreator(interpretationContext, inputURI);
-
+	public void testAddCustomer() {
 		// load instance models
-		interpretationContext.addResource(interpretationContext.loadResource(inputURI + "customersA.xmi"));
-		interpretationContext.addResource(interpretationContext.loadResource(inputURI + "customersB.xmi"));
+		Resource customerAResource = context.loadResource(inputURI + "customersA.xmi");
+		Resource customerBResource = context.loadResource(inputURI + "customersB.xmi");
+		context.addResource(customerAResource);
+		context.addResource(customerBResource);
 
 		// load dependency models
-		Map<String, Graph> dependencies = new HashMap<String, Graph>();
-		dependencies.put("customer", (Graph) interpretationContext.loadResource(inputURI + "customer.dependencies")
-				.getContents().get(0));
-		interpretationContext.setDependencies(dependencies);
+		dependencies.add((Graph) context.loadResource(inputURI + "customer.dependencies").getContents()
+				.get(0));
+		context.setDependencies(dependencies);
 		// create change in memory (http://www.eclipse.org/emf/2003/Change)
-		ChangeDescription addCustomerToCustomerADC = cdCreator.createAddCustomerToResourceCD(interpretationContext
-				.getResources().get(0));
+		ChangeDescription addCustomerToCustomerADC = cdCreator.createAddCustomerToResourceCD(customerAResource);
 		// execute interpreter
 		Interpreter interpreter = new Interpreter();
-		if (interpreter.interprete(addCustomerToCustomerADC, interpretationContext)) {
+		if (interpreter.interprete(addCustomerToCustomerADC, context)) {
 			System.out.println("Synchronization is successful!");
 		}
 		// save things to output
-		interpretationContext.saveResource(addCustomerToCustomerADC, outputURI + "addCustomerToCustomersA.change.xmi");
-		interpretationContext.saveResource(interpretationContext.getResources().get(0), outputURI + "customersA.xmi");
-		interpretationContext.saveResource(interpretationContext.getResources().get(1), outputURI + "customersB.xmi");
-		interpretationContext.saveResource(interpretationContext.findOrCreateTraceLinksDomainRoot(), outputURI
+		context.saveResource(addCustomerToCustomerADC, outputURI + "addCustomerToCustomersA.change.xmi");
+		context.saveResource(customerAResource, outputURI + "customersA.xmi");
+		context.saveResource(customerBResource, outputURI + "customersB.xmi");
+		context.saveResource(context.findOrCreateTraceLinksDomainRoot(), outputURI
 				+ "customersABTraceLinks.xmi");
 		// check result
-		DomainRoot customersADR = interpretationContext.findOrCreateDomainRoot("customersA");
-		DomainRoot customersBDR = interpretationContext.findOrCreateDomainRoot("customersB");
-		DomainRoot traceLinksDR = interpretationContext.findOrCreateTraceLinksDomainRoot();
+		DomainRoot customersADR = context.findOrCreateDomainRoot("customersA");
+		DomainRoot customersBDR = context.findOrCreateDomainRoot("customersB");
+		DomainRoot traceLinksDR = context.findOrCreateTraceLinksDomainRoot();
 		assertNotNull("Domain root customersA doesn't exist", customersADR);
 		assertNotNull("Domain root customersB doesn't exist", customersBDR);
 		assertNotNull("Domain root trace links doens't exist", traceLinksDR);
-		assertTrue("customersA has no elements", customersADR.getEObjects().size()>0);
+		assertTrue("customersA has no elements", customersADR.getEObjects().size() > 0);
 		assertNotNull("CustomerCore is not in customersA", customersADR.getEObjects().get(0));
 		EObject customerA = customersADR.getEObjects().get(0);
-		assertTrue("customersB has no elements", customersBDR.getEObjects().size()>0);
+		assertTrue("customersB has no elements", customersBDR.getEObjects().size() > 0);
 		assertNotNull("CustomerCore is not in customersB", customersBDR.getEObjects().get(0));
 		EObject customerB = customersBDR.getEObjects().get(0);
 		assertNotNull("A trace link doesn't exist", traceLinksDR.getEObjects().get(0));
-		assertTrue("The first object is not an equal trace link", traceLinksDR.getEObjects().get(0) instanceof EqualTraceLink);
+		assertTrue("The first object is not an equal trace link",
+				traceLinksDR.getEObjects().get(0) instanceof EqualTraceLink);
 		EqualTraceLink equalTraceLink = (EqualTraceLink) traceLinksDR.getEObjects().get(0);
 		assertEquals(customerA, equalTraceLink.getSource());
 		assertEquals(customerB, equalTraceLink.getTarget());
+	}
+
+	public void testAddClassDiagram() {
+		// load dependency models
+		// TODO add more dependencies
+		dependencies.add((Graph) context.loadResource(inputURI + "bctest01.dependencies").getContents()
+				.get(0));
+		context.setDependencies(dependencies);
+		// create change in memory (http://www.eclipse.org/emf/2003/Change)
+		ChangeDescription addClassDiagramDC = cdCreator.createAddClassDiagram("classDomain");
+		// execute interpreter
+		Interpreter interpreter = new Interpreter();
+		if (interpreter.interprete(addClassDiagramDC, context)) {
+			System.out.println("Synchronization is successful!");
+		}
+		// save things to output
+		saveDomainRootsToResources(context, outputURI, "Scene01");
+		// check result
+		DomainRoot classDomainDR = context.findOrCreateDomainRoot("classDomain");
+		DomainRoot blockDomainDR = context.findOrCreateDomainRoot("blockDomain");
+		DomainRoot traceLinksDR = context.findOrCreateTraceLinksDomainRoot();
+		assertNotNull(classDomainDR);
+		assertNotNull(blockDomainDR);
+		assertNotNull(traceLinksDR);
+		assertTrue(classDomainDR.getEObjects().size() > 0);
+		assertNotNull(classDomainDR.getEObjects().get(0));
+		EObject classDiagram = classDomainDR.getEObjects().get(0);
+		assertTrue(classDiagram instanceof ClassDiagramCore);
+		assertTrue(blockDomainDR.getEObjects().size() > 0);
+		assertNotNull(blockDomainDR.getEObjects().get(0));
+		EObject blockDiagram = blockDomainDR.getEObjects().get(0);
+		assertTrue(blockDiagram instanceof BlockDiagramCore);
+		assertNotNull(traceLinksDR.getEObjects().get(0));
+		assertTrue(traceLinksDR.getEObjects().get(0) instanceof EqualTraceLink);
+		EqualTraceLink equalTraceLink = (EqualTraceLink) traceLinksDR.getEObjects().get(0);
+		// this is based on dependencies specification
+		assertEquals(blockDiagram, equalTraceLink.getSource());
+		assertEquals(classDiagram, equalTraceLink.getTarget());
+	}
+	
+	public void testAddSystemBlock(){
+		//load resources
+		context.addResource(context.loadResource(inputURI+"Scene02blockDomain.xmi"));
+		context.addResource(context.loadResource(inputURI+"Scene02classDomain.xmi"));
+		context.addResource(context.loadResource(inputURI+"Scene02TraceLinks.xmi"));
+		// load dependency models
+		// TODO test more dependencies
+		dependencies.add((Graph) context.loadResource(inputURI + "bctest01.dependencies").getContents()
+				.get(0));
+		dependencies.add((Graph) context.loadResource(inputURI + "bctest02.dependencies").getContents()
+				.get(0));
+		context.setDependencies(dependencies);
+		// create change in memory (http://www.eclipse.org/emf/2003/Change)
+		// domain name is in the dependencies
+		ChangeDescription addSystemBlockDC = cdCreator.createSystemBlock("blockDomain");
+		// execute interpreter
+		Interpreter interpreter = new Interpreter();
+		if (interpreter.interprete(addSystemBlockDC, context)) {
+			System.out.println("Synchronization is successful!");
+		}
+		// save domain roots to output, there is a base resource
+		saveDomainRootsToResources(context, outputURI, "Scene02");
+		// check result
+		DomainRoot classDomainDR = context.findOrCreateDomainRoot("classDomain");
+		DomainRoot blockDomainDR = context.findOrCreateDomainRoot("blockDomain");
+		DomainRoot traceLinksDR = context.findOrCreateTraceLinksDomainRoot();
+		EObject stereotype = getCoreEObject(classDomainDR, ClassdomainPackage.eINSTANCE.getStereotypeCore());
+		assertNotNull(stereotype);
+		EClass name = BlockclassbasePackage.eINSTANCE.getName_();
+		Name stereotypeName = (Name) getRoleOf(stereotype, name); 
+		assertNotNull(stereotypeName);
+		assertEquals("system", stereotypeName.getName());
+		EObject classEObject = getCoreEObject(classDomainDR, ClassdomainPackage.eINSTANCE.getClassCore());
+		assertNotNull(classEObject);
+		EObject systemBlock = getCoreEObject(blockDomainDR, BlockdomainPackage.eINSTANCE.getSystemBlockCore());
+		assertNotNull(systemBlock);
+		EObject synchSystemBlock = getSynchronizedObject(classEObject, traceLinksDR);
+		assertNotNull(synchSystemBlock);
+		assertEquals(systemBlock, synchSystemBlock);
+		EObject className = getRoleOf(classEObject, name);
+		assertNotNull(className);
+		EObject systemBlockName = getRoleOf(systemBlock, name);
+		assertEquals(className, systemBlockName);
+	}
+
+	private EObject getSynchronizedObject(EObject coreEObject, DomainRoot traceLinksDR) {
+		for (EObject eObject : traceLinksDR.getEObjects()) {
+			if(eObject instanceof EqualTraceLink){
+				EqualTraceLink link = (EqualTraceLink) eObject;
+				if (link.getSource().equals(coreEObject)){
+					return link.getTarget();
+				}
+				if (link.getTarget().equals(coreEObject)){
+					return link.getSource();
+				}
+			}
+		}
+		return null;
+	}
+
+	private EObject getRoleOf(EObject coreEObject, EClass role) {
+		List<EObject> roles = context.getRoleEObjects(coreEObject, role);
+		if (roles!=null&&roles.size()>0){
+			return roles.get(0);
+		}
+		return null;
+	}
+
+	private EObject getCoreEObject(DomainRoot domainRoot, EClass coreEClass) {
+		List<EObject> coreEObjects = getCoreEObjects(domainRoot, coreEClass);
+		if (coreEObjects.size()>0){
+			return coreEObjects.get(0);
+		}
+		return null;
+	}
+
+	private List<EObject> getCoreEObjects(DomainRoot classDomainDR, EClass coreEClass) {
+		List<EObject> coreEObjects = new ArrayList<EObject>();
+		for (EObject eObject : classDomainDR.getEObjects()) {
+			if (eObject.eClass().equals(coreEClass)){
+				coreEObjects.add(eObject);
+			}
+		}
+		return coreEObjects;
+	}
+
+	private void saveDomainRootsToResources(InterpretationContext context, String location, String prefix) {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		List<Resource> resources = new ArrayList<Resource>();
+		for (DomainRoot domainRoot : context.getDomainRoots()) {
+			Resource resource = resourceSet.createResource(URI.createFileURI(location + prefix + domainRoot.getName() + ".xmi"));
+			resource.getContents().add(domainRoot);
+			resources.add(resource);
+		}
+		for (Resource resource : resources) {
+			context.saveResource(resource, null);
+		}
 	}
 
 	private void register(EPackage ePackage) {
