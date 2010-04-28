110fef37c5604d0ea62eb153292b2cc004da6d9c
Mirko Seifert <mirko.seifert@devboost.de>
2010-04-28 15:23:57 +0000

    added initial syntax definition for timed automata

---
+++ org.emftext.language.timedautomata/metamodel/timedAutomata.cs
@@ -0,0 +1,29 @@
+SYNTAXDEF ta
+FOR <http://www.emftext.org/language/timedAutomata/core>
+START Project
+
+IMPORTS {
+	//org.emftext.language.timedAutomata.bnf:<bnf>
+	//org.emftext.language.timedAutomata.bnf.expressions:<expressions>
+	//org.emftext.language.timedAutomata.bnf.declarations:<declarations>
+	//org.emftext.language.timedAutomata.bnf.types:<types>
+	//org.emftext.language.timedAutomata.core:<core>
+	//org.emftext.language.simpleGraph:<http://www.emftext.org/language/simpleGraph>
+}
+
+OPTIONS {
+	reloadGeneratorModel = "true";
+	resourcePluginID = "org.emftext.language.timedautomata.resource.ta";
+	basePackage = "org.emftext.language.timedautomata.resource.ta";
+}
+
+TOKENS {
+	DEFINE COMMENT $'//'(~('\n'|'\r'|'\uffff'))*$;
+}
+
+RULES {
+	Project ::= "project" id[] templates*;
+	Template ::= "template" id[] locations* edges*;
+	Location ::= "location" id[];
+	Edge ::= "edge" id[] "from" source[] "to" target[];
+}
