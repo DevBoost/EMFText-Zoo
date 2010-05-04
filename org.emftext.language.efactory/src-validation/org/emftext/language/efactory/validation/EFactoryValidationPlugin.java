47f168558098a51da4db505791320173759369e8
Mirko Seifert <mirko.seifert@devboost.de>
2010-05-04 15:56:50 +0000

    removed unused class

--- org.emftext.language.efactory/src-validation/org/emftext/language/efactory/validation/EFactoryValidationPlugin.java
+++
@@ -1,7 +0,0 @@
-package org.emftext.language.efactory.validation;
-
-public class EFactoryValidationPlugin {
-
-	public static final String PLUGIN_ID = "org.emftext.language.efactory.validation";
-
-}
