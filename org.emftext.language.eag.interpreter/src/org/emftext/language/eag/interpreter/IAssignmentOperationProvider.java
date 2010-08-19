4d3c0245a83b9f9f6da3c9541c1d0c0673cd5ac6
Mirko Seifert <mirko.seifert@devboost.de>
2010-08-19 20:20:49 +0000

    cleaned up

--- org.emftext.language.eag.interpreter/src/org/emftext/language/eag/interpreter/IAssignmentOperationProvider.java
+++
@@ -1,6 +0,0 @@
-package org.emftext.language.eag.interpreter;
-
-import org.emftext.language.eag.AssignmentOperator;
-
-public interface IAssignmentOperationProvider extends IOperationProvider<AssignmentOperator> {
-}
