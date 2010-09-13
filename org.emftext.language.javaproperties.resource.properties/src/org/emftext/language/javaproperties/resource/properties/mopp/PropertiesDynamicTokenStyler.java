c9919d6aa093ddd6f13c8169c884342084876d6f
Mirko Seifert <mirko.seifert@devboost.de>
2010-09-13 19:11:43 +0000

    fixed token styling

--- org.emftext.language.javaproperties.resource.properties/src/org/emftext/language/javaproperties/resource/properties/mopp/PropertiesDynamicTokenStyler.java
+++ org.emftext.language.javaproperties.resource.properties/src/org/emftext/language/javaproperties/resource/properties/mopp/PropertiesDynamicTokenStyler.java
@@ -12,17 +12,24 @@ public class PropertiesDynamicTokenStyler {
 	 * This method is called to dynamically style tokens.
 	 * 
 	 * @param resource the TextResource that contains the token
-	 * @param tokenName the name of the token
-	 * @param tokenText the text of the token
+	 * @param token the token to obtain a style for
 	 * @param staticStyle the token style as set in the editor preferences (is
 	 * <code>null</code> if syntax highlighting for the token is disabled)
 	 */
-	public org.emftext.language.javaproperties.resource.properties.IPropertiesTokenStyle getDynamicTokenStyle(org.emftext.language.javaproperties.resource.properties.IPropertiesTextResource resource, String tokenName, String tokenText, org.emftext.language.javaproperties.resource.properties.IPropertiesTokenStyle staticStyle) {
+	public org.emftext.language.javaproperties.resource.properties.IPropertiesTokenStyle getDynamicTokenStyle(org.emftext.language.javaproperties.resource.properties.IPropertiesTextResource resource, org.emftext.language.javaproperties.resource.properties.IPropertiesTextToken token, org.emftext.language.javaproperties.resource.properties.IPropertiesTokenStyle staticStyle) {
+		String tokenName = token.getName();
+		String tokenText = token.getText();
+		boolean atBeginningOfLine = token.getColumn() == 0;
 		// The default implementation returns the static style without any changes. To
 		// implement dynamic token styling, set the overrideDynamicTokenStyler option to
 		// <code>false</code> and customize this method.
-		if ("VALUE".equals(tokenName) && tokenText.startsWith(" ")) {
+		boolean hasTrailingWhitespace = tokenText.startsWith(" ") || tokenText.startsWith("\t") || tokenText.startsWith("\f");
+		if ("VALUE".equals(tokenName) && hasTrailingWhitespace && atBeginningOfLine) {
 			System.out.println("PropertiesDynamicTokenStyler.getDynamicTokenStyle() " + tokenText);
+			return new PropertiesTokenStyle(
+				new int [] {0x07, 0xa8, 0xec}, 
+				null, 
+				true, false, false, false);
 		}
 		return staticStyle;
 	}
