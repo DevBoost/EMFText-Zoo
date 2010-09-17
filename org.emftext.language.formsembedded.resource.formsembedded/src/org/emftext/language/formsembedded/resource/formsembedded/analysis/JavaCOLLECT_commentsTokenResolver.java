e09a85913a89591ecf2b072ec08ca5a51dbab3bb
Christian Wende <christian.wende@devboost.de>
2010-09-17 09:09:48 +0000

    adding languages for models tutorial

---
+++ org.emftext.language.formsembedded.resource.formsembedded/src/org/emftext/language/formsembedded/resource/formsembedded/analysis/JavaCOLLECT_commentsTokenResolver.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.emftext.language.formsembedded.resource.formsembedded.analysis;
+
+public class JavaCOLLECT_commentsTokenResolver implements org.emftext.language.formsembedded.resource.formsembedded.IFormsembeddedTokenResolver {
+	
+	private org.emftext.language.java.resource.java.analysis.JavaCOLLECT_commentsTokenResolver importedResolver = new org.emftext.language.java.resource.java.analysis.JavaCOLLECT_commentsTokenResolver();
+	
+	public String deResolve(Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		String result = importedResolver.deResolve(value, feature, container);
+		return result;
+	}
+	
+	public void resolve(String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, final org.emftext.language.formsembedded.resource.formsembedded.IFormsembeddedTokenResolveResult result) {
+		importedResolver.resolve(lexem, feature, new org.emftext.language.java.resource.java.IJavaTokenResolveResult() {
+			public String getErrorMessage() {
+				return result.getErrorMessage();
+			}
+			
+			public Object getResolvedToken() {
+				return result.getResolvedToken();
+			}
+			
+			public void setErrorMessage(String message) {
+				result.setErrorMessage(message);
+			}
+			
+			public void setResolvedToken(Object resolvedToken) {
+				result.setResolvedToken(resolvedToken);
+			}
+			
+		});
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		importedResolver.setOptions(options);
+	}
+	
+}
