40a12fd32f9c76afe3e03ccf31cbad1eb5467fdc
Mirko Seifert <mirko.seifert@devboost.de>
2010-09-20 13:01:03 +0000

    simplified code

--- org.emftext.language.formsextension.resource.formsextension/src/org/emftext/language/formsextension/resource/formsextension/mopp/FormsextensionBuilder.java
+++ org.emftext.language.formsextension.resource.formsextension/src/org/emftext/language/formsextension/resource/formsextension/mopp/FormsextensionBuilder.java
@@ -6,26 +6,19 @@
  */
 package org.emftext.language.formsextension.resource.formsextension.mopp;
 
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.Collections;
-import java.util.HashMap;
 
-import org.eclipse.core.internal.resources.WorkspaceRoot;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.impl.PlatformResourceURIHandlerImpl.WorkbenchHelper;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.emftext.language.formsextension.ExtendedForm;
-import org.emftext.language.java.JavaClasspath;
-import org.emftext.language.java.JavaPackage;
+import org.emftext.language.formsextension.resource.formsextension.IFormsextensionBuilder;
 import org.emftext.language.java.classifiers.Class;
 import org.emftext.language.java.classifiers.ClassifiersFactory;
-import org.emftext.language.java.expressions.Expression;
-import org.emftext.language.java.expressions.ExpressionsFactory;
-import org.emftext.language.java.expressions.impl.ExpressionsFactoryImpl;
 import org.emftext.language.java.instantiations.InstantiationsFactory;
 import org.emftext.language.java.instantiations.NewConstructorCall;
 import org.emftext.language.java.members.ClassMethod;
@@ -37,17 +30,21 @@ import org.emftext.language.java.statements.ExpressionStatement;
 import org.emftext.language.java.statements.Statement;
 import org.emftext.language.java.statements.StatementsFactory;
 import org.emftext.language.java.types.ClassifierReference;
-import org.emftext.language.java.types.NamespaceClassifierReference;
 import org.emftext.language.java.types.TypesFactory;
 
-public class FormsextensionBuilder implements org.emftext.language.formsextension.resource.formsextension.IFormsextensionBuilder {
+public class FormsextensionBuilder implements IFormsextensionBuilder {
 	
-	public boolean isBuildingNeeded(org.eclipse.emf.common.util.URI uri) {
-		// change this to return true to enable building of all resources
+	public boolean isBuildingNeeded(URI uri) {
+		// return true to enable building of all resources
 		return true;
 	}
-	public org.eclipse.core.runtime.IStatus build(org.emftext.language.formsextension.resource.formsextension.mopp.FormsextensionResource resource, org.eclipse.core.runtime.IProgressMonitor monitor) {
-		if (resource.getWarnings().size() + resource.getErrors().size() > 0) return org.eclipse.core.runtime.Status.CANCEL_STATUS; 
+	
+	public IStatus build(FormsextensionResource resource, IProgressMonitor monitor) {
+		
+		if (resource.getWarnings().size() + resource.getErrors().size() > 0) {
+			return org.eclipse.core.runtime.Status.CANCEL_STATUS; 
+		}
+		
 		EObject root = resource.getContents().get(0);
 		if (root instanceof ExtendedForm) {
 			ExtendedForm eform = (ExtendedForm) root;
@@ -74,16 +71,13 @@ public class FormsextensionBuilder implements org.emftext.language.formsextensio
 			IFile file = WorkspaceSynchronizer.getFile(resource);
 			String uri = file.getProjectRelativePath().toString();
 			stringExpression.setValue(uri);
-			methodCall.getArguments().add(stringExpression );
+			methodCall.getArguments().add(stringExpression);
 			
 			constructorCall.setNext(methodCall);
 			
-			
 			try {
-				
 				javaMethod.eResource().save(Collections.EMPTY_MAP);
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 				return org.eclipse.core.runtime.Status.CANCEL_STATUS;
 			}
@@ -91,5 +85,4 @@ public class FormsextensionBuilder implements org.emftext.language.formsextensio
 		
 		return org.eclipse.core.runtime.Status.OK_STATUS;
 	}
-	
 }
