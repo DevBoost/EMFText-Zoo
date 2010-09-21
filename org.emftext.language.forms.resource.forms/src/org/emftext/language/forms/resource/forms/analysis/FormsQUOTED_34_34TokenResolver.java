69f820a6774d988d379eea1cf16a5948378e017f
Mirko Seifert <mirko.seifert@devboost.de>
2010-09-21 18:30:03 +0000

    reverted syntax changes from revision 11235 (using REDEFINE operator in importing syntax instead)

--- org.emftext.language.forms.resource.forms/src/org/emftext/language/forms/resource/forms/analysis/FormsQUOTED_34_34TokenResolver.java
+++ org.emftext.language.forms.resource.forms/src/org/emftext/language/forms/resource/forms/analysis/FormsQUOTED_34_34TokenResolver.java
@@ -1,17 +1,25 @@
-/**
- * <copyright>
- * </copyright>
- *
+/*******************************************************************************
+ * Copyright (c) 2006-2010 
+ * Software Technology Group, Dresden University of Technology
  * 
- */
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany 
+ *      - initial API and implementation
+ ******************************************************************************/
+
 package org.emftext.language.forms.resource.forms.analysis;
 
-public class FormsSTRING_LITERALTokenResolver implements org.emftext.language.forms.resource.forms.IFormsTokenResolver {
+public class FormsQUOTED_34_34TokenResolver implements org.emftext.language.forms.resource.forms.IFormsTokenResolver {
 	
 	private org.emftext.language.forms.resource.forms.analysis.FormsDefaultTokenResolver defaultTokenResolver = new org.emftext.language.forms.resource.forms.analysis.FormsDefaultTokenResolver();
 	
 	public String deResolve(Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = defaultTokenResolver.deResolve(value, feature, container);
+		String result = defaultTokenResolver.deResolve(value, feature, container);
 		result += "\"";
 		result = "\"" + result;
 		return result;
--- org.emftext.language.forms.resource.forms/src/org/emftext/language/forms/resource/forms/analysis/FormsTEXTTokenResolver.java
+++ org.emftext.language.forms.resource.forms/src/org/emftext/language/forms/resource/forms/analysis/FormsTEXTTokenResolver.java
@@ -1,12 +1,20 @@
-/**
- * <copyright>
- * </copyright>
- *
+/*******************************************************************************
+ * Copyright (c) 2006-2010 
+ * Software Technology Group, Dresden University of Technology
  * 
- */
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Software Technology Group - TU Dresden, Germany 
+ *      - initial API and implementation
+ ******************************************************************************/
+
 package org.emftext.language.forms.resource.forms.analysis;
 
-public class FormsIDENTIFIERTokenResolver implements org.emftext.language.forms.resource.forms.IFormsTokenResolver {
+public class FormsTEXTTokenResolver implements org.emftext.language.forms.resource.forms.IFormsTokenResolver {
 	
 	private org.emftext.language.forms.resource.forms.analysis.FormsDefaultTokenResolver defaultTokenResolver = new org.emftext.language.forms.resource.forms.analysis.FormsDefaultTokenResolver();
 	
