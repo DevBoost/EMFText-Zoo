a9cd3e9fb0f0f600940b95768ccdaf5cffbbfd62
Mirko Seifert <mirko.seifert@devboost.de>
2010-09-27 10:34:31 +0000

    refactored token styles formatted syntax rules

--- org.emftext.language.forms/metamodel/forms.cs
+++ org.emftext.language.forms/metamodel/forms.cs
@@ -28,26 +28,22 @@ TOKENS {
 
 TOKENSTYLES {
 	"TEXT" COLOR #da0000;
-	"FORM" COLOR #000000, BOLD;
-	"ITEM" COLOR #000000, BOLD;
-	"CHOICE" COLOR #000000, BOLD;
-	"ONLY" COLOR #da0000, BOLD;
-	"IF" COLOR #da0000, BOLD;
-	"DATE" COLOR #000000, BOLD;
-	"FREETEXT" COLOR #000000, BOLD;
-	"NUMBER" COLOR #000000, BOLD;
-	"DECISION" COLOR #000000, BOLD;
-	"GROUP" COLOR #000000, BOLD;
+	"FORM", "ITEM", "CHOICE", "DATE", "FREETEXT", "NUMBER", "DECISION", "GROUP" COLOR #000000, BOLD;
+	"ONLY", "IF" COLOR #da0000, BOLD;
 }
-  
+
 RULES {
 	Form ::= "FORM" caption['"','"'] !1 groups*;
 	Group ::= !0 "GROUP" name['"','"'] !0 items*;
-	Item ::= "ITEM" text['"','"'] ( explanation['"','"'] )? ("ONLY" "IF" dependentOf[])? ":" itemType !0;
-	Choice ::= "CHOICE" (multiple[MULTIPLE])? "(" options ("," options)* ")";
-	Option ::= ( id[] ":")? text['"','"'];
-	Date ::= "DATE";
+	
+	Item     ::= "ITEM" text['"','"'] ( explanation['"','"'] )? 
+				 ("ONLY" "IF" dependentOf[])? ":" 
+				 itemType !0;
+	
+	Choice   ::= "CHOICE" (multiple[MULTIPLE])? "(" options ("," options)* ")";
+	Option   ::= ( id[] ":")? text['"','"'];
+	Date     ::= "DATE";
 	FreeText ::= "FREETEXT";
-	Number ::= "NUMBER";
+	Number   ::= "NUMBER";
 	Decision ::=  "DECISION" "(" options "," options ")"; 
 }
