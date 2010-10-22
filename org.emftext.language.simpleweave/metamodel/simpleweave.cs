c73275e3768e86d0c26e89eb953d7b5a4e615ed7
Jendrik Johannes <jendrik.johannes@devboost.de>
2010-10-22 10:11:24 +0000

    more attributes

--- org.emftext.language.simpleweave/metamodel/simpleweave.cs
+++ org.emftext.language.simpleweave/metamodel/simpleweave.cs
@@ -3,9 +3,9 @@ FOR <http://www.emftext.org/language/simpleweave>
 START WModel
 
 RULES{
-	WModel::= "core" coreID['<','>'] "{" aspects+ "}"  ;
+	WModel::= "type" type[] "core" coreID['<','>'] "{" aspects+ "}"  ;
 	
-	Aspect::= "weave"  aspectID['<','>'] "{" weavings+ "}";
+	Aspect::= "weave"  name[] ":" aspectID['<','>'] "{" weavings+ "}";
 	
 	Weaving ::= adviceContentID[] "into" joinPointID[];
 }
