74a278cde02c8858cbcc87050b7b71f3898f26f6
Mirko Seifert <mirko.seifert@devboost.de>
2010-11-05 13:27:16 +0000

    removed unused import

--- org.emftext.language.manifest.test/src/org/emftext/language/manifest/test/ManifestTestSuite.java
+++ org.emftext.language.manifest.test/src/org/emftext/language/manifest/test/ManifestTestSuite.java
@@ -13,7 +13,6 @@ import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.StringWriter;
 import java.util.Map;
 
 import org.eclipse.emf.common.util.URI;
