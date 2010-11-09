6fd4f08a9116f97446e788fd05063766de50a48e
Mirko Seifert <mirko.seifert@devboost.de>
2010-11-09 19:56:25 +0000

    committing example project for SoSym DSL

---
+++ org.emftext.language.egui.example/src/org/emftext/language/egui/example/LoginService.java
@@ -0,0 +1,10 @@
+package org.emftext.language.egui.example;
+
+import org.emftext.language.egui.statemodel.Event;
+
+public class LoginService {
+
+	public Event checkLogin() {
+		return null;
+	}
+}
