f2be53c896249c992223e21fd783a4e18ec4b9b4
Jendrik Johannes <jendrik.johannes@devboost.de>
2010-11-15 14:47:28 +0000

    added import feature

--- org.emftext.language.egui/metamodel/egui.cs
+++ org.emftext.language.egui/metamodel/egui.cs
@@ -1,14 +1,22 @@
 SYNTAXDEF egui
 FOR <http://www.emftext.org/language/egui>
 START Application
+// == IMPORT ==
+, screenimport.ScreenTemplate
+// ============
 
 IMPORTS {
 	action : <http://www.emftext.org/language/egui/actions>
 	sm : <http://www.emftext.org/language/egui/statemodel>
 	screen : <http://www.emftext.org/language/egui/screenmodel>
 	widget : <http://www.emftext.org/language/egui/widgets>
+	screenimport : <http://www.emftext.org/language/egui/screenimport>
 	
 	java : <http://www.emftext.org/java/members>
+}
+
+OPTIONS {
+	additionalDependencies="org.emftext.language.java.resource";
 }
 
 TOKENS {
@@ -34,10 +42,17 @@ RULES {
 	// the widgets
 	widget.Panel     ::= "panel" name[] #1 "{" (!1 compounds)* !0 "}";
 	widget.Button    ::= "button" name[] #1 label['"','"'] ";";
-	widget.UIList      ::= "list" name[] ";";
+	widget.UIList    ::= "list" name[] ";";
 	widget.TextField ::= "textfield" name[] ";";
+	widget.Text      ::= "text" name[] #1 text['"','"'] ";";
 	
 	// actions
 	action.ShowScreenAction ::= "show" screen[];
 	action.JavaAction       ::= "call" #1 method['<','>'];
+	
+	//IMPORT MECHANISM
+	screenimport.ScreenTemplate ::= "template" screen;
+	screenimport.ScreenImport ::= "import" id['<','>'] "as" name[] (#1 "(" settings ("," #1 settings)* ")")? ";" !0;
+	screenimport.ParameterSetting ::= parameter[] "=" value['"','"'];
+	//======
 }
