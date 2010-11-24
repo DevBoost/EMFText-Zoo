1d19db39380454bcb6b777eef95bfae7f3559754
Jendrik Johannes <jendrik.johannes@devboost.de>
2010-11-24 14:06:39 +0000

    renamed to appflow

---
+++ org.emftext.language.egui.example/src/org/emftext/language/appflow/example/LoginService.java
@@ -0,0 +1,10 @@
+package org.emftext.language.appflow.example;
+
+import org.emftext.language.appflow.statemodel.Event;
+
+public class LoginService {
+
+	public Event checkLogin() {
+		return null;
+	}
+}
