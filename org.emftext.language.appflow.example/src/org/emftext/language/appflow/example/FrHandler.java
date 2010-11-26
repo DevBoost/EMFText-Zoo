547d5ad4e9d0b266bac547af67bf6e10a0782087
Sebastian Cech <sebastian.cech@tu-dresden.de>
2010-11-26 14:19:55 +0000

    appflow based mdpe example

---
+++ org.emftext.language.appflow.example/src/org/emftext/language/appflow/example/FrHandler.java
@@ -0,0 +1,18 @@
+package org.emftext.language.appflow.example;
+
+import org.emftext.language.appflow.statemodel.Event;
+
+public class FrHandler {
+
+	public Event acceptFeatureRequest() {
+		return null;
+	}
+	
+	public Event rejectFeatureRequest() {
+		return null;
+	}
+	
+	public Event notifyCustomer(){
+		return null;
+	}
+}
