6e63ac77118767fc94836e2df0699c52736132d2
Mirko Seifert <mirko.seifert@devboost.de>
2011-02-24 10:18:24 +0000

    cleaned resolvers

--- org.emftext.language.rails.resource.rails/src/org/emftext/language/rails/resource/rails/analysis/ConnectionFromReferenceResolver.java
+++ org.emftext.language.rails.resource.rails/src/org/emftext/language/rails/resource/rails/analysis/ConnectionFromReferenceResolver.java
@@ -13,20 +13,27 @@
  ******************************************************************************/
 package org.emftext.language.rails.resource.rails.analysis;
 
-public class ConnectionFromReferenceResolver implements org.emftext.language.rails.resource.rails.IRailsReferenceResolver<org.emftext.language.rails.Connection, org.emftext.language.rails.Out> {
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EReference;
+import org.emftext.language.rails.Connection;
+import org.emftext.language.rails.Out;
+import org.emftext.language.rails.resource.rails.IRailsReferenceResolveResult;
+import org.emftext.language.rails.resource.rails.IRailsReferenceResolver;
+
+public class ConnectionFromReferenceResolver implements IRailsReferenceResolver<Connection, Out> {
 	
-	private org.emftext.language.rails.resource.rails.analysis.RailsDefaultResolverDelegate<org.emftext.language.rails.Connection, org.emftext.language.rails.Out> delegate = new org.emftext.language.rails.resource.rails.analysis.RailsDefaultResolverDelegate<org.emftext.language.rails.Connection, org.emftext.language.rails.Out>();
+	private RailsDefaultResolverDelegate<Connection, Out> delegate = new RailsDefaultResolverDelegate<Connection, Out>();
 	
-	public void resolve(java.lang.String identifier, org.emftext.language.rails.Connection container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, final org.emftext.language.rails.resource.rails.IRailsReferenceResolveResult<org.emftext.language.rails.Out> result) {
+	public void resolve(String identifier, Connection container, EReference reference, int position, boolean resolveFuzzy, final IRailsReferenceResolveResult<Out> result) {
 		delegate.resolve(identifier, container, reference, position, resolveFuzzy, result);
 	}
 	
-	public java.lang.String deResolve(org.emftext.language.rails.Out element, org.emftext.language.rails.Connection container, org.eclipse.emf.ecore.EReference reference) {
+	public String deResolve(Out element, Connection container, EReference reference) {
 		return delegate.deResolve(element, container, reference);
 	}
 	
-	public void setOptions(java.util.Map<?,?> options) {
-		// TODO save options in a field or leave method empty if this resolver does not depend on any option
+	public void setOptions(Map<?,?> options) {
+		// not used
 	}
-	
 }
--- org.emftext.language.rails.resource.rails/src/org/emftext/language/rails/resource/rails/analysis/ConnectionToReferenceResolver.java
+++ org.emftext.language.rails.resource.rails/src/org/emftext/language/rails/resource/rails/analysis/ConnectionToReferenceResolver.java
@@ -13,20 +13,27 @@
  ******************************************************************************/
 package org.emftext.language.rails.resource.rails.analysis;
 
-public class ConnectionToReferenceResolver implements org.emftext.language.rails.resource.rails.IRailsReferenceResolver<org.emftext.language.rails.Connection, org.emftext.language.rails.In> {
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EReference;
+import org.emftext.language.rails.Connection;
+import org.emftext.language.rails.In;
+import org.emftext.language.rails.resource.rails.IRailsReferenceResolveResult;
+import org.emftext.language.rails.resource.rails.IRailsReferenceResolver;
+
+public class ConnectionToReferenceResolver implements IRailsReferenceResolver<Connection, In> {
 	
-	private org.emftext.language.rails.resource.rails.analysis.RailsDefaultResolverDelegate<org.emftext.language.rails.Connection, org.emftext.language.rails.In> delegate = new org.emftext.language.rails.resource.rails.analysis.RailsDefaultResolverDelegate<org.emftext.language.rails.Connection, org.emftext.language.rails.In>();
+	private RailsDefaultResolverDelegate<Connection, In> delegate = new RailsDefaultResolverDelegate<Connection, In>();
 	
-	public void resolve(java.lang.String identifier, org.emftext.language.rails.Connection container, org.eclipse.emf.ecore.EReference reference, int position, boolean resolveFuzzy, final org.emftext.language.rails.resource.rails.IRailsReferenceResolveResult<org.emftext.language.rails.In> result) {
+	public void resolve(String identifier, Connection container, EReference reference, int position, boolean resolveFuzzy, final IRailsReferenceResolveResult<In> result) {
 		delegate.resolve(identifier, container, reference, position, resolveFuzzy, result);
 	}
 	
-	public java.lang.String deResolve(org.emftext.language.rails.In element, org.emftext.language.rails.Connection container, org.eclipse.emf.ecore.EReference reference) {
+	public String deResolve(In element, Connection container, EReference reference) {
 		return delegate.deResolve(element, container, reference);
 	}
 	
-	public void setOptions(java.util.Map<?,?> options) {
-		// TODO save options in a field or leave method empty if this resolver does not depend on any option
+	public void setOptions(Map<?,?> options) {
+		// not used
 	}
-	
 }
