5bfe2130ba84d2039806ad5350fecc7f650a28bf
Mirko Seifert <mirko.seifert@devboost.de>
2011-04-07 15:56:31 +0000

    added codegen plug-in for WebTest DSL

---
+++ org.emftext.language.webtest.codegen/src/org/emftext/language/webtest/codegen/WebTestBuilder.java
@@ -0,0 +1,55 @@
+package org.emftext.language.webtest.codegen;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.emftext.language.webtest.codegen.files.Generate;
+import org.emftext.language.webtest.resource.webtest.IWebtestBuilder;
+import org.emftext.language.webtest.resource.webtest.mopp.WebtestResource;
+
+public class WebTestBuilder extends IncrementalProjectBuilder implements IWebtestBuilder {
+
+	public org.eclipse.core.resources.IProject[] build(int kind, @SuppressWarnings("rawtypes") java.util.Map args, final org.eclipse.core.runtime.IProgressMonitor monitor) throws org.eclipse.core.runtime.CoreException {
+		return new org.emftext.language.webtest.resource.webtest.mopp.WebtestBuilderAdapter().build(kind, args, monitor, this, getProject());
+	}
+
+	public boolean isBuildingNeeded(URI uri) {
+		return !uri.toString().contains("/bin/");
+	}
+
+	public IStatus build(WebtestResource resource, IProgressMonitor monitor) {
+		Generate generate = new Generate();
+		URI uri = resource.getURI();
+		IResource wRes = getResourceInWorkspace(uri);
+		File targetFolder = getTargetFolder(wRes);
+		try {
+			generate.initialize(uri, targetFolder, Collections.EMPTY_LIST);
+			generate.doGenerate(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return Status.OK_STATUS;
+	}
+
+	private File getTargetFolder(IResource member) {
+		File targetFolder =  new File(member.getRawLocation().toOSString());
+		return targetFolder;
+	}
+
+	private IResource getResourceInWorkspace(URI model) {
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		URI targetURI = model.trimSegments(1);
+		String platformString = targetURI.toPlatformString(true);
+		IResource member = root.findMember(platformString);
+		return member;
+	}
+}
