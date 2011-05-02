90644f2968790f7817dcc27ad27d53c47d6ecf44
Mirko Seifert <mirko.seifert@devboost.de>
2011-05-02 18:33:19 +0000

    enabled keyword escaping for TextEcore

--- org.emftext.language.ecore.resource.text/src/org/emftext/language/ecore/resource/text/analysis/TextEcoreTEXTTokenResolver.java
+++ org.emftext.language.ecore.resource.text/src/org/emftext/language/ecore/resource/text/analysis/TextEcoreTEXTTokenResolver.java
@@ -15,19 +15,21 @@ package org.emftext.language.ecore.resource.text.analysis;
 
 import java.util.Map;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.emftext.language.ecore.resource.text.ITextEcoreTokenResolveResult;
 import org.emftext.language.ecore.resource.text.ITextEcoreTokenResolver;
 
 public class TextEcoreTEXTTokenResolver implements ITextEcoreTokenResolver {
 	
-	private TextEcoreDefaultTokenResolver defaultResolver = new TextEcoreDefaultTokenResolver();
+	private TextEcoreDefaultTokenResolver defaultResolver = new TextEcoreDefaultTokenResolver(true);
 	
-	public java.lang.String deResolve(java.lang.Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
-		java.lang.String result = defaultResolver.deResolve(value, feature, container);
+	public String deResolve(Object value, EStructuralFeature feature, EObject container) {
+		String result = defaultResolver.deResolve(value, feature, container);
 		return result;
 	}
 	
-	public void resolve(java.lang.String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, ITextEcoreTokenResolveResult result) {
+	public void resolve(String lexem, EStructuralFeature feature, ITextEcoreTokenResolveResult result) {
 		defaultResolver.resolve(lexem, feature, result);
 	}
 
