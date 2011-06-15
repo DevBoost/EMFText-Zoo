85140b7577ade8dbc64f8214068ef50594ffce91
Mirko Seifert <mirko.seifert@devboost.de>
2011-06-15 11:24:48 +0000

    removed unused options regenerated with current SDK

--- org.emftext.language.mecore/metamodel/mecore.cs
+++ org.emftext.language.mecore/metamodel/mecore.cs
@@ -4,13 +4,7 @@ FOR <http://www.emftext.org/language/mecore> <mecore.genmodel>
 START MPackage
 
 OPTIONS {
-	//baseResourcePlugin = "org.emftext.language.mecore.resource";
 	licenceHeader ="../../org.dropsbox/licence.txt";
-	
-	resourcePluginID = "org.emftext.language.mecore.resource.mecore";
-	basePackage = "org.emftext.language.mecore.resource.mecore";
-	resourceUIPluginID = "org.emftext.language.mecore.resource.mecore.ui";
-	uiBasePackage = "org.emftext.language.mecore.resource.mecore.ui";
 	
 	usePredefinedTokens = "false";
 	overrideBuilder = "false";
