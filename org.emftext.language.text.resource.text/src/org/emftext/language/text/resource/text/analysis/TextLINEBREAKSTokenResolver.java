289dded81c0beeaa833d2bd3f7c060838372e45b
Christian Wende <christian.wende@devboost.de>
2011-07-11 07:57:33 +0000

    added .text language to parse plain text files into paragraphs, words and whitespaces

---
+++ org.emftext.language.text.resource.text/src/org/emftext/language/text/resource/text/analysis/TextLINEBREAKSTokenResolver.java
@@ -0,0 +1,28 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * 
+ */
+package org.emftext.language.text.resource.text.analysis;
+
+public class TextLINEBREAKSTokenResolver implements org.emftext.language.text.resource.text.ITextTokenResolver {
+	
+	private org.emftext.language.text.resource.text.analysis.TextDefaultTokenResolver defaultTokenResolver = new org.emftext.language.text.resource.text.analysis.TextDefaultTokenResolver(true);
+	
+	public String deResolve(Object value, org.eclipse.emf.ecore.EStructuralFeature feature, org.eclipse.emf.ecore.EObject container) {
+		// By default token de-resolving is delegated to the DefaultTokenResolver.
+		String result = defaultTokenResolver.deResolve(value, feature, container, null, null, null);
+		return result;
+	}
+	
+	public void resolve(String lexem, org.eclipse.emf.ecore.EStructuralFeature feature, org.emftext.language.text.resource.text.ITextTokenResolveResult result) {
+		// By default token resolving is delegated to the DefaultTokenResolver.
+		defaultTokenResolver.resolve(lexem, feature, result, null, null, null);
+	}
+	
+	public void setOptions(java.util.Map<?,?> options) {
+		defaultTokenResolver.setOptions(options);
+	}
+	
+}
---
+++ org.emftext.language.text/metamodel/text.text.cs
@@ -0,0 +1,24 @@
+SYNTAXDEF text
+FOR <http://org.emftext/text.text.ecore>
+START Document
+
+OPTIONS {
+	usePredefinedTokens = "false";
+	defaultTokenName = "TEXT";
+}
+
+TOKENS {
+
+	DEFINE TEXT $~('\n'|'\r'|'\uffff'|' '|'\t'|'\f'|'\r\n')+$ ;
+	DEFINE WHITESPACE $(' '|'\t'|'\f')$;
+	DEFINE LINEBREAKS $('\r\n'|'\r'|'\n')$;
+}
+
+
+RULES {
+	Document ::= (parts)*;
+	Paragraph ::= tokens:Whitespace* ((tokens:Word tokens:Whitespace* tokens:Linebreak? tokens:Whitespace*))* end:Linebreak;
+	Word ::= text[TEXT];
+	Whitespace ::= text[WHITESPACE];
+	Linebreak ::= text[LINEBREAKS];
+}
