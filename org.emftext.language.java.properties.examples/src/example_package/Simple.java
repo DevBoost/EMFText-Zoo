68bfa327272283333ce6160b138c7835c518426c
Mirko Seifert <mirko.seifert@devboost.de>
2011-07-21 18:58:49 +0000

    updated propjava example

--- org.emftext.language.java.properties.examples/src/example_package/Simple.java
+++ org.emftext.language.java.properties.examples/src/example_package/Simple.java
@@ -1,29 +1,19 @@
-/*******************************************************************************
- * Copyright (c) 2006-2011
- * Software Technology Group, Dresden University of Technology
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Software Technology Group - TU Dresden, Germany
- *      - initial API and implementation
- ******************************************************************************/
-package example_package ;
+package example_package;
 
 
 public class Simple {
-	private Object p1 ;
-	;
-	private Object p2 ;
-	;
-	Object getP1 () {
-		return null;
-	}
-	Object getP2 () {
-		return null;
-	}
+	private Object property1 ;
+	private Object property2 ;
+	Object getProperty1 () {
+	return property1 ;
 }
+	Object getProperty2 () {
+	return property2 ;
+}
+	void setProperty2 (Object newValue) {
+	property2 = newValue ;
+}
+}
+
+
 
