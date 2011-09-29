55c8cf1a5d7d6d33ba9c6b8b9684b0a26c47886c
Jendrik Johannes <jendrik.johannes@devboost.de>
2011-09-29 23:19:36 +0000

    fixed test case to load Ecore models directly instead of using the registry (the old version was buggy: some EClasses were loaded multiple times which caused problems during type checking in the reference resolvers)

--- org.emftext.language.efactory.test/src/org/emftext/language/efactory/test/LoadTest.java
+++ org.emftext.language.efactory.test/src/org/emftext/language/efactory/test/LoadTest.java
@@ -14,7 +14,7 @@
 package org.emftext.language.efactory.test;
 
 import java.io.File;
-import java.io.FileFilter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,11 +23,9 @@ import junit.framework.TestCase;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EPackage.Registry;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.Resource.Diagnostic;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
@@ -40,35 +38,33 @@ public class LoadTest extends TestCase {
 
 	private final static String EFACTORY_EXTENSION = new EfactoryMetaInformation().getSyntaxName();
 	
-	private ResourceSet rs = new ResourceSetImpl();
+	private final static ResourceSet rs = new ResourceSetImpl();
 
-	public void setUp() {
+	static {
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
 				"ecore",
 				new XMIResourceFactoryImpl());
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put(
 				EFACTORY_EXTENSION,
 				new EfactoryResourceFactory());
-
-		registerEPackages();
 	}
 
-	public void testEnumModel() {
+	public void testEnumModel() throws IOException {
 		testLoadModel("enumtest.efactory");
 		testLoadModel("enumtest2.efactory");
 	}
 	
-	public void testLoadSmallModel() {
+	public void testLoadSmallModel() throws IOException {
 		testLoadModel("small.efactory");
 	}
 	
-	public void testLoadLargeModel() {
+	public void testLoadLargeModel() throws IOException {
 		testLoadModel("large.efactory");
 	}
 
-	private void testLoadModel(String filename) {
+	private void testLoadModel(String filename) throws IOException {
 		System.out.println("loading " + filename + "...");
-		Resource resource = rs.getResource(URI.createFileURI("." + File.separator + "input_load" + File.separator + filename), true);
+		Resource resource = rs.getResource(URI.createFileURI(new File("." + File.separator + "input_load" + File.separator + filename).getCanonicalPath()), true);
 		EList<Diagnostic> errors = resource.getErrors();
 		EList<Diagnostic> warnings = resource.getWarnings();
 		print(errors, "Error: ");
@@ -94,42 +90,6 @@ public class LoadTest extends TestCase {
 	private void print(EList<Diagnostic> diagnostics, String string) {
 		for (Diagnostic diagnostic : diagnostics) {
 			System.out.println(string + diagnostic.getMessage());
-		}
-	}
-
-	private void registerEPackages() {
-		File metamodelFolder = new File("." + File.separator + "metamodels");
-		File[] metamodels = metamodelFolder.listFiles(new FileFilter() {
-			
-			public boolean accept(File file) {
-				return file.getName().endsWith(".ecore");
-			}
-		});
-		for (File metamodel : metamodels) {
-			Resource resource = rs.getResource(URI.createFileURI(metamodel.getAbsolutePath()), true);
-			assertNotNull(resource);
-			EcoreUtil.resolveAll(resource);
-			EList<EObject> contents = resource.getContents();
-			for (EObject eObject : contents) {
-				if (eObject instanceof EPackage) {
-					EPackage ePackage = (EPackage) eObject;
-					register(ePackage);
-				}
-			}
-		}
-	}
-
-	private void register(EPackage ePackage) {
-		String nsURI = ePackage.getNsURI();
-		System.out.println("LoadTest.register() " + nsURI);
-		Registry registry = EPackage.Registry.INSTANCE;
-		if (registry.containsKey(nsURI)) {
-			//return;
-		}
-		registry.put(nsURI, ePackage);
-		EList<EPackage> eSubpackages = ePackage.getESubpackages();
-		for (EPackage subPackage : eSubpackages) {
-			register(subPackage);
 		}
 	}
 }
