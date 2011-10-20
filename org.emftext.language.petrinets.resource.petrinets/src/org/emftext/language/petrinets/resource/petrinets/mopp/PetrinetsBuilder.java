4435ac097ce26d15ecc4ac36a94ff96cc0185ac0
Christian Wende <christian.wende@devboost.de>
2011-10-20 12:28:46 +0000

    removed dependencies to owltext

--- org.emftext.language.petrinets.resource.petrinets/src/org/emftext/language/petrinets/resource/petrinets/mopp/PetrinetsBuilder.java
+++ org.emftext.language.petrinets.resource.petrinets/src/org/emftext/language/petrinets/resource/petrinets/mopp/PetrinetsBuilder.java
@@ -7,8 +7,6 @@
 package org.emftext.language.petrinets.resource.petrinets.mopp;
 
 import org.emftext.language.petrinets.resource.petrinets.PetrinetsEProblemType;
-import org.emftext.runtime.owltext.OWLTextValidationMarker;
-
 
 public class PetrinetsBuilder implements
 		org.emftext.language.petrinets.resource.petrinets.IPetrinetsBuilder {
@@ -20,19 +18,14 @@ public class PetrinetsBuilder implements
 	public org.eclipse.core.runtime.IStatus build(
 			org.emftext.language.petrinets.resource.petrinets.mopp.PetrinetsResource resource,
 			org.eclipse.core.runtime.IProgressMonitor monitor) {
-//		if (resource.getErrors().isEmpty()) {
-////					OWLTextValidationMarker ovm = new OWLTextValidationMarker();
-////					ovm.annotateValidationResults(resource);
-//		}
-//		else {
-//			resource.addError("The petrinet was not build due to errors in the resource",PetrinetsEProblemType.BUILDER_ERROR, resource.getContents().get(0));
-//		}
 		if (resource.getErrors().isEmpty()) {
 			PetriNetsCodeGenerator pcg = new PetriNetsCodeGenerator();
 			pcg.generateJavaCode(resource);
-		}
-		else {
-			resource.addWarning("The petrinet may not be build correctly due to errors in the resource",PetrinetsEProblemType.BUILDER_ERROR, resource.getContents().get(0));
+		} else {
+			resource.addWarning(
+					"The petrinet may not be build correctly due to errors in the resource",
+					PetrinetsEProblemType.BUILDER_ERROR, resource.getContents()
+							.get(0));
 			PetriNetsCodeGenerator pcg = new PetriNetsCodeGenerator();
 			pcg.generateJavaCode(resource);
 		}
