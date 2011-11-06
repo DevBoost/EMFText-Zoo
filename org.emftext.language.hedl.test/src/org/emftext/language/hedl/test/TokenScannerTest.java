477e9c6e9345efc6c1519977c105781b58856c74
Mirko Seifert <mirko.seifert@devboost.de>
2011-11-06 22:45:10 +0000

    added test plug-in for HEDL DSL which included tests for the task item detection

---
+++ org.emftext.language.hedl.test/src/org/emftext/language/hedl/test/TokenScannerTest.java
@@ -0,0 +1,52 @@
+package org.emftext.language.hedl.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.rules.IToken;
+import org.emftext.language.hedl.resource.hedl.IHedlTokenStyle;
+import org.emftext.language.hedl.resource.hedl.mopp.HedlTokenStyle;
+import org.emftext.language.hedl.resource.hedl.ui.HedlColorManager;
+import org.emftext.language.hedl.resource.hedl.ui.HedlTokenScanner;
+
+public class TokenScannerTest extends TestCase {
+
+	public void testTokenScanner() {
+		List<IToken> scan = scan("// TODO test item");
+		assertEquals(3, scan.size());
+		
+		scan = scan("/**\n * TODO item one \n * TODO item two \n*/");
+		assertEquals(5, scan.size());
+
+		scan = scan("/**\n * TODO item one \n * TODO item two \n * TODO item three \n*/");
+		assertEquals(7, scan.size());
+	}
+
+	private List<IToken> scan(String text) {
+		HedlColorManager colorManager = new HedlColorManager();
+		HedlTokenScanner scanner = new HedlTokenScanner(null, colorManager) {
+			@Override
+			public IHedlTokenStyle getStaticTokenStyle() {
+				int[] foreground = new int[] {0,0,0};
+				int[] background = new int[] {0,0,0};
+				return new HedlTokenStyle(foreground, background, false, false, false, false);
+			}
+		};
+		
+		IDocument document = new Document();
+		document.set(text);
+		scanner.setRange(document, 0, text.length());
+		
+		List<IToken> allTokens = new ArrayList<IToken>();
+		IToken nextToken = scanner.nextToken();
+		while (nextToken != null && !nextToken.isEOF()) {
+			allTokens.add(nextToken);
+			nextToken = scanner.nextToken();
+		}
+		return allTokens;
+	}
+}
