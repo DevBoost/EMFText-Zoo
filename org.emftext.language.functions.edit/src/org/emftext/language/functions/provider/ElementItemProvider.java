e03cc228c5e5b6266af8cfcfaa0c16ea5ab000f8
Mirko Seifert <mirko.seifert@devboost.de>
2011-11-21 15:58:08 +0000

    organized imports

--- org.emftext.language.functions.edit/src/org/emftext/language/functions/provider/ElementItemProvider.java
+++ org.emftext.language.functions.edit/src/org/emftext/language/functions/provider/ElementItemProvider.java
@@ -12,9 +12,6 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-
-import org.eclipse.emf.common.util.ResourceLocator;
-
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
@@ -22,7 +19,6 @@ import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
 import org.emftext.language.functions.Element;
-import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 
 /**
  * This is the item provider adapter for a {@link org.emftext.language.functions.Element} object.
--- org.emftext.language.functions.edit/src/org/emftext/language/functions/provider/functionsEditPlugin.java
+++ org.emftext.language.functions.edit/src/org/emftext/language/functions/provider/functionsEditPlugin.java
@@ -7,7 +7,6 @@
 package org.emftext.language.functions.provider;
 
 import org.eclipse.emf.common.EMFPlugin;
-
 import org.eclipse.emf.common.util.ResourceLocator;
 
 /**
