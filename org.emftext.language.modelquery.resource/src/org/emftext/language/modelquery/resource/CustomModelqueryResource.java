6d593c7ca7e53951147e84003015c12781175387
Jendrik Johannes <jendrik.johannes@devboost.de>
2011-12-31 14:52:40 +0000

    debug output

--- org.emftext.language.modelquery.resource/src/org/emftext/language/modelquery/resource/CustomModelqueryResource.java
+++ org.emftext.language.modelquery.resource/src/org/emftext/language/modelquery/resource/CustomModelqueryResource.java
@@ -230,6 +230,8 @@ public class CustomModelqueryResource extends ModelqueryResource {
 				contents.add(copy);
 				modelQuery.getResultIDs().add(result.get(copy));
 			}
+			System.out.println("[Modelquery] Query:  " + modelQuery.getQueries());
+			System.out.println("[Modelquery] Result: " + modelQuery.getResultIDs());
 		}
 		
 
