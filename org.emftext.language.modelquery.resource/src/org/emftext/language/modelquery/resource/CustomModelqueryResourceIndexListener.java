6fdb7b0d79b5543ef79065ce596d89e80628c82a
Jendrik Johannes <jendrik.johannes@devboost.de>
2011-12-31 11:20:59 +0000

    Updated tests to use juni4 @Before annotation for initialization / added debug output in CustomModelqueryResourceIndexListener

--- org.emftext.language.modelquery.resource/src/org/emftext/language/modelquery/resource/CustomModelqueryResourceIndexListener.java
+++ org.emftext.language.modelquery.resource/src/org/emftext/language/modelquery/resource/CustomModelqueryResourceIndexListener.java
@@ -16,6 +16,7 @@ package org.emftext.language.modelquery.resource;
 import java.lang.ref.WeakReference;
 import java.util.Set;
 
+import org.eclipse.emf.common.util.URI;
 import org.reuseware.sokan.ID;
 import org.reuseware.sokan.index.notify.IndexListener;
 import org.reuseware.sokan.index.util.IndexUtil;
@@ -23,10 +24,12 @@ import org.reuseware.sokan.index.util.IndexUtil;
 public class CustomModelqueryResourceIndexListener implements IndexListener {
 
 	private WeakReference<CustomModelqueryResource> resourceRef = null;
+	private URI resourceURI = null;
 	
 	public CustomModelqueryResourceIndexListener(
 			CustomModelqueryResource resource) {
 		this.resourceRef = new WeakReference<CustomModelqueryResource>(resource);
+		this.resourceURI = resource.getURI();
 	}
 
 	public void indexChanged(Set<ID> delta) {
@@ -35,6 +38,7 @@ public class CustomModelqueryResourceIndexListener implements IndexListener {
 			resource.executeQuery();
 		}
 		else {
+			System.err.println("[Modelquery] Removing Index Listener for: " + resourceURI.toString());
 			IndexUtil.INSTANCE.removeListener(this);
 		}
 	}
