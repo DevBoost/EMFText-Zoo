a9076a4ef2e817e811bc49dcbf1abd5ba19f7d5e
Mirko Seifert <mirko.seifert@devboost.de>
2012-01-30 15:22:05 +0000

    added missing method handleDeletion()

--- org.emftext.language.formsfamily.resource.formsfamily/src/org/emftext/language/formsfamily/resource/formsfamily/mopp/FormsfamilyBuilder.java
+++ org.emftext.language.formsfamily.resource.formsfamily/src/org/emftext/language/formsfamily/resource/formsfamily/mopp/FormsfamilyBuilder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2011
+ * Copyright (c) 2006-2012
  * Software Technology Group, Dresden University of Technology
  *
  * All rights reserved. This program and the accompanying materials
@@ -13,16 +13,25 @@
  ******************************************************************************/
 package org.emftext.language.formsfamily.resource.formsfamily.mopp;
 
-public class FormsfamilyBuilder implements org.emftext.language.formsfamily.resource.formsfamily.IFormsfamilyBuilder {
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.emftext.language.formsfamily.resource.formsfamily.IFormsfamilyBuilder;
+
+public class FormsfamilyBuilder implements IFormsfamilyBuilder {
 	
-	public boolean isBuildingNeeded(org.eclipse.emf.common.util.URI uri) {
+	public boolean isBuildingNeeded(URI uri) {
 		// change this to return true to enable building of all resources
 		return false;
 	}
 	
-	public org.eclipse.core.runtime.IStatus build(org.emftext.language.formsfamily.resource.formsfamily.mopp.FormsfamilyResource resource, org.eclipse.core.runtime.IProgressMonitor monitor) {
+	public IStatus build(FormsfamilyResource resource, IProgressMonitor monitor) {
 		// set option overrideBuilder to 'false' and then perform build here
-		return org.eclipse.core.runtime.Status.OK_STATUS;
+		return Status.OK_STATUS;
 	}
-	
+
+	public IStatus handleDeletion(URI uri, IProgressMonitor monitor) {
+		return Status.OK_STATUS;
+	}
 }
