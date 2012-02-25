bac1fe9a110049a9e8d5396731a49724c8ffeb4d
Mirko Seifert <mirko.seifert@devboost.de>
2012-02-25 09:55:35 +0000

    fixed compile error

--- org.emftext.language.java.javabehavior4uml.resource/src/org/emftext/language/java/javabehavior4uml/resource/actions/AddToUMLClassifierAction.java
+++ org.emftext.language.java.javabehavior4uml.resource/src/org/emftext/language/java/javabehavior4uml/resource/actions/AddToUMLClassifierAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006-2011
+ * Copyright (c) 2006-2012
  * Software Technology Group, Dresden University of Technology
  *
  * All rights reserved. This program and the accompanying materials
@@ -165,7 +165,7 @@ public class AddToUMLClassifierAction implements IObjectActionDelegate {
 						e.printStackTrace();
 					}
 				}
-				JavabehaviorMarkerHelper.unmark(behaviorResource);
+				new JavabehaviorMarkerHelper().unmark(behaviorResource);
 			}
 		}
 	}
