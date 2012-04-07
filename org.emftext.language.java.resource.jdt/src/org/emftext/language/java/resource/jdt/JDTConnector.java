1fe0ff60de1109115d99d630eba310e5142bda9b
Mirko Seifert <mirko.seifert@devboost.de>
2012-04-07 19:22:29 +0000

    refactored JaMoPP JDT connector to reuse EMFText JDT connector

--- org.emftext.language.java.resource.jdt/src/org/emftext/language/java/resource/jdt/JDTConnector.java
+++ org.emftext.language.java.resource.jdt/src/org/emftext/language/java/resource/jdt/JDTConnector.java
@@ -13,21 +13,16 @@
  ******************************************************************************/
 package org.emftext.language.java.resource.jdt;
 
-import org.eclipse.core.resources.IProject;
+import java.util.List;
+
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.JavaModelException;
-import org.eclipse.jdt.core.search.IJavaSearchConstants;
-import org.eclipse.jdt.core.search.SearchEngine;
-import org.eclipse.jdt.core.search.TypeNameRequestor;
+import org.emftext.commons.jdt.JDTJavaClass;
+import org.emftext.commons.jdt.resolve.JDTClassifierResolver;
 import org.emftext.language.java.JavaClasspath;
 
 public class JDTConnector implements JavaClasspath.Initializer {
@@ -44,29 +39,6 @@ public class JDTConnector implements JavaClasspath.Initializer {
 		return false;
 	}
 	
-	private boolean isJavaProject(IProject project) {
-		if (project == null) {
-			return false;
-		}
-		try {
-			return project.isNatureEnabled("org.eclipse.jdt.core.javanature");
-		} catch (CoreException e) {
-		}
-		return false;
-	}
-
-	private IProject getProject(URI uri) {
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		if (uri.isPlatformResource() && uri.segmentCount() > 2) {
-			return root.getProject(uri.segment(1));
-		}
-		return null;
-	}
-	
-	private IJavaProject getJavaProject(IProject project) {
-		return (isJavaProject(project) ? JavaCore.create(project) : null);
-	}
-	
 	private void initializeResourceSet(ResourceSet resourceSet, URI resourceURI) {
 		if (resourceSet == null) {
 			return;
@@ -80,81 +52,61 @@ public class JDTConnector implements JavaClasspath.Initializer {
 		}
 
 		if (resourceURI != null) {
-			IJavaProject javaProject = getJavaProject(getProject(resourceURI));
-			registerJavaProjectInClassPath(resourceSet, javaProject);
+			JDTClassifierResolver jdtClassResolver = new JDTClassifierResolver();
+			IJavaProject javaProject = jdtClassResolver.getJavaProject(resourceURI);
+			List<JDTJavaClass> classifiersInClassPath = jdtClassResolver.getAllClassifiersInClassPath(javaProject);
+			registerJavaProjectInClassPath(resourceSet, classifiersInClassPath);
 		}
 	}
 
-	private void registerJavaProjectInClassPath(
-			final ResourceSet resourceSet,
-			final IJavaProject javaProject) {
+	private void registerJavaProjectInClassPath(ResourceSet resourceSet,
+			List<JDTJavaClass> classifiersInClassPath) {
 
-		SearchEngine searchEngine = new SearchEngine();
-		
-		try {
-			searchEngine.searchAllTypeNames(null, null, 
-					SearchEngine.createJavaSearchScope(new IJavaProject[] {javaProject}), 
-					new ClasspathFiller(JavaClasspath.get(resourceSet)), IJavaSearchConstants.FORCE_IMMEDIATE_SEARCH, null);
-		} catch (JavaModelException e) { 
-			log("Problem building classpath", e);
+		JavaClasspath javaClasspath = JavaClasspath.get(resourceSet);
+		for (JDTJavaClass jdtClassifier : classifiersInClassPath) {
+			registerClassifier(javaClasspath, jdtClassifier);
 		}
 	}
 
-	private void log(String msg, JavaModelException e) {
-		ResourcesPlugin.getPlugin().getLog().log(new 
-				Status(IStatus.WARNING, "org.emftext.language.java", "[JaMoPP] " + msg, e));
-	}	
-
-	private static final class ClasspathFiller extends TypeNameRequestor {
-		private final JavaClasspath classPath;
-
-		private ClasspathFiller(JavaClasspath classPath) {
-			this.classPath = classPath;
-		}
-
-		@Override
-		public void acceptType(int modifiers,
-				char[] packageName, char[] simpleTypeName,
-				char[][] enclosingTypeNames, String path) {
-			
-			String filePath = null;
-			String inArchivePath = null;
-			if (path.contains(".jar|")) {
-				String[] split = path.split("\\|");
-				filePath = split[0];
-				inArchivePath = split[1];
-			} else {
-				filePath = path;
-			}
-			
-			URI uri;
-			if (isInWorkspace(filePath)) {
-				if (inArchivePath != null) {
-					uri = URI.createURI("archive:" + URI.createPlatformResourceURI(filePath, true) + "!/" + inArchivePath);
-				} else {
-					uri = URI.createPlatformResourceURI(filePath, true);
-				}		
-			} else {
-				if (inArchivePath != null) {
-					uri = URI.createURI("archive:" + URI.createFileURI(filePath).toString() + "!/" + inArchivePath);
-				} else {
-					uri = URI.createFileURI(filePath);
-				}
-			}
-
-			String fullContainerName = String.valueOf(packageName) + ".";
-			for (char[] enclosingType : enclosingTypeNames) {
-				fullContainerName = fullContainerName + String.valueOf(enclosingType) + "$";
-			}
-			
-			classPath.registerClassifier(
-					fullContainerName, 
-					String.valueOf(simpleTypeName), uri);			
+	private void registerClassifier(JavaClasspath javaClasspath, JDTJavaClass jdtClass) {
+		String path = jdtClass.getPath();
+		String filePath = null;
+		String inArchivePath = null;
+		if (path.contains(".jar|")) {
+			String[] split = path.split("\\|");
+			filePath = split[0];
+			inArchivePath = split[1];
+		} else {
+			filePath = path;
 		}
 		
-		private boolean isInWorkspace(String path) {
-			IWorkspaceRoot wsRoot = ResourcesPlugin.getWorkspace().getRoot();
-			return wsRoot.findMember(path) != null;
+		URI uri;
+		if (isInWorkspace(filePath)) {
+			if (inArchivePath != null) {
+				uri = URI.createURI("archive:" + URI.createPlatformResourceURI(filePath, true) + "!/" + inArchivePath);
+			} else {
+				uri = URI.createPlatformResourceURI(filePath, true);
+			}		
+		} else {
+			if (inArchivePath != null) {
+				uri = URI.createURI("archive:" + URI.createFileURI(filePath).toString() + "!/" + inArchivePath);
+			} else {
+				uri = URI.createFileURI(filePath);
+			}
 		}
+
+		String fullContainerName = jdtClass.getPackageName() + ".";
+		for (String enclosingType : jdtClass.getEnclosingTypeNames()) {
+			fullContainerName = fullContainerName + enclosingType + "$";
+		}
+		
+		javaClasspath.registerClassifier(
+				fullContainerName, 
+				jdtClass.getSimpleName(), uri);			
+	}
+		
+	private boolean isInWorkspace(String path) {
+		IWorkspaceRoot wsRoot = ResourcesPlugin.getWorkspace().getRoot();
+		return wsRoot.findMember(path) != null;
 	}
 }
