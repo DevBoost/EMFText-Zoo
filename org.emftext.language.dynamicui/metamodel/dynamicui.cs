1aec2e481eff735a3e74bb42c70bb7dd360df8e7
Mirko Seifert <mirko.seifert@devboost.de>
2012-04-11 11:44:04 +0000

    added license header option

--- org.emftext.language.dynamicui/metamodel/dynamicui.cs
+++ org.emftext.language.dynamicui/metamodel/dynamicui.cs
@@ -2,6 +2,9 @@ SYNTAXDEF dynamicui
 FOR <http://www.dynamicui.org/>
 START UIElement
 
+OPTIONS {
+	licenceHeader ="../../org.dropsbox/licence.txt";
+}
 
 RULES {
 	UIElement ::= name[] properties* "{" children* "}";
